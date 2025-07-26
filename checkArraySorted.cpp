57f4092d9e5484ac1c7327d27678652c2fd463fc
Sahana240107 <sahana240107@gmail.com>
2025-07-26 22:33:03 +0530

    checkArraySorted created

---
+++ checkArraySorted.cpp
@@ -0,0 +1,16 @@
+//Given an array arr[], check whether it is sorted in non-decreasing order.
+//  Return true if it is sorted otherwise false.
+class Solution {
+  public:
+    bool isSorted(vector<int>& arr) {
+        // code here
+        for(int i=0;i<arr.size()-1;i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
