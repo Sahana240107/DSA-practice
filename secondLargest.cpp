8b4da129c49fefe1cf1681c3d15cb97f354e26db
Sahana240107 <sahana240107@gmail.com>
2025-07-27 23:41:03 +0530

    secondLargest question added

--- secondLargest.cpp
+++ secondLargest.cpp
@@ -1,3 +1,26 @@
 /*Given an array of positive integers arr[], return the second largest element from the array. 
 If the second largest element doesn't exist then return -1.
 Note: The second largest element should not be equal to the largest element.*/
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        if(arr.size()<2)
+        {
+            return -1;
+        }
+        int max=INT_MIN,second=INT_MIN;
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]>max)
+            {
+                second=max;
+                max=arr[i];
+            }
+            if(arr[i]>second && arr[i]<max)
+            {
+                second=arr[i];
+            }
+        }
+        return (second==INT_MIN)?-1:second;
+    }
+};
