8ff530542992b2b5b203c1e99106dbdef7be02fa
Sahana240107 <sahana240107@gmail.com>
2025-07-27 23:38:54 +0530

    bubbleSort question solved

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,1 +1,19 @@
 /*Given an array, arr[]. Sort the array using bubble sort algorithm.*/
+class Solution {
+  public:
+    void bubbleSort(vector<int>& arr) {
+        int t;
+        for(int i=0;i<arr.size()-1;i++)
+        {
+            for(int j=0;j<arr.size()-1-i;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    t=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=t;
+                }
+            }
+        }
+    }
+};
