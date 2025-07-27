30856099389013295febea8049aff78dc61772d5
Sahana240107 <sahana240107@gmail.com>
2025-07-27 07:18:22 +0530

    reverseArray question added

---
+++ reverseArray.cpp
@@ -0,0 +1,5 @@
+/*You are given an array of integers arr[]. 
+Your task is to reverse the given array.
+Note: Modify the array in place.*/
+/*Input: arr = [1, 4, 3, 2, 6, 5]
+Output: [5, 6, 2, 3, 4, 1]*/
