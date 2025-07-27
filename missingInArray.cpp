808cde84db1f2eb4a408b39bbb8785cc4a84bae1
Sahana240107 <sahana240107@gmail.com>
2025-07-27 07:16:22 +0530

    missingInArray question solved

--- missingInArray.cpp
+++ missingInArray.cpp
@@ -5,3 +5,15 @@ Your task is to identify and return the missing element.*/
 /*Input: arr[] = [8, 2, 4, 5, 3, 7, 1]
 Output: 6
 Explanation: All the numbers from 1 to 8 are present except 6.*/
+class Solution {
+  public:
+    int missingNum(vector<int>& arr) {
+        long int n=arr.size()+1,sum=0;
+        long int expectedSum=n*(n+1)/2;
+        for(int i=0;i<arr.size();i++)
+        {
+            sum+=arr[i];
+        }
+        return expectedSum-sum;//Total sum-actual sum will give missing number
+    }
+};
