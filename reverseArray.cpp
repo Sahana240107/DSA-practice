66ed15080ecb399bd876b9327a49ba608bfc2281
Sahana240107 <sahana240107@gmail.com>
2025-07-27 07:18:56 +0530

    reverseArray question solved

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -3,3 +3,18 @@ Your task is to reverse the given array.
 Note: Modify the array in place.*/
 /*Input: arr = [1, 4, 3, 2, 6, 5]
 Output: [5, 6, 2, 3, 4, 1]*/
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code here
+        int i=0,j=arr.size()-1,temp;
+        while(i<j)
+        {
+            temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+};
