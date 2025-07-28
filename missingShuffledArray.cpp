b75c1f3eabdec919af4e6d4717e13a91445a3962
Sahana240107 <sahana240107@gmail.com>
2025-07-28 23:08:07 +0530

    missingShuffledArray question added

---
+++ missingShuffledArray.cpp
@@ -0,0 +1,5 @@
+/*Given an array arr1. The contents of arr are copied into another array arr2 and numbers are shuffled.
+ Also, one element is removed from arr2. The task is to find the missing element.*/
+/*Input: arr1[] = [4, 8, 1, 3, 7] and arr2[] = [7, 4, 3, 1]
+Output: 8
+Explanation: 8 is the only element missing from arr2.*/
