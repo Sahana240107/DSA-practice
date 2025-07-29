9298dc14202895872f4db0d2ea9e6207d55d4b6b
Sahana240107 <sahana240107@gmail.com>
2025-07-29 07:29:52 +0530

    singleinDoubleSorted question added

---
+++ singleinDoubleSorted.cpp
@@ -0,0 +1,5 @@
+/*Given a sorted array arr[]. Find the element that appears only once in the array. 
+All other elements appear exactly twice. */
+/*Input: arr[] = [1, 1, 2, 2, 3, 3, 4, 50, 50, 65, 65]
+Output: 4
+Explanation: 4 is the only element that appears exactly once.*/
