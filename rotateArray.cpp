bc9ca3853539563f31c98cf850455432589b580c
Sahana240107 <sahana240107@gmail.com>
2025-07-29 07:27:27 +0530

    rotateArray question solved

--- rotateArray.cpp
+++ rotateArray.cpp
@@ -4,3 +4,15 @@ Note: Consider the array as circular.*/
 /*Input: arr[] = [1, 2, 3, 4, 5], d = 2
 Output: [3, 4, 5, 1, 2]
 Explanation: when rotated by 2 elements, it becomes 3 4 5 1 2.*/
+class Solution {
+  public:
+
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    void rotateArr(vector<int>& arr, int d) {
+        int n=arr.size();
+        d=d%n;
+        reverse(arr.begin(),arr.begin()+d);
+        reverse(arr.begin()+d,arr.end());
+        reverse(arr.begin(),arr.end());
+    }
+};
