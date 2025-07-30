ca5ff02d1643fce718e7884af5c8cecd59d60811
Sahana240107 <sahana240107@gmail.com>
2025-07-30 19:20:58 +0530

    search2Dsorted question solved

--- search2Dsorted.cpp
+++ search2Dsorted.cpp
@@ -4,3 +4,25 @@ Examples:
 Input: mat[][] = [[3, 30, 38],[20, 52, 54],[35, 60, 69]], x = 62
 Output: false
 Explanation: 62 is not present in the matrix, so output is false.*/
+// User function template for C++
+class Solution {
+  public:
+    bool matSearch(vector<vector<int>> &mat, int x) {
+        long row=mat.size();
+        long col=mat[0].size();
+        for(int i=0;i<row;i++)
+        {
+            if(mat[i][col-1]>=x)//since sorted find the row first
+            {
+                for(int j=col-1;j>=0;j--)
+                {
+                    if(mat[i][j]==x)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
