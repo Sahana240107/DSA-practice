032d20cf32453e4b40621364234cb82e40070dca
Sahana240107 <sahana240107@gmail.com>
2025-07-30 19:16:06 +0530

    arrayDuplicates.cpp question solved

--- arrayDuplicates.cpp
+++ arrayDuplicates.cpp
@@ -5,3 +5,22 @@ Examples:
 Input: arr[] = [2, 3, 1, 2, 3]
 Output: [2, 3] 
 Explanation: 2 and 3 occur more than once in the given array.*/
+class Solution {
+  public:
+    vector<int> findDuplicates(vector<int>& arr) {
+        vector<int> res;       
+        for(int i=0;i<arr.size();i++)
+        {
+            int j=abs(arr[i])-1;
+            if(arr[j]<0)//already visited
+            {
+                res.push_back(j+1);
+            }
+            else
+            {
+                arr[j]=-arr[j];//mark as visited
+            }
+        }
+        return res;
+    }
+};
