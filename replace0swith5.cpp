a3c0cffa790f2588468f12915d634c60e55dfe53
Sahana240107 <sahana240107@gmail.com>
2025-07-30 19:10:47 +0530

    replace0swith5 question solved

--- replace0swith5.cpp
+++ replace0swith5.cpp
@@ -3,3 +3,27 @@ Examples:
 Input: n = 1004
 Output: 1554
 Explanation: There are two zeroes in 1004 on replacing all zeroes with 5,the new number will be 1554.*/
+class Solution {
+  public:
+    int convertFive(int n) {
+        int a,res=0,i=1;
+        if(n==0)
+        {
+            return 5;
+        }
+        while(n!=0)
+        {
+            a=n%10;
+            n/=10;
+            if(a==0)
+            {
+                res=res+5*i;
+            }
+            else{
+                res=res+a*i;
+            }
+            i*=10;
+        }
+        return res;
+    }
+};
