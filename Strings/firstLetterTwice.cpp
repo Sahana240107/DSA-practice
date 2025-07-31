121cc026e25ffd45f0c11ec6ef63515e20501818
Sahana240107 <sahana240107@gmail.com>
2025-07-31 07:34:47 +0530

    firstLetterTwice question added

---
+++ Strings/firstLetterTwice.cpp
@@ -0,0 +1,7 @@
+/*Given a string s. The task is to find the first repeated character in it. 
+We need to find the character that occurs more than once and whose index of second occurrence is smallest. 
+s contains only lowercase letters.
+Examples :
+Input: s ="geeksforgeeks"
+Output: "e"
+Explanation: 'e' repeats at third position.*/
