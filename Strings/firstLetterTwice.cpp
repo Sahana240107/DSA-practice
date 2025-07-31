ef5feef31b679eb81816696e2d1dc9bc407afd25
Sahana240107 <sahana240107@gmail.com>
2025-07-31 07:35:37 +0530

    firstLetterTwice question solved

--- Strings/firstLetterTwice.cpp
+++ Strings/firstLetterTwice.cpp
@@ -5,3 +5,25 @@ Examples :
 Input: s ="geeksforgeeks"
 Output: "e"
 Explanation: 'e' repeats at third position.*/
+class Solution {
+  public:
+    string firstRepChar(string s) {
+        int count[26]={0};
+        if(s.length()<2)
+        {
+            return "-1";
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            if(count[s[i]-97]>0)
+            {
+                return string(1,s[i]);//convert to string
+            }
+            else
+            {
+                count[s[i]-97]++;
+            }
+        }
+        return "-1";
+    }
+};
