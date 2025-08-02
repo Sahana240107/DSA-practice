d8ff99777849cc4fc9298ccade6a02639e212e4a
Sahana240107 <sahana240107@gmail.com>
2025-08-02 23:03:14 +0530

    countOccurrenceK.cpp question added

---
+++ countOccurrenceK.cpp
@@ -0,0 +1,5 @@
+/*Given an array arr[]. Your task is to return an integer denoting the total number of times digit k appears in the array.
+Examples:
+Input: k=1, arr[] = [11, 12, 13, 14, 15]
+Output: 6 
+Explanation: Here, digit 1 appears in the whole array 6 times.*/
