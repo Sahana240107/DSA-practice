cfe3ff756d20dbb1b3ca0b8a373dd9331afcb02b
Sahana240107 <sahana240107@gmail.com>
2025-08-02 23:04:12 +0530

    countOccurrenceK.cpp question solved

--- countOccurrenceK.cpp
+++ countOccurrenceK.cpp
@@ -3,3 +3,21 @@ Examples:
 Input: k=1, arr[] = [11, 12, 13, 14, 15]
 Output: 6 
 Explanation: Here, digit 1 appears in the whole array 6 times.*/
+class Solution {
+  public:
+    int num(int k, vector<int>& arr) {
+        int count=0;
+        for(int i=0;i<arr.size();i++)
+        {
+            while(arr[i]>0)
+            {
+                if(arr[i]%10==k)
+                {
+                    count++;
+                }
+                arr[i]/=10;
+            }
+        }
+        return count;
+    }
+};
