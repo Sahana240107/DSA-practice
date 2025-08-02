2d4847f7eb8702cea50e88e1d4880872b04a2d8c
Sahana240107 <sahana240107@gmail.com>
2025-08-02 23:10:30 +0530

    patterninText question added

---
+++ Strings/patterninText.cpp
@@ -0,0 +1,8 @@
+/*Given two strings text and pattern, find the first index where pattern exactly matches with any substring of text. 
+Return -1 if pattern doesn't exist as a substring in text.
+Example 1:
+Input:
+text = gffgfg
+pattern = gfg
+Output: 3
+Explanation:  Considering 0-based indexing, substring of text from 3rd to last index is gfg.*/
