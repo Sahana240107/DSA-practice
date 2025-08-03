3a0e62658d3d448aee9a101742bfe92c42daafba
Sahana240107 <sahana240107@gmail.com>
2025-08-03 23:12:29 +0530

    transitionPoint question added

--- transitionPoint.cpp
+++ transitionPoint.cpp
@@ -4,3 +4,24 @@ Examples:
 Input: arr[] = [0, 0, 0, 1, 1]
 Output: 3
 Explanation: index 3 is the transition point where 1 begins.*/
+class Solution {
+  public:
+    int transitionPoint(vector<int>& arr) {
+        // code here
+        int low=0,high=arr.size()-1,res=-1,mid;
+        while(low<=high)
+        {
+            mid=low+(high-low)/2;
+            if(arr[mid]==1)
+            {
+                res=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return res;
+    }
+};
