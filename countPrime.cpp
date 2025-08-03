4b1e4fa6ef2096420b5b9f471cfa3c34e8087ad5
Sahana240107 <sahana240107@gmail.com>
2025-08-03 23:16:37 +0530

    countPrime question solved

--- countPrime.cpp
+++ countPrime.cpp
@@ -3,3 +3,26 @@ Example 1:
 Input: n = 10
 Output: 4
 Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.*/
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n<2)
+        {
+            return 0;
+        }
+        vector<bool> prime(n,true);
+        prime[0]=prime[1]=false;
+        for(int i=2;i*i<n;i++)
+        {
+            if(prime[i])
+            {
+                for(int j=i*i;j<n;j+=i)
+                {
+                        prime[j]=false;
+                }
+
+            }
+        }
+        return count(prime.begin(),prime.end(),true);
+    }
+};
