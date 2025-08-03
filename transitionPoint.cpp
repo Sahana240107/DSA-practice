6de5b76303d82f21c942a33cf029e5573ded49d3
Sahana240107 <sahana240107@gmail.com>
2025-08-03 23:12:05 +0530

    transitionPoint question added

---
+++ transitionPoint.cpp
@@ -0,0 +1,6 @@
+/*Given a sorted array, arr[] containing only 0s and 1s, find the transition point, i.e., the first index where 1 was observed, and before that, only 0 was observed.  
+If arr does not have any 1, return -1. If array does not have any 0, return 0.
+Examples:
+Input: arr[] = [0, 0, 0, 1, 1]
+Output: 3
+Explanation: index 3 is the transition point where 1 begins.*/
