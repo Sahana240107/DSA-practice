b46998e0478bbdf926802453af956f3e2ab913ae
Sahana240107 <sahana240107@gmail.com>
2025-08-04 07:25:10 +0530

    datetoBinary question solved

--- Strings/datetoBinary.cpp
+++ Strings/datetoBinary.cpp
@@ -4,3 +4,38 @@ Return the binary representation of date.
 Example 1:
 Input: date = "2080-02-29"
 Output: "100000100000-10-11101"*/
+class Solution {
+public:
+    string toBinary(int n)
+    {
+        string s="";
+        if(n==0)
+        {
+            s='0';
+        }
+        while(n>0)
+        {
+            s=char(n%2+'0')+s;
+            n/=2;
+        }
+        return s;
+        
+    }
+    string convertDateToBinary(string date) {
+        string datestr,monthstr,yearstr,binary;
+        int day,month,year;
+        stringstream ss(date);
+        getline(ss,yearstr,'-');
+        getline(ss,monthstr,'-');
+        getline(ss,datestr,'-');
+        day=stoi(datestr);
+        month=stoi(monthstr);
+        year=stoi(yearstr);
+        datestr=toBinary(day);
+        monthstr=toBinary(month);
+        yearstr=toBinary(year);
+        binary=yearstr+"-"+monthstr+"-"+datestr;
+        return binary;
+
+    }
+};
