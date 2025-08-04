33ddb194b78383656ab15e1b45df06e02e65547e
Sahana240107 <sahana240107@gmail.com>
2025-08-04 07:03:44 +0530

    decimal toBinary question solved

--- Strings/decimaltoBinary.cpp
+++ Strings/decimaltoBinary.cpp
@@ -1,2 +1,15 @@
 /*Given a decimal number n, return its binary equivalent.
 */
+class Solution {
+  public:
+    string decToBinary(int n) {
+       string s="";
+       while(n>0)
+       {
+           s=char(n%2+'0')+s;
+           n/=2;
+       }
+       return s;
+        
+    }
+};
