f7bb19e5583c113174f9505f52f740e77dccc17b
Sahana240107 <sahana240107@gmail.com>
2025-08-05 23:02:05 +0530

    nthFiboDigit question added

---
+++ nthFiboDigit.cpp
@@ -0,0 +1,9 @@
+/*Given a positive integer N, 
+find the last digit of the Nth term from the Fibonacci series.
+Input:
+N = 14
+Output:
+7
+Explanation:
+14th Fibonacci number is 377
+It's last digit is 7*/
