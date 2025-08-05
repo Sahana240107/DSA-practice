bfb645c0d8ee18cda15b78fac97c5df39779fb37
Sahana240107 <sahana240107@gmail.com>
2025-08-05 23:02:50 +0530

    nthFiboDigit question solved

--- nthFiboDigit.cpp
+++ nthFiboDigit.cpp
@@ -7,3 +7,21 @@ Output:
 Explanation:
 14th Fibonacci number is 377
 It's last digit is 7*/
+class Solution {
+  public:
+    int fib(int N) {
+
+        long first=0,second=1,next;
+        if(N<=1)
+        {
+            return N;
+        }
+        for(int i=2;i<=N;i++)
+        {
+            next=(first+second)%10;//we just need the last digit
+            first=second;
+            second=next;
+        }
+        return (second%10);
+    }
+};
