817e3dc4fb10455af9a95f366a64606450d392be
Sahana240107 <sahana240107@gmail.com>
2025-08-17 12:52:29 +0530

    pairSumVector question added

---
+++ pairSumVector.cpp
@@ -0,0 +1,8 @@
+/*Pairs and vectors can be used together to achieve some amazing results. Here we will learn to use a vector that holds pairs.
+You are given a vector V of size n. The vector hold pair of integers. Example V={(1,2),(3,4)...}. Now, you need to sum the second elements.
+Example 1:
+Input:
+N = 5
+vector -> {(1, 2), (3, 4), (5, 6),(7, 8), (9, 10)}
+Output: 
+30*/
