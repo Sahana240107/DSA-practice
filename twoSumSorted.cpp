76a491e7662197c77044027de5682e9546c83ffa
Sahana240107 <sahana240107@gmail.com>
2025-08-17 13:03:25 +0530

    twoSumSorted solved

--- twoSumSorted.cpp
+++ twoSumSorted.cpp
@@ -5,3 +5,21 @@ Your solution must use only constant extra space.*/
 /*Input: numbers = [2,7,11,15], target = 9
 Output: [1,2]
 Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i=0,j=numbers.size()-1,sum=0;
+        while(i<j)
+        {
+            sum=numbers[i]+numbers[j];
+            if(sum==target)
+                return {i+1,j+1};
+            else if(sum<target)
+                i++;
+            else
+                j--;         
+        }
+        return {};
+    }
+};
