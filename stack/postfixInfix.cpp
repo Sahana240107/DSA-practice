9a55b9ad843c9eabfa463f922b263120857eb398
Sahana240107 <sahana240107@gmail.com>
2025-08-30 22:31:26 +0530

    postfixInfix question solved

--- stack/postfixInfix.cpp
+++ stack/postfixInfix.cpp
@@ -3,3 +3,30 @@
 ab*c+ 
 Output: 
 ((a*b)+c)*/
+// User function Template for C++
+
+class Solution {
+  public:
+    string postToInfix(string exp) {
+        // Write your code here
+        stack<string> st;
+        for(int i=0;i<exp.length();i++)
+        {
+            if(isalnum(exp[i]))
+            {
+                string s=string(1,exp[i]);
+                st.push(s);
+            }
+            else
+            {
+                string s2=st.top();
+                st.pop();
+                string s1=st.top();
+                st.pop();
+                string str='('+s1+exp[i]+s2+')';
+                st.push(str);
+            }   
+        }
+        return st.top();
+    }
+};
