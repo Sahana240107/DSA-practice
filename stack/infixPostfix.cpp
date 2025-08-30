a2464feb2f2f3d506a66541a0a0e9513b31305d7
Sahana240107 <sahana240107@gmail.com>
2025-08-30 22:15:57 +0530

    infixPostfix question added

---
+++ stack/infixPostfix.cpp
@@ -0,0 +1,4 @@
+/*Given an infix expression in the form of string s. Convert this infix expression to a postfix expression.
+Infix expression: The expression of the form a op b. When an operator is in between every pair of operands.
+Postfix expression: The expression of the form a b op. When an operator is followed for every pair of operands.
+Note: The order of */
