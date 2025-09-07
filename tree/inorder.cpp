5f7e83889f1d339894bad94acbbf45673266b1c4
Sahana240107 <sahana240107@gmail.com>
2025-09-07 19:21:38 +0530

    inorder question solved

--- tree/inorder.cpp
+++ tree/inorder.cpp
@@ -1,3 +1,36 @@
 /*Given a Binary Tree, your task is to return its In-Order Traversal.
 
-An inorder traversal first visits the left child (including its entire subtree), then visits the node, and finally visits the right child (including its entire subtree).*/
+An inorder traversal first visits the left child (including its entire subtree), then visits the node, and finally visits the right child (including its entire subtree).*//*
+// Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+     // Function to return a list containing the inorder traversal of the tree.
+    void helper(Node *root,vector<int> &res)
+    {
+        if(root==NULL)
+            return;
+        helper(root->left,res);
+        res.push_back(root->data);
+        helper(root->right,res);
+    }
+    vector<int> inOrder(Node* root) {
+        vector<int> ans;
+        helper(root,ans);
+        return ans;
+    }
+};
