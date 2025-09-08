2f9ab9cef3637f4b142cbf81236f4cc8c41d9d6e
Sahana240107 <sahana240107@gmail.com>
2025-09-08 20:04:52 +0530

    reverseList question solved

--- linked list/reverseList.cpp
+++ linked list/reverseList.cpp
@@ -1,1 +1,30 @@
 /*You are given the head of a singly linked list. You have to reverse the linked list and return the head of the reversed list.*/
+/*
+class Node {
+ public:
+    int data ;
+    Node *next ;
+
+    Node(int x) {
+        data = x ;
+        next = NULL ;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* reverseList(Node* head) {
+        // code here
+        Node *temp=head;
+        Node *prev=NULL;
+        while(temp!=NULL)
+        {
+            Node *front=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;
+        }
+        return prev;
+    }
+};
