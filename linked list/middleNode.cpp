ada91de6d4043d2790ff7d0043a060a8e4ec0f96
Sahana240107 <sahana240107@gmail.com>
2025-09-27 22:30:29 +0530

    middleNode question added

---
+++ linked list/middleNode.cpp
@@ -0,0 +1,5 @@
+/*You are given the head of a linked list, You have to return the value of the middle node of the linked list.
+
+If the number of nodes is odd, return the middle node value.
+If the number of nodes is even, there are two middle nodes, so return the second middle node value.
+*/
