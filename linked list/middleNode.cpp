73a1fe5d027ec353a162a8f3a7a827f25de7f5be
Sahana240107 <sahana240107@gmail.com>
2025-09-27 22:31:08 +0530

    middleNode question added

--- linked list/middleNode.cpp
+++ linked list/middleNode.cpp
@@ -3,3 +3,18 @@
 If the number of nodes is odd, return the middle node value.
 If the number of nodes is even, there are two middle nodes, so return the second middle node value.
 */
+int getMiddle(Node* head) {
+        if(head==NULL || head->next==NULL)
+            return head->data;
+        Node* middle=head;
+        Node* temp=head;
+        int i=0;
+        while(temp->next!=NULL)
+        {
+            if(i%2==0)
+                middle= ->next;
+            temp=temp->next;
+            i++;
+        }
+        return middle->data;
+}
