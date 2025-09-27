bcd0539d95aaf2b719d9210a52821ff5c97f38bc
Sahana240107 <sahana240107@gmail.com>
2025-09-27 22:47:27 +0530

    moveLastNodetoFront question solved

--- linked list/moveLastNodetoFront.cpp
+++ linked list/moveLastNodetoFront.cpp
@@ -1,2 +1,17 @@
 /*You are given the head of a Linked List. You have to move the last element to the front of the Linked List and return the head the modified linked list.
 */
+Node *moveToFront(Node *head) {
+        if(head==NULL || head->next==NULL)
+        return head;
+        Node* prev=NULL;
+        Node* temp=head;
+        while(temp->next!=NULL)
+        {
+            prev=temp;
+            temp=temp->next;
+        }
+        prev->next=NULL;
+        temp->next=head;
+        head=temp;
+        return head;
+    }
