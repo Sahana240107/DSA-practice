e989667296debe2e70a6914697aea2a8515d0f15
Sahana240107 <sahana240107@gmail.com>
2025-09-28 23:12:49 +0530

    addNum question added

--- linked list/addNum.cpp
+++ linked list/addNum.cpp
@@ -3,3 +3,58 @@
 Note: There can be leading zeros in the input lists, but there should not be any leading zeros in the output list.
 
 */
+class Solution {
+  public:
+    Node* reverse(Node *head)
+    {
+        Node* temp=head;
+        Node *prev=NULL;
+        while(temp!=NULL)
+        {
+            Node *front=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;
+        }
+        return prev;
+    }
+    Node* removeZeros(Node* head)
+    {
+        while(head!=NULL && head->data==0)
+        {
+            head=head->next;
+        }
+        if(head==NULL)
+            return new Node(0);
+        return head;
+    }
+    Node* addTwoLists(Node* head1, Node* head2) {
+        head1=reverse(head1);
+        head2=reverse(head2);
+        Node dummy(0);
+        Node *current=&dummy;
+        int sum=0,carry=0;
+        while(head1!=NULL || head2!=NULL || carry!=0)
+        {
+            sum=0;
+            if(head1!=NULL)
+            {
+                sum+=head1->data;
+                head1=head1->next;
+            }
+            if(head2!=NULL)
+            {
+                sum+=head2->data;
+                head2=head2->next;
+            }
+            sum+=carry;
+            carry=sum/10;
+            current->next=new Node(sum%10);
+            current=current->next;
+        }
+        dummy.next=reverse(dummy.next);
+        dummy.next=removeZeros(dummy.next);
+        return dummy.next;
+        
+    }
+};
