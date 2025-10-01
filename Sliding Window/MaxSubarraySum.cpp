33fb6a44aaceace87caed796cffe7f2a1912f0eb
Sahana240107 <sahana240107@gmail.com>
2025-10-01 22:08:41 +0530

    MaxSubarraySum question added

--- Sliding Window/MaxSubarraySum.cpp
+++ Sliding Window/MaxSubarraySum.cpp
@@ -7,3 +7,21 @@ Examples:
 Input: arr[] = [100, 200, 300, 400], k = 2
 Output: 700
 Explanation: arr2 + arr3 = 700, which is maximum.*/
+class Solution {
+  public:
+    int maxSubarraySum(vector<int>& arr, int k) {
+        // code here
+        long maxSum=0,subSum=0;
+        for(int i=0;i<k;i++)
+        {
+            subSum+=arr[i];
+        }
+        maxSum=subSum;
+        for(int i=k;i<arr.size();i++)
+        {
+            subSum+=arr[i]-arr[i-k];
+            maxSum=max(maxSum,subSum);
+        }
+        return maxSum;
+    }
+};
