fd62b95def40a67cd5f719a10540977bef560501
Sahana240107 <sahana240107@gmail.com>
2025-10-03 22:43:09 +0530

    firstNegative question added

---
+++ Sliding Window/firstNegative.cpp
@@ -0,0 +1,14 @@
+/*Given an array arr[]  and a positive integer k, find the first negative integer for each and every window(contiguous subarray) of size k.
+
+Note: If a window does not contain a negative integer, then return 0 for that window.
+
+Examples:
+
+Input: arr[] = [-8, 2, 3, -6, 10] , k = 2
+Output: [-8, 0, -6, -6]
+Explanation:
+Window [-8, 2] First negative integer is -8.
+Window [2, 3] No negative integers, output is 0.
+Window [3, -6] First negative integer is -6.
+Window [-6, 10] First negative integer is -6.
+*/
