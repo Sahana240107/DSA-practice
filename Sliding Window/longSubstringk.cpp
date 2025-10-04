c920f995759b5384ed57b2b3e65967885b152ad7
Sahana240107 <sahana240107@gmail.com>
2025-10-04 22:57:38 +0530

    longSubstringk question solved

--- Sliding Window/longSubstringk.cpp
+++ Sliding Window/longSubstringk.cpp
@@ -8,3 +8,24 @@ Input:s = "aabacbebebe", k = 3
 Output: 7
 Explanation: The longest substring with exactly 3 distinct characters is "cbebebe", which includes 'c', 'b', and 'e'.
 ```*/
+class Solution {
+  public:
+    int longestKSubstr(string &s, int k) {
+        int maxlength=-1,start=0;
+        unordered_map<char,int> freq;
+        for(int end=0;end<s.length();end++)
+        {
+            freq[s[end]]++;
+            if(freq.size()==k)
+                maxlength=max(maxlength,end-start+1);
+            while(freq.size()>k)
+            {
+                freq[s[start]]--;
+                if(freq[s[start]]==0)
+                    freq.erase(s[start]);
+                start++;
+            }
+        }
+        return maxlength;
+    }
+};
