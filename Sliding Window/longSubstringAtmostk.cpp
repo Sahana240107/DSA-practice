a2ca859c338197ad831e8164ddcabf17db60ef9b
Sahana240107 <sahana240107@gmail.com>
2025-10-04 23:00:37 +0530

    longSubstringAtmostk question solved

--- Sliding Window/longSubstringAtmostk.cpp
+++ Sliding Window/longSubstringAtmostk.cpp
@@ -6,3 +6,26 @@ Output: 3
 Explanation: The substring is "ece".
 
 */
+int longDistinct(string s, int k) {
+    int start = 0, maxCount = 0;
+    unordered_map<char,int> freq;
+
+    for(int end = 0; end < s.length(); end++) {
+        freq[s[end]]++;
+
+        // Shrink window if distinct characters > k
+        while(freq.size() > k) {
+            freq[s[start]]--;
+            if(freq[s[start]] == 0) {
+                freq.erase(s[start]);
+            }
+            start++;
+        }
+
+        // Update maximum length
+        maxCount = max(maxCount, end - start + 1);
+    }
+
+    return maxCount;
+}
+
