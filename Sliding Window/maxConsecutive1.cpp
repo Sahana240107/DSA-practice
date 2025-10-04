e530d937c8a5188ac30caf8c50ab3d338bf40489
Sahana240107 <sahana240107@gmail.com>
2025-10-04 22:52:29 +0530

    maxConsecutive1 question added

---
+++ Sliding Window/maxConsecutive1.cpp
@@ -0,0 +1,8 @@
+/*Given a binary array nums and an integer k, 
+return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+Example 1:
+
+Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+Output: 6
+Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+*/
