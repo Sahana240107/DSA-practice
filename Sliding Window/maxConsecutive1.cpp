37e9338ec87cb705af1908c86d9957facaf2e4cf
Sahana240107 <sahana240107@gmail.com>
2025-10-04 22:53:50 +0530

    maxConsecutive1 question solved

--- Sliding Window/maxConsecutive1.cpp
+++ Sliding Window/maxConsecutive1.cpp
@@ -6,3 +6,22 @@ Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
 Output: 6
 Explanation: [1,1,1,0,0,1,1,1,1,1,1]
 */
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int zero_count=0,max1=0,start=0;
+        for(int end=0;end<nums.size();end++)
+        {
+            if(nums[end]==0)
+                zero_count++;
+            while(zero_count>k)
+            {
+                if(nums[start]==0)
+                    zero_count--;
+                start++;
+            }
+            max1=max(max1,end-start+1);
+        }
+        return max1;
+    }
+};
