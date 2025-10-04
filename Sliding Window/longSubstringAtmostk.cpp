3ed8941d75e09d050ab3d9a6b0c0497665c66725
Sahana240107 <sahana240107@gmail.com>
2025-10-04 23:00:05 +0530

    longSubstringAtmostk question added

---
+++ Sliding Window/longSubstringAtmostk.cpp
@@ -0,0 +1,8 @@
+/*Given a string `s` and an integer `k`, find the length of the **longest substring** that contains **at most k distinct characters**.
+
+**Example 1:**
+Input: s = "eceba", k = 2
+Output: 3
+Explanation: The substring is "ece".
+
+*/
