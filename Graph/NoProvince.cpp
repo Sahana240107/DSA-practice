556aac9289f2735dde91a419f5f2d4101500c490
Sahana240107 <sahana240107@gmail.com>
2025-10-05 16:21:52 +0530

    NoProvince question solved

--- Graph/NoProvince.cpp
+++ Graph/NoProvince.cpp
@@ -8,3 +8,31 @@ Output:
 Explanation:
 The graph clearly has 2 Provinces [1,3] and [2]. As city 1 and city 3 has a path between them they belong to a single province. City 2 has no path to city 1 or city 3 hence it belongs to another province.
 */
+// User function Template for C++
+
+class Solution {
+    void dfs(int node,vector<vector<int>>& adj,vector<bool>& visited)
+    {
+        visited[node]=true;
+        for(int i=0;i<adj[node].size();i++)
+        {
+            if(adj[node][i]==1 && !visited[i])
+                dfs(i,adj,visited);
+        }
+    }
+  public:
+    int numProvinces(vector<vector<int>> adj, int V) {
+        int count=0;
+        vector<bool> visited(adj.size(),false);
+        for(int i=0;i<V;i++)
+        {
+            if(!visited[i])
+            {
+                count++;
+                dfs(i,adj,visited);
+            }
+        }
+        return count;
+        
+    }
+};
