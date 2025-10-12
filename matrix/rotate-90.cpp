8bc048e3795567cefb4590fefab707393b45ef4a
Sahana240107 <sahana240107@gmail.com>
2025-10-12 19:37:07 +0530

    rotate 90 degrees anticlockwise question added

--- matrix/rotate-90.cpp
+++ matrix/rotate-90.cpp
@@ -1,9 +1,10 @@
-/*Given a square mat[][]. The task is to rotate it by 90 degrees in clockwise direction without using any extra space.
+/*Given a square matrix mat[][] of size n x n. The task is to rotate it by 90 degrees in an anti-clockwise direction without using any extra space. 
 
 Examples:
 
-Input: mat[][] = [[1 2 3], [4 5 6], [7 8 9]]
-Output:
-7 4 1 
-8 5 2
-9 6 3*/
+Input: mat[][] = [[0, 1, 2], 
+                [3, 4, 5], 
+                [6, 7, 8]] 
+Output: [[2, 5, 8],
+        [1, 4, 7],
+        [0, 3, 6]]*/
