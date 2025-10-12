b0588a08eee3061d9d29a68b9cdf5c269b87b623
Sahana240107 <sahana240107@gmail.com>
2025-10-12 19:57:23 +0530

    snake pattern question added

---
+++ matrix/snakepattern.cpp
@@ -0,0 +1,9 @@
+/*Given a matrix mat[][] of size n x n. Print the elements of the matrix in the snake like pattern depicted below.
+
+
+Examples :
+
+Input: n = 3, mat[][] = [[45, 48, 54], [21, 89, 87], [70, 78, 15]]
+Output: [45, 48, 54, 87, 89, 21, 70, 78, 15] 
+Explanation: Printing it in snake pattern will lead to the output as [45, 48, 54, 87, 89, 21, 70, 78, 15] 
+.*/
