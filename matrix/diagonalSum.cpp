759bb7873b77d218cf5e7bcf5c01981ffceb4bac
Sahana240107 <sahana240107@gmail.com>
2025-10-12 20:16:23 +0530

    diagonalSum question solved

--- matrix/diagonalSum.cpp
+++ matrix/diagonalSum.cpp
@@ -2,3 +2,21 @@
 
 Only include the sum of all the elements on the primary diagonal and all the
  elements on the secondary diagonal that are not part of the primary diagonal.*/
+ class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=mat[i][i];//primary diagonal
+            sum+=mat[i][n-1-i];//secondary diagonal
+        }
+        if(n%2!=0)
+        {
+            sum-=mat[n/2][n/2];//remove center counted twice
+        }
+        return sum;
+    }
+};
