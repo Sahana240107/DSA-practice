1852a61ceaabc7c82f40fdd0775434b669348bb7
Sahana240107 <sahana240107@gmail.com>
2025-10-13 22:04:33 +0530

    longSubstring question solved using freq map

--- Sliding Window/longSubstring.cpp
+++ Sliding Window/longSubstring.cpp
@@ -6,3 +6,24 @@ Input: s = "abcabcbb"
 Output: 3
 Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
 */
+//Using frequency map
+int lengthOfLongestSubstring(string s) {
+    unordered_map<char, int> freq;
+    int start = 0, maxLen = 0;
+
+    for (int end = 0; end < s.length(); end++) {
+        freq[s[end]]++;
+
+        while (freq[s[end]] > 1) {
+            freq[s[start]]--;
+            if (freq[s[start]] == 0) freq.erase(s[start]);
+            start++;
+        }
+
+        maxLen = max(maxLen, end - start + 1);
+    }
+    return maxLen;
+}
+
+
+
