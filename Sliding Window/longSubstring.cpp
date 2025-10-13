87d5a0337261d197031cb240305791f80d984c48
Sahana240107 <sahana240107@gmail.com>
2025-10-13 22:04:58 +0530

    longSubstring question solved using index map

--- Sliding Window/longSubstring.cpp
+++ Sliding Window/longSubstring.cpp
@@ -26,4 +26,23 @@ int lengthOfLongestSubstring(string s) {
 }
 
 
+//Using index map (last occurrence)
+int lengthOfLongestSubstring(string s) {
+    unordered_map<char, int> lastIndex;
+    int start = 0, maxLen = 0;
+
+    for (int end = 0; end < s.length(); end++) {
+        char c = s[end];
+
+        // if char seen before and inside current window
+        if (lastIndex.find(c) != lastIndex.end() && lastIndex[c] >= start) {
+            start = lastIndex[c] + 1;
+        }
+
+        lastIndex[c] = end;
+        maxLen = max(maxLen, end - start + 1);
+    }
+
+    return maxLen;
+}
 
