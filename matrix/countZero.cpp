3f41fdc03cee1555ff1af8a4cafb526c6e453dcc
Sahana240107 <sahana240107@gmail.com>
2025-10-16 21:40:10 +0530

    countZero question added

---
+++ matrix/countZero.cpp
@@ -0,0 +1,13 @@
+/*Given a N X N binary Square Matrix where each row and column of the matrix is sorted in ascending order. Find the total number of zeros present in the matrix.
+
+Example 1:
+
+Input:
+N = 3
+A = {{0, 0, 0},
+     {0, 0, 1},
+     {0, 1, 1}}
+Output: 6
+Explanation: 
+The first, second and third row contains 3, 2 and 1
+zeroes respectively.*/
