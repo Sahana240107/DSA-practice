a5aacbba2fb56eba1e0a003ba797ab466fc8eb84
Sahana240107 <sahana240107@gmail.com>
2025-10-16 22:22:40 +0530

    difDiagonal question added

---
+++ matrix/difDiagonal.cpp
@@ -0,0 +1,13 @@
+/*Given a matrix Grid[][] of size NxN. Calculate the absolute difference between the sums of its diagonals.
+
+Example 1:
+
+Input:
+N=3
+Grid=[[1,2,3],[4,5,6],[7,8,9]]
+Output: 
+0
+Explanation:
+Sum of primary diagonal = 1+5+9 = 15.
+Sum of secondary diagonal = 3+5+7 = 15.
+Difference = |15 - 15| = 0.*/
