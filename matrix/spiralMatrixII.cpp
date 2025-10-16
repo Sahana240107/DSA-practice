da9fdd0f2c4816da49706e79c0fe2f3adb3d4c84
Sahana240107 <sahana240107@gmail.com>
2025-10-16 21:28:45 +0530

    spiralMatrixII question solved

--- matrix/spiralMatrixII.cpp
+++ matrix/spiralMatrixII.cpp
@@ -4,3 +4,34 @@ Example 1:
 
 Input: n = 3
 Output: [[1,2,3],[8,9,4],[7,6,5]]*/
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+         vector<vector<int>> ans(n,vector<int>(n));
+         int count=1;
+         int top=0,bottom=n-1;
+         int left=0,right=n-1;
+         while(top<=bottom && left<=right)
+         {
+            for(int j=left;j<=right;j++)
+                ans[top][j]=count++;
+            top++;
+            for(int i=top;i<=bottom;i++)
+                ans[i][right]=count++;
+            right--;
+            if(top<=bottom)
+            {
+                for(int j=right;j>=left;j--)
+                    ans[bottom][j]=count++;
+                bottom--;
+            }
+            if(left<=right)
+            {
+                for(int i=bottom;i>=top;i--)
+                    ans[i][left]=count++;
+                left++;
+            }
+         }
+         return ans;
+    }
+};
