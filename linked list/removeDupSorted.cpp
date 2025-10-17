1a03958ffb7262c92afff210aa5d76a678a6b7b2
Sahana240107 <sahana240107@gmail.com>
2025-10-17 19:27:09 +0530

    removeDupSorted question solved

--- linked list/removeDupSorted.cpp
+++ linked list/removeDupSorted.cpp
@@ -4,3 +4,21 @@ Example 1:
 
 Input: head = [1,1,2]
 Output: [1,2]*/
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* prev=NULL;
+        ListNode* temp=head;
+        while(temp!=NULL)
+        {
+            if(prev && prev->val==temp->val)
+            {
+                prev->next=temp->next;
+            }
+            else//move prev only when it is not a duplicate
+                prev=temp;
+            temp=temp->next;
+        }
+        return head;
+    }
+};
