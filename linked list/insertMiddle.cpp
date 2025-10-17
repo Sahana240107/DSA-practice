794bc05d00b12b042641afe5a1c52784f26921fc
Sahana240107 <sahana240107@gmail.com>
2025-10-17 22:52:24 +0530

    insertMiddle question solved

--- linked list/insertMiddle.cpp
+++ linked list/insertMiddle.cpp
@@ -12,3 +12,22 @@ Output: 10->20->30->40->50
 Explanation: 
 
 The new element is inserted after the current middle element in the linked list and Hence, the output is 10->20->30->40->50.*/
+class Solution {
+  public:
+    Node *insertInMiddle(Node *head, int x) {
+        // Code Here
+        Node* p=new Node(x);
+        if(head==NULL)
+            return p;
+        Node* slow=head;
+        Node* fast=head->next;//for even length to stop at first middle
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        p->next=slow->next;
+        slow->next=p;
+        return head;
+    }
+};
