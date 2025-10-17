f84152dbd418d6278845b43225f5aefbee8980a9
Sahana240107 <sahana240107@gmail.com>
2025-10-17 21:48:05 +0530

    insertEnd question solved

--- linked list/insertEnd.cpp
+++ linked list/insertEnd.cpp
@@ -8,3 +8,19 @@ Input: x = 6,
 Output: 1 -> 2 -> 3 -> 4 -> 5 -> 6
 Explanation: We can see that 6 is inserted at the end of the linkedlist.
    */
+   class Solution {
+  public:
+    Node *insertAtEnd(Node *head, int x) {
+        // Code here
+        Node *newNode=new Node(x);
+        if(head==NULL)
+         return newNode;
+        Node* temp=head;
+        while(temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next=newNode;
+        return head;
+    }
+};
