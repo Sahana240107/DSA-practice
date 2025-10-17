a62cfe41d5baa53f3d47a74238c4639d14591914
Sahana240107 <sahana240107@gmail.com>
2025-10-17 19:34:08 +0530

    insertEnd question added

---
+++ linked list/insertEnd.cpp
@@ -0,0 +1,10 @@
+/*You are given the head of a Singly Linked List and a value x, insert that value x at the end of the LinkedList 
+and return the head of the modified Linked List.
+
+Examples :
+
+Input: x = 6,
+   
+Output: 1 -> 2 -> 3 -> 4 -> 5 -> 6
+Explanation: We can see that 6 is inserted at the end of the linkedlist.
+   */
