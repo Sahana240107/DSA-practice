5f22af2aee9b4514a515ade9dfdd12e2d2c061f9
Sahana240107 <sahana240107@gmail.com>
2025-10-18 17:04:20 +0530

    delAlt question solved

--- linked list/delAlt.cpp
+++ linked list/delAlt.cpp
@@ -8,3 +8,17 @@ Output: 1->3->5
 
 Explanation: Deleting alternate nodes ie 2, 4, 6 results in the linked list with elements 1->3->5.
 */
+class Solution {
+  public:
+    void deleteAlt(struct Node *head) {
+        // Code here
+        Node* temp=head;  
+        while(temp!=NULL&& temp->next!=NULL)
+        {
+            Node* toDelete=temp->next;
+            temp->next=toDelete->next;
+            delete toDelete;
+            temp=temp->next;
+        }
+    }
+};
