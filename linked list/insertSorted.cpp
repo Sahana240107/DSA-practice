889c4a2ed60b555dc27f84ab23ff87c66aa51271
Sahana240107 <sahana240107@gmail.com>
2025-10-18 15:27:00 +0530

    insertSorted question added

---
+++ linked list/insertSorted.cpp
@@ -0,0 +1,12 @@
+/*Given a linked list sorted in ascending order and an integer called key, insert data in the linked list such that the list remains sorted.
+
+Examples:
+
+Input: LinkedList: 25->36->47->58->69->80, key: 19
+Output: 19->25->36->47->58->69->80
+
+Explanation: After inserting 19 the sorted linked list will look like the one in the output.
+Input: LinkedList: 50->100, key: 75
+Output: 50->75->100
+
+Explanation: After inserting 75 the sorted linked list will look like the one in the output*/
