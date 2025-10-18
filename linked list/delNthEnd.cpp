34a732dcc912124b0537e6e2763dadaa12bbfb94
Sahana240107 <sahana240107@gmail.com>
2025-10-18 22:41:49 +0530

    delNthEnd question solved

--- linked list/delNthEnd.cpp
+++ linked list/delNthEnd.cpp
@@ -8,3 +8,26 @@ Example 2:
 Input: head = [1], n = 1
 Output: []
 */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* first=head;
+        for(int i=1;i<=n;i++)
+            first=first->next;
+        if(first==NULL)//if n==length that is to delete head
+        {
+            head=head->next;
+            return head;
+        }
+        ListNode* second=head;
+        while(first->next!=NULL)//to get previous node of nth node from end
+        {
+            first=first->next;
+            second=second->next;
+        }
+        ListNode* toDelete=second->next;
+        second->next=toDelete->next;
+        delete toDelete;
+        return head;
+    }
+};
