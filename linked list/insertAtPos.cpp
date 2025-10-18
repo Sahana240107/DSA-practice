ab2d5116605ad9bf30d8111cdb2860625a0e970a
Sahana240107 <sahana240107@gmail.com>
2025-10-18 14:04:12 +0530

    insertAtPos question solved

--- linked list/insertAtPos.cpp
+++ linked list/insertAtPos.cpp
@@ -10,3 +10,27 @@ Input: pos = 2, val = 5,
     
 Output: 1 -> 5 -> 2 -> 9
 */
+class Solution {
+  public:
+    Node *insertPos(Node *head, int pos, int val) {
+        Node* newNode=new Node(val);
+        if(head==NULL)
+            return newNode;
+        if(pos==1)
+        {
+            newNode->next=head;
+            head=newNode;
+            return head;
+        }
+        int i=1;
+        Node* temp=head;
+        while(temp!=NULL && i<pos-1)
+        {
+            temp=temp->next;
+            i++;
+        }
+        newNode->next=temp->next;
+        temp->next=newNode;
+        return head;
+    }
+};
