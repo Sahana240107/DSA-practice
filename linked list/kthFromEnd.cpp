7543718dd9b1b1dac973caa774b08951ea9a813d
Sahana240107 <sahana240107@gmail.com>
2025-10-18 16:17:18 +0530

    KthFromEnd question added

---
+++ linked list/kthFromEnd.cpp
@@ -0,0 +1,13 @@
+/*You are given the head of a linked list and the number k, You have to return the kth node from the end of linkedList. If k is more than the number of nodes, then the return -1.
+
+Examples
+
+Input: LinkedList: 1->2->3->4->5->6->7->8->9, k = 2
+Output: 8
+Explanation: The given linked list is 1->2->3->4->5->6->7->8->9. The 2nd node from end is 8.
+
+Input: LinkedList: 10->5->100->5, k = 5
+Output: -1
+Explanation: The given linked list is 10->5->100->5. Since 'k' is more than the number of nodes, the output is -1.
+
+*/
