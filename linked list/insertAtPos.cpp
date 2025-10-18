ee6169ed78067dc2151d7f9db70e3318ff2fc25d
Sahana240107 <sahana240107@gmail.com>
2025-10-18 14:02:48 +0530

    insertAtPos question added

---
+++ linked list/insertAtPos.cpp
@@ -0,0 +1,12 @@
+/*You are given the head of a Singly Linked List, a position pos and value val. 
+Insert the val data at the given position (1-based index) of the LinkedList and return the head of the modified Linked List.
+
+Examples:
+
+Input: pos = 3, val = 4,
+    
+Output: 1 -> 3 -> 4
+Input: pos = 2, val = 5,
+    
+Output: 1 -> 5 -> 2 -> 9
+*/
