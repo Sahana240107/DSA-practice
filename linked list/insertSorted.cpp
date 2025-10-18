39d25aeed3b852c7beaa5b8b5469245f35f5b5c6
Sahana240107 <sahana240107@gmail.com>
2025-10-18 15:27:28 +0530

    insertSorted question solved

--- linked list/insertSorted.cpp
+++ linked list/insertSorted.cpp
@@ -10,3 +10,29 @@ Input: LinkedList: 50->100, key: 75
 Output: 50->75->100
 
 Explanation: After inserting 75 the sorted linked list will look like the one in the output*/
+class Solution {
+  public:
+    // Should return head of the modified linked list
+    Node* sortedInsert(Node* head, int key) {
+        // Code here
+        Node* p=new Node(key);
+        if(!head)
+            return p;
+        if(head->data>key)
+        {
+            p->next=head;
+            head=p;
+            return head;
+        }
+        Node* temp=head;
+        Node* prev=NULL;
+        while(temp!=NULL && temp->data<key)
+        {
+            prev=temp;
+            temp=temp->next;
+        }
+        prev->next=p;
+        p->next=temp;
+        return head;
+    }
+};
