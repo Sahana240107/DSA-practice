68bc9bac072c9ee67e15589f5ff9d3b8bc2f8627
Sahana240107 <sahana240107@gmail.com>
2025-10-21 23:01:09 +0530

    delAtP question added

---
+++ linked list/delAtP.cpp
@@ -0,0 +1,12 @@
+/*You are given the head of a singly linked list and an integer x. Delete the xth node (1-based indexing) from the singly linked list.
+
+Examples:
+
+Input: x = 1,
+    
+Output: 2 -> 3 -> 1 -> 7
+
+Input: x = 5,
+    
+Output: 2 -> 3 -> 4 -> 5
+    */
