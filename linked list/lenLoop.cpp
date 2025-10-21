c43eef3a21eade3fb43fa17dc3b8d4773a285dd5
Sahana240107 <sahana240107@gmail.com>
2025-10-21 22:51:15 +0530

    lenLoop question solved

--- linked list/lenLoop.cpp
+++ linked list/lenLoop.cpp
@@ -1,3 +1,29 @@
 /*Given the head of a linked list, determine whether the list contains a loop.
  If a loop is present, return the number of nodes in the loop, otherwise return 0.
 */
+class Solution {
+  public:
+    int lengthOfLoop(Node *head) {
+        // code here
+        Node* slow=head;
+        Node* fast=head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+                break;
+        }
+        if(fast==NULL || fast->next==NULL)
+            return 0;
+        int n=1;
+        Node* temp=slow->next;
+        while(temp!=slow)
+        {
+            n++;
+            temp=temp->next;
+        }
+        return n;
+        
+    }
+};
