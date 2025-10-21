07e576ead227dec73417541a6662c8b6ad94beb3
Sahana240107 <sahana240107@gmail.com>
2025-10-21 22:29:04 +0530

    compareLL question solved

--- linked list/compareLL.cpp
+++ linked list/compareLL.cpp
@@ -10,3 +10,27 @@ Input: head1 = a->b->a->b->a , head2 = a->b->a->a
 Output: 1
 Explanation: String consisting of nodes of first linked list is lexicographically greater than the second one. So, the result is 1.
 */
+class Solution {
+  public:
+    int compare(Node *head1, Node *head2) {
+        // Code Here
+        Node* t1=head1;
+        Node* t2=head2;
+        bool flag=true;
+        while(t1 && t2)
+        {
+            if(t1->data >t2->data)
+                return 1;
+            else if(t1->data < t2->data)
+                return -1;
+            t1=t1->next;
+            t2=t2->next;
+        }
+        if(!t1 && !t2)
+            return 0;
+        if(!t1)
+            return -1;
+        else
+            return 1;
+    }
+};
