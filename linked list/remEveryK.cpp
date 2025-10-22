0f62160cdcfe2cc73ec4c353fb4df7058449f805
Sahana240107 <sahana240107@gmail.com>
2025-10-22 10:32:47 +0530

    remEveryK question solved

--- linked list/remEveryK.cpp
+++ linked list/remEveryK.cpp
@@ -10,3 +10,31 @@ Input: Linked list: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10, k = 3
 Output: 1 -> 2 -> 4 -> 5 -> 7 -> 8 -> 10
 
 Explanation: After removing every 3rd node of the linked list, the resultant linked list will be: 1 -> 2 -> 4 -> 5 -> 7 -> 8 -> 10.*/
+
+class Solution {
+  public:
+    Node* deleteK(Node* head, int k) {
+        // code here
+        if(head==NULL || k==0)
+            return
+        if(k==1)
+            return NULL;
+        int i=1;
+        Node* temp=head;
+        Node* prev=NULL;
+        while(temp!=NULL)
+        {
+         if(i%k==0)
+         {
+             prev->next=temp->next;
+         }
+         else
+         {
+             prev=temp;
+         }
+         i++;
+         temp=temp->next;
+        }
+        return head;
+    }
+};
