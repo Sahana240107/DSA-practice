e977e766b152297802c45814d8638b2907b5d56f
Sahana240107 <sahana240107@gmail.com>
2025-10-22 21:45:30 +0530

    minBST question added

---
+++ tree/minBST.cpp
@@ -0,0 +1,9 @@
+/*Given the root of a Binary Search Tree. Your task is to find the minimum element in this given BST.
+
+Examples
+
+Input: root = [5, 4, 6, 3, N, N, 7, 1]
+ex-1
+Output: 1
+Explanation: The minimum element in the given BST is 1.
+*/
