8fa88f25363c378139308557e692f6ca898f876a
Sahana240107 <sahana240107@gmail.com>
2025-10-22 21:03:52 +0530

    symmetricTree question solved

--- tree/symmetricTree.cpp
+++ tree/symmetricTree.cpp
@@ -6,3 +6,21 @@ Example 2:
 Input: root = [1,2,2,null,3,null,3]
 Output: false
 */
+class Solution {
+    bool helper(TreeNode* t1,TreeNode* t2)
+    {
+        if(!t1 && !t2)
+            return true;
+        else if(!t1 || !t2)
+            return false;
+        else if(t1->val !=t2->val)
+            return false;
+        return helper(t1->left,t2->right)&& helper(t1->right,t2->left);
+    }
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(!root)
+            return true;
+        return helper(root->left,root->right);
+    }
+};
