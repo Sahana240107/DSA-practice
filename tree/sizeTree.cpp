e9cb0e46c0945a42f399102f35764f14ec500b0e
Sahana240107 <sahana240107@gmail.com>
2025-10-22 18:55:42 +0530

    sizeTree question solved

--- tree/sizeTree.cpp
+++ tree/sizeTree.cpp
@@ -9,3 +9,12 @@ Input:
 Output: 3
 Explanation: There are three nodes in given binary tree.
 */
+class Solution {
+  public:
+    int getSize(Node* node) {
+        int n=0;
+        if(node==NULL)
+            return 0;
+        return 1+getSize(node->left)+getSize(node->right)
+    }
+};
