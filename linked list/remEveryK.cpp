e0c03053444b80d58fc2562c5c54ac40c24e8010
Sahana240107 <sahana240107@gmail.com>
2025-10-22 10:30:53 +0530

    remEveryK question added

---
+++ linked list/remEveryK.cpp
@@ -0,0 +1,12 @@
+/*Given a singly linked list, your task is to remove every kth node from the linked list. 
+
+Examples:
+
+Input: Linked list: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8, k = 2
+Output: 1 -> 3 -> 5 -> 7
+
+Explanation: After removing every 2nd node of the linked list, the resultant linked list will be: 1 -> 3 -> 5 -> 7.
+Input: Linked list: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10, k = 3
+Output: 1 -> 2 -> 4 -> 5 -> 7 -> 8 -> 10
+
+Explanation: After removing every 3rd node of the linked list, the resultant linked list will be: 1 -> 2 -> 4 -> 5 -> 7 -> 8 -> 10.*/
