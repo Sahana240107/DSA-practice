f398c5dfcf3a54a727ce9cf811c33ca10458240d
Sahana240107 <sahana240107@gmail.com>
2025-10-22 18:55:08 +0530

    sizeTree question added

---
+++ tree/sizeTree.cpp
@@ -0,0 +1,11 @@
+/*Given a binary tree, you have to return the size of it. Size of binary tree is defined as number of nodes in it.
+
+Examples:
+
+Input:      
+       1
+     /  \
+    2    3
+Output: 3
+Explanation: There are three nodes in given binary tree.
+*/
