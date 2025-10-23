e69e230e5a56d27cc9f19ed809e6a8720644dd91
Sahana240107 <sahana240107@gmail.com>
2025-10-23 18:16:46 +0530

    1stLastPosition question solved

--- Binary Search/1stLastPosition.cpp
+++ Binary Search/1stLastPosition.cpp
@@ -12,3 +12,47 @@ Example 2:
 
 Input: nums = [5,7,7,8,8,10], target = 6
 Output: [-1,-1]*/
+class Solution {
+    int findFirst(vector<int>& num,int target)
+    {
+        int low=0,high=num.size()-1,first=-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(num[mid]==target)
+            {
+                first=mid;
+                high=mid-1;
+            }
+            else if(num[mid]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return first;
+    }
+    int findLast(vector<int>& num,int target)
+    {
+        int low=0,high=num.size()-1,last=-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(num[mid]==target)
+            {
+                last=mid;
+                low=mid+1;
+            }
+            else if(num[mid]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return last;
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first=findFirst(nums,target);
+        int last=findLast(nums,target);
+        return {first,last};
+    }
+};
