fbfc75b0a9df3d74792136a8f2ea7fe9359bce3a
Sahana240107 <sahana240107@gmail.com>
2025-11-24 12:55:06 +0530

    MaxSumLessthanK question added

---
+++ Sliding Window/MaxSumLessthanK.cpp
@@ -0,0 +1,8 @@
+/*Given an array arr[] of integers and a number x, the task is to find the sum of subarray having a maximum sum less than or equal to the given value of x.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4, 5], x = 11 
+Output: 10
+Explanation: Subarray having maximum sum is [1, 2, 3, 4].
+*/
