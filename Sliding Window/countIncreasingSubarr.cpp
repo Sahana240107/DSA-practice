416717fa85ffdcd9ae54517a23e3abe3d2ad8a61
Sahana240107 <sahana240107@gmail.com>
2025-11-24 13:55:28 +0530

    CountIncreasingSubarr question added

---
+++ Sliding Window/countIncreasingSubarr.cpp
@@ -0,0 +1,8 @@
+/*Given an array arr[] of integers, count the number of subarrays in arr[] which are strictly increasing with size greater or equal to 2. A subarray is a contiguous part of array. A subarray is strictly increasing if each element is greater then it's previous element if it exists.
+
+Examples:
+
+Input: arr[] = [1, 3, 3, 2, 3, 5]
+Output: 4
+Explanation: Increasing Subarrays are : arr[0,1], arr[3,4], arr[3,5], arr[4,5].
+*/
