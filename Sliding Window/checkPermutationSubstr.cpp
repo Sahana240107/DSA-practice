73043c8d6ee3f96999bd0bedfb2e55d4b581a078
Sahana240107 <sahana240107@gmail.com>
2025-11-24 15:34:12 +0530

    checkPermutationSubstr question added

---
+++ Sliding Window/checkPermutationSubstr.cpp
@@ -0,0 +1,11 @@
+/*Given two strings txt and pat having lowercase letters, the task is to check if any permutation of pat is a substring of txt.
+
+Examples:
+
+Input: txt = "geeks", pat = "eke"
+Output: true
+Explanation: "eek" is a permutation of "eke" which exists in "geeks".
+Input: txt = "programming", pat = "rain"
+Output: false
+Explanation: No permutation of "rain" exists as a substring in "programming".
+*/
