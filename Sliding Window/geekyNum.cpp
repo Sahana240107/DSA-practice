a765962a3240c13bdf6251cd7dbcc5223f310f65
Sahana240107 <sahana240107@gmail.com>
2025-11-24 12:34:06 +0530

    geekyNum question added

---
+++ Sliding Window/geekyNum.cpp
@@ -0,0 +1,17 @@
+/*Geek just learned about Fibonacci numbers.
+
+The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, ...
+where the next number is found by adding up the two numbers before it.
+
+He defines a new series called Geeky numbers. Here the next number is the sum of the K preceding numbers.
+You are given an array of size K, GeekNum[ ], where the ith element of the array represents the ith Geeky number. Return its Nth term.
+
+Input:
+N = 5, K = 3
+GeekNum[] = {0, 1, 2}
+Output: 
+6
+Explanation: 
+Terms are 0, 1, 2, 3, 6.
+So the 5th term is 6
+*/
