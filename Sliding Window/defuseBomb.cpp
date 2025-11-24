38667de792161f0e332050688c51589343c8ce07
Sahana240107 <sahana240107@gmail.com>
2025-11-24 10:15:41 +0530

    defuseBomb question added

--- Sliding Window/defuseBomb.cpp
+++ Sliding Window/defuseBomb.cpp
@@ -10,3 +10,48 @@ As code is circular, the next element of code[n-1] is code[0], and the previous 
 Given the circular array code and an integer key k, return the decrypted code to defuse the bomb!
 
 */
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n=code.size();
+        if(k==0)
+        {
+            vector<int> ans(n,0);
+            return ans;
+        }
+        if(k>0)
+        {
+            vector<int> ans;
+            int sum;
+            for(int i=0;i<n;i++)
+            {
+                sum=0;
+                for(int j=1;j<=k;j++)
+                {
+                    int ind=(i+j)%n;
+                    sum+=code[ind];
+                }
+                ans.push_back(sum);
+            }
+            return ans;
+        }
+        else
+        {
+        vector<int> ans;
+           int sum;
+           k=-k;
+            for(int i=n-1;i>=0;i--)
+            {
+                sum=0;
+                for(int j=1;j<=k;j++)
+                {
+                    int ind=(i-j+n)%n;
+                    sum+=code[ind];
+                }
+                ans.push_back(sum);
+            }
+            reverse(ans.begin(),ans.end());
+            return ans; 
+        }
+    }
+};
