2976bbe053c304ef431ebca2b778892f45cf73af
Sahana240107 <sahana240107@gmail.com>
2025-11-24 19:12:35 +0530

    anagramStr question solved

--- Sliding Window/anagramStr.cpp
+++ Sliding Window/anagramStr.cpp
@@ -8,3 +8,31 @@ Explanation:
 The substring with start index = 0 is "cba", which is an anagram of "abc".
 The substring with start index = 6 is "bac", which is an anagram of "abc".
 */
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+        int n=s.size(),m=p.size();
+        if(m>n)
+            return ans;
+        vector<int> fs(26,0),fp(26,0);
+        for(char c:p)
+        {
+            fp[c-'a']++;
+        }
+        for(int i=0;i<m;i++)
+        {
+            fs[s[i]-'a']++;
+        }
+        if(fp==fs)
+            ans.push_back(0);
+        for(int i=m;i<s.size();i++)
+        {
+            fs[s[i]-'a']++;
+            fs[s[i-m]-'a']--;
+            if(fs==fp)
+                ans.push_back(i-m+1);
+        }
+        return ans;
+    }
+};
