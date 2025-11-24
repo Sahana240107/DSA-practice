56e83c35ab17a059b18b99453858d8476012f6a5
Sahana240107 <sahana240107@gmail.com>
2025-11-24 13:57:59 +0530

    CountIncreasingSubarr question solved

--- Sliding Window/countIncreasingSubarr.cpp
+++ Sliding Window/countIncreasingSubarr.cpp
@@ -6,3 +6,20 @@ Input: arr[] = [1, 3, 3, 2, 3, 5]
 Output: 4
 Explanation: Increasing Subarrays are : arr[0,1], arr[3,4], arr[3,5], arr[4,5].
 */
+class Solution {
+  public:
+    int countIncreasing(vector<int>& arr) {
+        // code here.
+        int count=0,first=0;
+        for(int i=1;i<arr.size();i++)
+        {
+            if(arr[i]<=arr[i-1])
+                first=i;
+            else
+                count++;
+            if(first<i-1)
+                count+=(i-first-1);
+        }
+        return count;
+    }
+};
