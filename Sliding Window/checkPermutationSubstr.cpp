c51ce9d8db3c1f83c19fc263c108813a2f9b18ad
Sahana240107 <sahana240107@gmail.com>
2025-11-24 15:34:49 +0530

    checkPermutationSubstr question solved

--- Sliding Window/checkPermutationSubstr.cpp
+++ Sliding Window/checkPermutationSubstr.cpp
@@ -9,3 +9,31 @@ Input: txt = "programming", pat = "rain"
 Output: false
 Explanation: No permutation of "rain" exists as a substring in "programming".
 */
+// User function Template for C++
+class Solution {
+  public:
+    bool search(string& txt, string& pat) {
+        // Write your code here
+        int n=txt.size(),m=pat.size();
+        vector<int> fpat(26,0),ftxt(26,0);
+        for(char c:pat)
+        {
+            fpat[c-'a']++;
+        }
+        for(int i=0;i<m;i++)
+        {
+            char c=txt[i];
+            ftxt[c-'a']++;
+        }
+        if(fpat==ftxt)
+            return true;
+        for(int i=m;i<n;i++)
+        {
+            ftxt[txt[i]-'a']++;
+            ftxt[txt[i-m]-'a']--;
+            if(ftxt==fpat)
+                return true;
+        }
+        return false;
+    }
+};
