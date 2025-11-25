555326b1fda1ce8e804fdd2829273f8c4699d253
Sahana240107 <sahana240107@gmail.com>
2025-11-25 23:20:11 +0530

    bracket number question solved

--- stack/bracketNo.cpp
+++ stack/bracketNo.cpp
@@ -7,3 +7,34 @@ Output: 1 2 2 1 3 3
 Explanation: The highlighted brackets in
 the given string (aa(bdc))p(dee) are
 assigned the numbers as: 1 2 2 1 3 3.*/
+// User function template for C++
+class Solution {
+  public:
+
+    vector<int> bracketNumbers(string str) {
+        // Your code goes here
+        vector<int> br;
+        int open=0;
+        stack<pair<char,int>> st;
+        for(char c:str)
+        {
+            if(c=='(' || c=='{' || c=='[')
+            {
+                st.push({c,++open});
+                br.push_back(open);
+            }
+            else if(c==')' || c=='}' || c==']')
+            {
+                if(!st.empty())
+                {
+                    if(c==')' && st.top().first=='(' || c=='}' && st.top().first=='{' || c==']' && st.top().first=='[')
+                    {
+                        br.push_back(st.top().second);
+                        st.pop();
+                    }
+                }
+            }
+        }
+        return br;
+    }
+};
