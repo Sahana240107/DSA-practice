a5d89251e3713f42492ec0cb9d36d161bc55eba2
Sahana240107 <sahana240107@gmail.com>
2025-11-26 20:59:44 +0530

    insert at bottom of stack question added

---
+++ stack/insertBottom.cpp
@@ -0,0 +1,15 @@
+/*You are given a stack st of n integers and an element x. You have to insert x at the bottom of the given stack. 
+
+Note: Everywhere in this problem, the bottommost element of the stack is shown first while priniting the stack.
+
+Example 1:
+
+Input:
+n = 5
+x = 2
+st = {4,3,2,1,8}
+Output:
+{2,4,3,2,1,8}
+Explanation:
+After insertion of 2, the final stack will be {2,4,3,2,1,8}.
+*/
