b254286f4a92bb6fb47e5edd859da36ccecb36e8
Sahana240107 <sahana240107@gmail.com>
2025-11-26 12:24:12 +0530

    compress string question solved

--- Strings/compressStr.cpp
+++ Strings/compressStr.cpp
@@ -8,3 +8,21 @@ Explanation: As 'a' appears 3 times
 consecutively and 'b' also 3 times,
 and 'b' and 'B' considered as same. 
 */
+string transform(string S) {
+    // complete the function here
+    string ans;
+    int n=S.length();
+    for(int i=0;i<S.length();i++)
+    {
+        char c=tolower(S[i]);
+        int count=1;
+        while(i+1<n && c==tolower(S[i+1]))
+        {
+            count++;
+            i++;
+        }
+        ans+=to_string(count);
+        ans+=c;
+    }
+    return ans;
+}
