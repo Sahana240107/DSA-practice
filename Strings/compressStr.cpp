56a414d8f77b1601d111700c1e400695e9124bae
Sahana240107 <sahana240107@gmail.com>
2025-11-26 12:23:35 +0530

    compress string question added

---
+++ Strings/compressStr.cpp
@@ -0,0 +1,10 @@
+/*You are given the string S . Compress the string when lower and upper cases are the same. In compressed string characters should be in lowercase.
+
+Example 1:
+
+Input: S = "aaABBb"
+Output: "3a3b"
+Explanation: As 'a' appears 3 times
+consecutively and 'b' also 3 times,
+and 'b' and 'B' considered as same. 
+*/
