204effc3401fe94c6617e387ca29965cf875149c
Sahana240107 <sahana240107@gmail.com>
2025-11-26 11:42:05 +0530

    reverse equation question solved

--- stack/reverseEqn.cpp
+++ stack/reverseEqn.cpp
@@ -8,3 +8,34 @@ S = "20-3+5*2"
 Output: 2*5+3-20
 Explanation: The equation is reversed with
 numbers remaining the same.*/
+class Solution {
+  public:
+    string reverseEqn(string s) {
+        // code here.
+        int n=s.length();
+        stack<string> st;
+        for(int i=0;i<n;i++)
+        {
+            if(isdigit(s[i]))
+            {
+                string num="";
+                while(i<n && isdigit(s[i]))
+                {
+                    num+=s[i];
+                    i++;
+                }
+                st.push(num);
+                i--;
+            }
+            else
+                st.push(string(1,s[i]));
+        }
+        string rev;
+        while(!st.empty())
+        {
+            rev+=st.top();
+            st.pop();
+        }
+        return rev;
+    }
+};
