b309374f3623ff26aca5fd186749d56839ad1191
Sahana240107 <sahana240107@gmail.com>
2025-11-26 21:02:19 +0530

    reverse K elements of Queue question added

---
+++ stack/revKQ.cpp
@@ -0,0 +1,6 @@
+/*Given an integer k and a queue of integers, we need to reverse the order of the first k elements of the queue, leaving the other elements in the same relative order.
+
+Input: k = 3, queue[] = [1, 2, 3, 4, 5]
+Output: 3 2 1 4 5
+Explanation: After reversing the given input from the 3rd position the resultant output will be 3 2 1 4 5.
+*/
