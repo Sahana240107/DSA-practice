c3cb103c049f847a41b4485f4613c614c75b5094
Sahana240107 <sahana240107@gmail.com>
2025-11-26 21:03:17 +0530

    Reverse K elements of queue question solved

--- stack/revKQ.cpp
+++ stack/revKQ.cpp
@@ -4,3 +4,30 @@ Input: k = 3, queue[] = [1, 2, 3, 4, 5]
 Output: 3 2 1 4 5
 Explanation: After reversing the given input from the 3rd position the resultant output will be 3 2 1 4 5.
 */
+class Solution {
+  public:
+    queue<int> modifyQueue(queue<int> &q, int k) {
+        // code here
+        int ind=q.size()-k;
+        stack<int> st;
+        while(k>0 && !q.empty())
+        {
+            st.push(q.front());
+            q.pop();
+            k--;
+        }
+        while(!st.empty())
+        {
+            q.push(st.top());
+            st.pop();
+        }
+        while(ind>0 && !q.empty())
+        {
+            q.push(q.front());
+            q.pop();
+            ind--;
+        }
+        return q;
+        
+    }
+};
