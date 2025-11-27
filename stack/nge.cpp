169939239a7016b0988efe2d0ca232eff31f7866
Sahana240107 <sahana240107@gmail.com>
2025-11-27 14:36:56 +0530

    next greater element question added

---
+++ stack/nge.cpp
@@ -0,0 +1,3 @@
+/*For each element in an array, the Next Greater Element (NGE) is the first element to the right that is strictly greater than the current element.
+
+If no such element exists → NGE = -1*/
