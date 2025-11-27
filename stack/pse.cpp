cf1cd0c9edbcbce26f806a73952cd3ed09a0e1a4
Sahana240107 <sahana240107@gmail.com>
2025-11-27 17:23:42 +0530

    previous smaller element question solved

--- stack/pse.cpp
+++ stack/pse.cpp
@@ -13,3 +13,22 @@ For 1, there is no element on the left, so answer is -1.
 For 6, previous smaller element is 1.
 For 2, previous smaller element is 1.
 */
+class Solution {
+  public:
+    vector<int> prevSmaller(vector<int>& arr) {
+        //  code here
+        int n=arr.size();
+        vector<int> pse(n);
+        stack<int> st;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && st.top()>=arr[i])
+            {
+                st.pop();
+            }
+            pse[i]=(st.empty())?-1:st.top();
+            st.push(arr[i]);
+        }
+        return pse;
+    }
+};
