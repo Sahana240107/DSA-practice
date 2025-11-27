102e3925e4a8dbed6b8f68a5d1f67cfdb0a47f8e
Sahana240107 <sahana240107@gmail.com>
2025-11-27 12:31:29 +0530

    remove duplicate consecutive chars in string question added

---
+++ stack/removeDupChar.cpp
@@ -0,0 +1,13 @@
+/*You are given string s. You need to remove the pair of duplicates.
+Note: The pair should be of adjacent elements and after removing a pair the remaining string is joined together. 
+
+Examples:
+
+Input: s = "aaabbaaccd"
+Output: ad
+Explanation: 
+Remove (aa)abbaaccd =>abbaaccd
+Remove a(bb)aaccd => aaaccd
+Remove (aa)accd => accd
+Remove a(cc)d => ad
+*/
