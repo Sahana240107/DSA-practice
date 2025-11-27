d6f95b465271700879a7c5ee4a0ae8c566f5a970
Sahana240107 <sahana240107@gmail.com>
2025-11-27 12:32:18 +0530

    remove duplicate consecutive chars in string question solved

--- stack/removeDupChar.cpp
+++ stack/removeDupChar.cpp
@@ -11,3 +11,25 @@ Remove a(bb)aaccd => aaaccd
 Remove (aa)accd => accd
 Remove a(cc)d => ad
 */
+class Solution {
+  public:
+    string removePair(string s) {
+        // code here
+        stack<char> st;
+        for(char c:s)
+        {
+            if(!st.empty() && st.top()==c)
+                st.pop();
+            else
+                st.push(c);
+        }
+        string ans;
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
