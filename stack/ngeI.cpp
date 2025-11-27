b286736987617a1412732c5ac03624ceb0f5974c
Sahana240107 <sahana240107@gmail.com>
2025-11-27 16:39:35 +0530

    next greater element I question solved

--- stack/ngeI.cpp
+++ stack/ngeI.cpp
@@ -17,3 +17,26 @@ Explanation: The next greater element for each value of nums1 is as follows:
 - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
 - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
 */
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res(nums1.size());
+        stack<int> st;
+        unordered_map<int,int> mp;
+        for(int i=nums2.size()-1;i>=0;i--)
+        {
+            while(!st.empty() && st.top()<=nums2[i])
+            {
+                st.pop();
+            }
+            mp[nums2[i]]=(st.empty())?-1:st.top();
+            st.push(nums2[i]);
+        }
+        for(int i=0;i<nums1.size();i++)
+        {
+            int nge=mp[nums1[i]];
+            res[i]=nge;
+        }
+        return res;
+    }
+};
