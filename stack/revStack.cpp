c133b1081b73e3337e7ec0b11998afd0bf8cafa5
Sahana240107 <sahana240107@gmail.com>
2025-11-27 19:08:09 +0530

    reverse stack question solved

--- stack/revStack.cpp
+++ stack/revStack.cpp
@@ -7,3 +7,20 @@ Output: [1, 2, 3, 4]
 Explanation: After reversing, the elements of stack are in opposite order.
 
 */
+class Solution {
+  public:
+    void reverseStack(stack<int> &st) {
+        // code here
+        queue<int> q;
+        while(!st.empty())
+        {
+            q.push(st.top());
+            st.pop();
+        }
+        while(!q.empty())
+        {
+            st.push(q.front());
+            q.pop();
+        }
+    }
+};
