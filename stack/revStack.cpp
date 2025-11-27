01ca92592dd200504a09322b2493fa307db33714
Sahana240107 <sahana240107@gmail.com>
2025-11-27 19:07:33 +0530

    reverse stack question added

---
+++ stack/revStack.cpp
@@ -0,0 +1,9 @@
+/*You are given a stack st[]. You have to reverse the stack.
+
+Examples:
+
+Input: st[] = [1, 2, 3, 4]
+Output: [1, 2, 3, 4]
+Explanation: After reversing, the elements of stack are in opposite order.
+
+*/
