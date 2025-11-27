9099e4a7f9c2eb09a6871fe4be6dbbbf0a141c3a
Sahana240107 <sahana240107@gmail.com>
2025-11-27 19:21:00 +0530

    reverse a stack question solved using recursion

--- stack/revStack.cpp
+++ stack/revStack.cpp
@@ -24,3 +24,30 @@ class Solution {
         }
     }
 };
+
+//Using Recursion
+
+class Solution {
+  public:
+    void insertAtBottom(stack<int>& st,int x)
+    {
+        if(st.empty())
+        {
+            st.push(x);
+            return;
+        }
+        int top=st.top();
+        st.pop();
+        insertAtBottom(st,x);
+        st.push(top);
+    }
+    void reverseStack(stack<int> &st) {
+        // code here
+        if(st.empty())
+            return;
+        int top=st.top();
+        st.pop();
+        reverseStack(st);
+        insertAtBottom(st,top);
+    }
+};
