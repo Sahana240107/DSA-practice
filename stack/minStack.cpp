d3fc3a8f255fefee7896197fa0386be4dfd62a2a
Sahana240107 <sahana240107@gmail.com>
2025-11-27 12:56:26 +0530

    minStack question solved

--- stack/minStack.cpp
+++ stack/minStack.cpp
@@ -10,3 +10,64 @@ int getMin() retrieves the minimum element in the stack.
 You must implement a solution with O(1) time complexity for each function.
 
 */
+class MinStack {
+public:
+    stack<long long> st;
+    long long mini;
+    MinStack() {
+        mini=LLONG_MAX;
+    }
+    
+    void push(int val) {
+        long long v=val;
+        if(st.empty())
+        {
+            st.push(v);
+            mini=v;
+        }
+        else
+        {
+            if(v<mini)
+            {
+                st.push(2LL*v-mini);
+                mini=v;
+            }
+            else
+                st.push(v);
+        }
+    }
+    
+    void pop() {
+        if(!st.empty())
+        {
+            long long top=st.top();
+            st.pop();
+            if(top<mini)
+            {
+                mini=2LL*mini-top;
+            }
+        }
+    }
+    
+    int top() {
+        if(st.empty())
+            return -1;
+        long long top=st.top();
+        if(top<mini)
+            return (int)mini;
+        return (int)top;
+    }
+    
+    int getMin() {
+        return (int)mini;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
