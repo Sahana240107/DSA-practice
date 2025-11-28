8e0c26dceafbe06982d8ca345608be80610f7103
Sahana240107 <sahana240107@gmail.com>
2025-11-28 11:04:24 +0530

    a pow b last digit question added

---
+++ math/aPowbLastDigit.cpp
@@ -0,0 +1,8 @@
+/*You are given two integer numbers in the form of string, the base a and the index b. You have to find the last digit of ab.
+
+Examples:
+
+Input: a = "3", b = "10"
+Output: 9
+Explanation: 310 = 59049. Last digit is 9.
+*/
