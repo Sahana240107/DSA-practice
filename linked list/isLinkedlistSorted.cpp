c3a277258127f39246c6146adad0abae147ca2ae
Sahana240107 <sahana240107@gmail.com>
2025-12-02 15:01:05 +0530

    isLinkedlistSorted question added

---
+++ linked list/isLinkedlistSorted.cpp
@@ -0,0 +1,8 @@
+/*Given the head of a singly linked list, you have to find whether the given linked list is sorted or not.
+Note: The sorting can either be non-increasing or non-decreasing.
+
+Examples:
+
+Input: head: 5 -> 5 -> 6 -> 7 -> 8
+Output: true
+*/
