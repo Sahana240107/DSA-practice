307cde43cd9f4a7ea34ea4bbc90d2e4a18ec335e
Sahana240107 <sahana240107@gmail.com>
2025-12-02 15:02:32 +0530

    isLinkedlistSorted question solved

--- linked list/isLinkedlistSorted.cpp
+++ linked list/isLinkedlistSorted.cpp
@@ -6,3 +6,37 @@ Examples:
 Input: head: 5 -> 5 -> 6 -> 7 -> 8
 Output: true
 */
+class Solution {
+  public:
+    bool isSorted(Node* head) {
+        // code here
+        int isAscend=1;
+        if(head==NULL || head->next==NULL)
+        {
+            return true;
+        }
+        Node* second=head->next;
+        if(head->data>second->data)
+        {
+            isAscend=0;
+        }
+        if(head->data==second->data)
+        {
+                Node* third=second->next;
+                if(second->data>third->data)
+                    isAscend=0;
+        }
+        Node* prev=head;
+        Node* temp=head->next;
+        while(temp!=NULL)
+        {
+            if(isAscend && prev->data>temp->data)
+                return false;
+            else if(!isAscend && prev->data<temp->data)
+                return false;
+            prev=temp;
+            temp=temp->next;
+        }
+        return true;
+    }
+};
