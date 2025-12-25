7832643c8fc9c85fa1473d13d0591e8b87132213
Sahana240107 <sahana240107@gmail.com>
2025-12-25 20:20:49 +0530

    level order traversal from leaf question solved

--- tree/revLevel.cpp
+++ tree/revLevel.cpp
@@ -4,3 +4,32 @@ Example 1:
 Input: root = [3,9,20,null,null,15,7]
 Output: [[15,7],[9,20],[3]]
 */
+class Solution {
+public:
+    
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        if(!root)
+            return {};
+        vector<vector<int>> levelOrder;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int size=q.size();
+            vector<int> lvl;
+            for(int i=0;i<size;i++)
+            {
+                TreeNode* node=q.front();
+                q.pop();
+                lvl.push_back(node->val);
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+            }
+            levelOrder.push_back(lvl);
+        }
+        reverse(levelOrder.begin(),levelOrder.end());
+        return levelOrder;
+    }
+};
