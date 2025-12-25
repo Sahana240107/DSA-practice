0226a1ca0ffe34084c86cddc46f89477e6c2eb63
Sahana240107 <sahana240107@gmail.com>
2025-12-25 19:44:44 +0530

    spiral level order traversal question added

---
+++ tree/spiralLevel.cpp
@@ -0,0 +1,7 @@
+/*Given a root binary tree and the task is to find the spiral order traversal of the tree 
+and return the list containing the elements.
+
+Spiral Order Traversal mean: Starting from level 0 for root node, for all the even levels
+ we print the node's value from right to left and for all the odd levels we print the node's value from left to right.
+
+*/
