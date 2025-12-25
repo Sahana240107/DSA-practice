ad68604e3a9bd3a953eda19a0224f54a3da38e28
Sahana240107 <sahana240107@gmail.com>
2025-12-25 19:14:26 +0530

    level order traversal question added

---
+++ tree/levelOrder.cpp
@@ -0,0 +1,4 @@
+/*Given the root of a Binary Tree, your task is to return its Level Order Traversal.
+
+Note: A level order traversal is a breadth-first search (BFS) of the tree. It visits nodes level by level, 
+starting from the root, and processes all nodes from left to right within each level before moving to the next.*/
