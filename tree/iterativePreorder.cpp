b9f7e8ae6b2eb2e8677465a2921e1a1918dbd7ea
Sahana240107 <sahana240107@gmail.com>
2025-12-25 21:41:22 +0530

    iterative preorder question solved

--- tree/iterativePreorder.cpp
+++ tree/iterativePreorder.cpp
@@ -1,1 +1,22 @@
 /*Preorder without recursion*/
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(!root)
+            return {};
+        vector<int> pre;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty())
+        {
+            TreeNode* node=st.top();
+            st.pop();
+            pre.push_back(node->val);
+            if(node->right)
+                st.push(node->right);
+            if(node->left)
+                st.push(node->left);
+        }
+        return pre;
+    }
+};
