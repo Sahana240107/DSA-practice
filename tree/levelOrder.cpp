7ff303679a433ce07e7dd823f578f22dbbdad19f
Sahana240107 <sahana240107@gmail.com>
2025-12-25 19:15:28 +0530

    level order traversal question solved

--- tree/levelOrder.cpp
+++ tree/levelOrder.cpp
@@ -2,3 +2,31 @@
 
 Note: A level order traversal is a breadth-first search (BFS) of the tree. It visits nodes level by level, 
 starting from the root, and processes all nodes from left to right within each level before moving to the next.*/
+class Solution {
+  public:
+    vector<vector<int>> levelOrder(Node *root) {
+        if(!root)
+            return {};
+        vector<vector<int>> ans;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int size=q.size();
+            vector<int> level;
+            for(int i=0;i<size;i++)
+            {
+                Node* node=q.front();
+                q.pop();
+                level.push_back(node->data);
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+            }
+            ans.push_back(level);
+        }
+        return ans;
+        
+    }
+};
