3d2a220dbecb19fdc2f60facc2061d517cd2b6bb
Sahana240107 <sahana240107@gmail.com>
2025-12-26 07:28:33 +0530

    iterative inorder question solved

--- tree/iterativeInorder.cpp
+++ tree/iterativeInorder.cpp
@@ -1,1 +1,30 @@
 /*Inorder without recursion (using stack)*/
+class Solution {
+  public:
+    vector<int> inOrder(Node* root) {
+        // code here
+        if(!root)
+            return {};
+        vector<int> inorder;
+        stack<Node*> st;
+        Node* node=root;
+        while(true)
+        {
+            if(node)
+            {
+                st.push(node);
+                node=node->left;
+            }
+            else
+            {
+                if(st.empty())
+                    break;
+                node=st.top();
+                st.pop();
+                inorder.push_back(node->data);
+                node=node->right;
+            }
+        }
+        return inorder;
+    }
+};
