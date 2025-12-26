87403a0920276b4f643315e666436a5cf9f088fb
Sahana240107 <sahana240107@gmail.com>
2025-12-26 23:21:45 +0530

    iterative postorder using 2 stacks added

--- tree/iterativePostorder.cpp
+++ tree/iterativePostorder.cpp
@@ -1,1 +1,29 @@
 /*Using 2 stack*/
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(!root)
+            return {};
+        stack<TreeNode*> st1;
+        stack<TreeNode*> st2;
+        st1.push(root);
+        while(!st1.empty())
+        {
+            TreeNode* node=st1.top();
+            st1.pop();
+            st2.push(node);
+            if(node->left)
+                st1.push(node->left);
+            if(node->right)
+                st1.push(node->right);
+        }
+        vector<int> postorder;
+        while(!st2.empty())
+        {
+            postorder.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return postorder;
+    }
+};
+
