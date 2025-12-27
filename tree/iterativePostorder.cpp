218d866a7cdf534656b3d0e8aa55cc29aaddd956
Sahana240107 <sahana240107@gmail.com>
2025-12-27 23:31:01 +0530

    iterative postorder using 1 stack added

--- tree/iterativePostorder.cpp
+++ tree/iterativePostorder.cpp
@@ -28,3 +28,39 @@ public:
 };
 
 /*Using 1 stack*/
+class Solution {
+  public:
+    vector<int> postOrder(Node* node) {
+        // code here
+        stack<Node*> st;
+        vector<int> post;
+        Node* cur=node;
+        while(cur!=NULL|| !st.empty())
+        {
+            if(cur)
+             {
+                st.push(cur);
+                cur=cur->left;
+            }
+            else
+            {
+                Node* temp=st.top()->right;
+                if(temp==NULL)
+                {
+                    temp=st.top();
+                    st.pop();
+                    post.push_back(temp->data);
+                    while(!st.empty() && temp==st.top()->right)
+                    {
+                        temp=st.top();
+                        st.pop();
+                        post.push_back(temp->data);
+                    }
+                }
+                else
+                    cur=temp;
+            }
+        }
+         return post;
+    }
+};
