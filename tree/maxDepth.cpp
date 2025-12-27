e7b68627f5ca1bc18e29b7b13e21f07b8cfed1bd
Sahana240107 <sahana240107@gmail.com>
2025-12-27 23:38:52 +0530

    maximum depth question solved

--- tree/maxDepth.cpp
+++ tree/maxDepth.cpp
@@ -7,3 +7,11 @@
 Output: 3
 Explanation:
 Maximum depth is between nodes 1 and 4, which is 3.*/
+class Solution {
+  public
+    int maxDepth(Node *root) {
+        if(root==nullptr)
+            return 0;
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
+    }
+};
