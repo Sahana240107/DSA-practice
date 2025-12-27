4af66c3c92fd20b1a1a2faa63883b5a07fd35016
Sahana240107 <sahana240107@gmail.com>
2025-12-27 23:35:00 +0530

    maxWidth question solved

--- tree/maxWidth.cpp
+++ tree/maxWidth.cpp
@@ -5,3 +5,28 @@ Examples:
 Input: root = [1, 2, 3, 4, 5, 6, 7]
           
 Output: 4*/
+class Solution {
+  public:
+    int maxWidth(Node* root) {
+        if(!root)
+            return 0;
+        int maxi=0;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int size=q.size();
+            maxi=max(maxi,size);
+            for(int i=0;i<size;i++)
+            {
+                Node* node=q.front();
+                q.pop();
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+            }
+        }
+        return maxi;
+    }
+};
