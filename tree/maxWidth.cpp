acd4fb39723f55d8195e6bbd740bd3428bcefbf6
Sahana240107 <sahana240107@gmail.com>
2025-12-27 23:34:14 +0530

    maxWidth question added

---
+++ tree/maxWidth.cpp
@@ -0,0 +1,7 @@
+/*Given a Binary Tree, find the maximum width of it. Maximum width is defined as the maximum number of nodes at any level.
+
+Examples:
+
+Input: root = [1, 2, 3, 4, 5, 6, 7]
+          
+Output: 4*/
