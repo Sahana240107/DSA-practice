6f0109967e219ddc05eb2724983d29188c41aa60
Sahana240107 <sahana240107@gmail.com>
2025-12-28 22:55:36 +0530

    add binary leading zero version added

--- math/addBin.cpp
+++ math/addBin.cpp
@@ -29,3 +29,45 @@ public:
         return res;
     }
 };
+
+//leading zeros version
+
+// User function template for C++
+class Solution {
+    string leadingZero(string s)
+    {
+        int lead=0;
+        for(char x:s)
+        {
+            if(x=='0')
+                lead++;
+            else
+                break;
+        }
+        if(lead==s.length())
+            return "0";
+        return s.substr(lead);
+        
+    }
+  public:
+    string addBinary(string& s1, string& s2) {
+        // your code here
+        s1=leadingZero(s1);
+        s2=leadingZero(s2);
+        int i=s1.length()-1,j=s2.length()-1,carry=0;
+        string res;
+        while(i>=0||j>=0||carry==1)
+        {
+            int sum=carry;
+            if(i>=0)
+                sum+=s1[i]-'0';
+            if(j>=0)
+                sum+=s2[j]-'0';
+            res+=(sum%2)+'0';
+            carry=sum/2;
+            i--;j--;
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
