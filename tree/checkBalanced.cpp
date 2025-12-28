bb3eb61b8fa1fc1f95aedf048c921fceb3190fbb
Sahana240107 <sahana240107@gmail.com>
2025-12-28 19:37:52 +0530

    check balanced tree queestion added

---
+++ tree/checkBalanced.cpp
@@ -0,0 +1,10 @@
+/*Given the root of a binary tree, determine if it is height-balanced or not.
+
+Note: A binary tree is considered height-balanced if the absolute difference in heights of the left and right subtrees is at most 1 for every node in the tree.
+
+Examples:
+
+Input: root = [10, 20, 30, 40, 60]
+    
+Output: true 
+Explanation: The height difference between the left and right subtrees at all nodes is at most 1. Hence, the tree is balanced.*/
