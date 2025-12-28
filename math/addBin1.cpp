966e291d2af2e57f2caf374cc5bfb23fcff42f59
Sahana240107 <sahana240107@gmail.com>
2025-12-28 23:02:40 +0530

    binary addition 1 question added

---
+++ math/addBin1.cpp
@@ -0,0 +1,13 @@
+/*You are given a binary string s of length n. You have to perform binary addition of the string with '1'.
+
+Example 1:
+
+Input: 
+n = 4
+s = 1010
+Output: 1011
+Explaination: 
+The decimal equivalent of given s is 10, 
+Adding 1 gives 11, its binary representation
+is 1011.
+*/
