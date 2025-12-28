02344d8b4d89409bbbc54697fd5f9d838cebd2e3
Sahana240107 <sahana240107@gmail.com>
2025-12-28 22:01:30 +0530

    longest increasing subarray question added

---
+++ longIncSubarr.cpp
@@ -0,0 +1,7 @@
+/*Given an array arr[] of integers. The problem is finding the longest contiguous subarray's length such that every element is strictly greater than its previous element in the same subarray.
+
+Examples:
+
+Input: arr[] = [5, 6, 3, 5, 7, 8, 9, 1, 2]
+Output: 5
+Explanation: here the subarray arr[2:6] = [3, 5, 7, 8, 9] is longest subarray to follow strictly increasing order.*/
