82b7f3e0b40b4d18f73eae48deea6d4ba2a68ed5
Sahana240107 <sahana240107@gmail.com>
2025-12-28 22:02:31 +0530

    longest increasing subarray question solved

--- longIncSubarr.cpp
+++ longIncSubarr.cpp
@@ -5,3 +5,22 @@ Examples:
 Input: arr[] = [5, 6, 3, 5, 7, 8, 9, 1, 2]
 Output: 5
 Explanation: here the subarray arr[2:6] = [3, 5, 7, 8, 9] is longest subarray to follow strictly increasing order.*/
+
+class Solution {
+  public:
+    // Function to find the length of longest increasing subarray
+    int lenOfLongIncSubArr(vector<int> &arr) {
+        int maxi=0,len=0;
+        for(int i=1;i<arr.size();i++)
+        {
+            if(arr[i]<=arr[i-1])
+                len=0;
+            else
+            {
+                len++;
+                maxi=max(maxi,len);
+            }
+        }
+        return maxi+1;
+    }
+};
