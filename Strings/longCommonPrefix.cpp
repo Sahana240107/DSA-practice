5f9067933d0fcdfa4ed6c45513ac95576ad0fda9
Sahana240107 <sahana240107@gmail.com>
2025-12-28 15:23:59 +0530

    longest common prefix question solved

--- Strings/longCommonPrefix.cpp
+++ Strings/longCommonPrefix.cpp
@@ -6,3 +6,21 @@ Example 1:
 
 Input: strs = ["flower","flow","flight"]
 Output: "fl"*/
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==0)
+            return "";
+        int size=strs.size();
+        for(int i=0;i<strs[0].length();i++)
+        {
+            char ch=strs[0][i];
+            for(int j=1;j<size;j++)
+            {
+                if(i>=strs[j].length() || strs[j][i]!=ch)
+                    return strs[0].substr(0,i);
+            }
+        }
+        return strs[0];
+    }
+};
