76db08aa6c2bd960f1dd9e938207f3c23657cafa
Sahana240107 <sahana240107@gmail.com>
2025-12-29 22:31:26 +0530

    maximum sum from root to leaf question added

---
+++ tree/maxSumRootLeaf.cpp
@@ -0,0 +1,31 @@
+/*Given a Binary Tree, find the maximum sum path from a leaf to root.
+
+
+Example 1:
+
+Input:
+        1
+       / \
+      2   3 
+Output:
+4
+Explanation: 
+Following the path 3 -> 1, results in a
+sum of 4, which is the maximum path sum
+from leaf to root for the given tree.
+Example 2:
+
+Input:
+       10
+      /  \
+    -2    7
+    / \   
+   8  -4    
+Output:
+17
+Explanation : 
+Following the path 7 -> 10, results in a
+sum of 17, which is the maximum path sum
+from leaf to root for the given tree.
+
+*/
