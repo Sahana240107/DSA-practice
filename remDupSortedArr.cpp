11e80f2b8174e8b5947fc5445797da6f60db7bbd
Sahana240107 <sahana240107@gmail.com>
2025-12-30 19:29:33 +0530

    remove duplicates from unsorted array question added

---
+++ remDupSortedArr.cpp
@@ -0,0 +1,7 @@
+/*Given an array arr of integers which may or may not contain duplicate elements. Your task is to remove duplicate elements.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 1, 4, 2]
+Output: [1, 2, 3, 4]
+Explanation: 2 and 1 have more than 1 occurence.*/
