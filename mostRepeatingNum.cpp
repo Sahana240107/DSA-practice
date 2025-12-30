4eb1e991f5e6ed28e1ccf4f00fde2aab0952f3f1
Sahana240107 <sahana240107@gmail.com>
2025-12-30 23:05:29 +0530

    mostRepeatingNumber question added

---
+++ mostRepeatingNum.cpp
@@ -0,0 +1,10 @@
+/*Given an array arr[]. The array contains numbers ranging from 0 to k-1 where k is a positive integer.
+ Find the maximum repeating number in this array. If there are two or more maximum repeating numbers, 
+ return the element with the least value.
+
+Examples:
+
+Input: k = 3, arr[] = [2, 2, 1, 2]
+Output: 2
+Explanation: 2 is the most frequent element.
+*/
