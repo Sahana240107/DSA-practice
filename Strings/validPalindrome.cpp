5c65b9528a9511949ba964ab37c4a2b5131a05de
Sahana240107 <sahana240107@gmail.com>
2025-12-30 21:41:09 +0530

    valid palindrome question solved

--- Strings/validPalindrome.cpp
+++ Strings/validPalindrome.cpp
@@ -7,3 +7,19 @@ Example 1:
 Input: s = "A man, a plan, a canal: Panama"
 Output: true
 Explanation: "amanaplanacanalpanama" is a palindrome.*/
+
+//naive approach
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string clean;
+        for(char c:s)
+        {
+            if(isalnum(c))
+                clean+=tolower(c);
+        }
+        string rev=clean;
+        reverse(rev.begin(),rev.end());
+        return clean==rev;
+    }
+};
