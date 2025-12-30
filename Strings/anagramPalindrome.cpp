88d55553d9e29bfb6a40c2c414e8b639f1dcfd53
Sahana240107 <sahana240107@gmail.com>
2025-12-30 22:40:09 +0530

    anagram palindrome question added

---
+++ Strings/anagramPalindrome.cpp
@@ -0,0 +1,19 @@
+/*Given a string S, Check if characters of the given string can be rearranged to form a palindrome.
+Note: You have to return 1 if it is possible to convert the given string into palindrome else return 0. 
+
+Example 1:
+
+Input:
+S = "geeksogeeks"
+Output: Yes
+Explanation: The string can be converted
+into a palindrome: geeksoskeeg
+
+Example 2:
+
+Input: 
+S = "geeksforgeeks"
+Output: No
+Explanation: The given string can't be
+converted into a palindrome.
+*/
