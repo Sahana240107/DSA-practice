473dedc213c02c321c83741e7c369b5ccc7e9b88
Sahana240107 <sahana240107@gmail.com>
2025-12-30 23:06:24 +0530

    most repeating number solved

--- mostRepeatingNum.cpp
+++ mostRepeatingNum.cpp
@@ -8,3 +8,24 @@ Input: k = 3, arr[] = [2, 2, 1, 2]
 Output: 2
 Explanation: 2 is the most frequent element.
 */
+// User function template for C++
+class Solution {
+  public:
+    // Returns maximum repeating element in arr[0..n-1].
+    // The array elements are in range from 0 to k-1
+    int maxRepeating(int k, vector<int>& arr) {
+        // code here
+        unordered_map<int,int> freq;
+        int maxi=0,freqx=0;
+        for(int x:arr)
+        {
+            freq[x]++;
+            if(freq[x]>maxi || (freq[x]==maxi && freqx>x))
+            {
+                maxi=freq[x];
+                freqx=x;
+            }
+        }
+        return freqx;
+    }
+};
