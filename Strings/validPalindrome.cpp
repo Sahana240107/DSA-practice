97229e236a40418e5ac93a62e2827424a00447f5
Sahana240107 <sahana240107@gmail.com>
2025-12-30 21:50:03 +0530

    valid palindrome solved using two pointer

--- Strings/validPalindrome.cpp
+++ Strings/validPalindrome.cpp
@@ -23,3 +23,24 @@ public:
         return clean==rev;
     }
 };
+
+//two pointer approach
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+    int left=0,right=s.length()-1;
+    while(left<right)
+    {
+        while(left<right && !isalnum(s[left]))
+            left++;
+        while(left<right && !isalnum(s[right]))
+            right--;
+        if(tolower(s[left])!=tolower(s[right]))
+            return false;
+        left++;
+        right--;
+    }
+    return true;
+    }
+};
