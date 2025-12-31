2563b796651904d0136e4e5a448c2764fb350cf8
Sahana240107 <sahana240107@gmail.com>
2025-12-31 08:47:18 +0530

    minimum distance solved

--- minDistance.cpp
+++ minDistance.cpp
@@ -6,3 +6,21 @@ Input: arr[] = [1, 2, 3, 2], x = 1, y = 2
 Output: 1
 Explanation: x = 1 and y = 2. There are two distances between x and y, which are 1 and 3 out of which the least is 1.
 */
+class Solution {
+  public:
+    int minDist(vector<int>& arr, int x, int y) {
+        int last=-1,ans=INT_MAX;
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]==x || arr[i]==y)
+            {
+                if(last!=-1 && arr[i]!=arr[last])
+                {
+                    ans=min(ans,i-last);
+                }
+                last=i;
+            }
+        }
+        return ans==INT_MAX?-1:ans;
+    }
+};
