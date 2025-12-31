7c290b9772f3ed4b51f26980b5b3066a4cb34264
Sahana240107 <sahana240107@gmail.com>
2025-12-31 08:45:57 +0530

    minimum distance between two elements in array

---
+++ minDistance.cpp
@@ -0,0 +1,8 @@
+/*You are given an array, arr[]. Find the minimum index based distance between two distinct elements of the array, x and y. Return -1, if either x or y does not exist in the array.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 2], x = 1, y = 2
+Output: 1
+Explanation: x = 1 and y = 2. There are two distances between x and y, which are 1 and 3 out of which the least is 1.
+*/
