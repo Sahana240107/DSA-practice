b927a942e349b50c62bfec10b7d72247e4ee3a58
Sahana240107 <sahana240107@gmail.com>
2026-01-01 23:16:51 +0530

    set approach added

--- linked list/intersectionLL.cpp
+++ linked list/intersectionLL.cpp
@@ -8,3 +8,25 @@ The test cases are generated such that there are no cycles anywhere in the entir
 Note that the linked lists must retain their original structure after the function returns.
 
 */
+
+//SET APPROACH TIME:O(N+M) SPACE:O(N)
+node* intersection(node* h1,node* h2)
+{
+    if(!h1 || !h2)
+        return NULL;
+    unordered_set<node*> s;
+    node* t1=h1;
+    while(t1!=NULL)
+    {
+        s.insert(t1);
+        t1=t1->next;
+    }
+    node* t2=h2;
+    while(t2!=NULL)
+    {
+        if(s.find(t2)!=s.end())
+            return t2;
+        t2=t2->next;
+    }
+    return NULL;
+}
