ab9a8fd421e8f580a605cc2e7e6c2fa817232963
Sahana240107 <sahana240107@gmail.com>
2026-01-01 23:33:05 +0530

    intersection by value solved

--- linked list/intersectionValue.cpp
+++ linked list/intersectionValue.cpp
@@ -9,3 +9,34 @@ Input: LinkedList1: 9->6->4->2->3->8 , LinkedList2: 1->2->8->6
 Output: 6->2->8
 Explanation: Nodes 6, 2 and 8 are common in both of the lists and the order will be according to LinkedList1. 
 */
+
+class Solution {
+  public:
+    Node* findIntersection(Node* head1, Node* head2) {
+
+        if(!head1 || !head2)
+            return NULL;
+        Node* dummy=new Node(0);
+        Node* cur=dummy;
+        Node* t2=head2;
+        unordered_set<int> s;
+        while(t2!=NULL)
+        {
+            s.insert(t2->data);
+            t2=t2->next;
+        }
+        Node* t1=head1;
+        while(t1!=NULL)
+        {
+            if(s.find(t1->data)!=s.end())
+            {
+                cur->next=new Node(t1->data);
+                cur=cur->next;
+            }
+            t1=t1->next;
+        }
+        return dummy->next;
+        
+        
+    }
+};
