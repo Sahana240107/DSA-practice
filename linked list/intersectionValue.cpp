9f5848b669d5601cb311a06397c1d8b842d185a4
Sahana240107 <sahana240107@gmail.com>
2026-01-01 23:32:39 +0530

    intersection by value of 2 linked lists question added

---
+++ linked list/intersectionValue.cpp
@@ -0,0 +1,11 @@
+/*Given two linked lists head1 and head2, find the intersection of two linked lists. Each of the two linked lists contains distinct node values.
+
+Note: The order of nodes in this list should be the same as the order in which those particular nodes appear in input head1 and return null if no common element is present.
+
+Examples:
+
+Input: LinkedList1: 9->6->4->2->3->8 , LinkedList2: 1->2->8->6
+ 
+Output: 6->2->8
+Explanation: Nodes 6, 2 and 8 are common in both of the lists and the order will be according to LinkedList1. 
+*/
