96e5e7a544c458f6551aa5df9a0c7457f4a92899
Sahana240107 <sahana240107@gmail.com>
2026-01-02 22:32:47 +0530

    intersection of sorted LL question added

---
+++ linked list/intersectionSortedLL.cpp
@@ -0,0 +1,11 @@
+/*Given that two linked lists are sorted in increasing order, create a new linked list representing the intersection of the two linked lists. The new linked list should be made without changing the original lists.
+
+Note: The elements of the linked list are not necessarily distinct.
+
+Examples:
+
+Input: LinkedList1 = 1->2->3->4->6, LinkedList2 = 2->4->6->8
+Output: 2->4->6
+Explanation: For the given two linked list, 2, 4 and 6 are the elements in the intersection.
+
+*/
