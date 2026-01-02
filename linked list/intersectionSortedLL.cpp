d7818642d872386612752ea9fc782cec98b05da0
Sahana240107 <sahana240107@gmail.com>
2026-01-02 22:34:38 +0530

    intersection of sorted linked lists

--- linked list/intersectionSortedLL.cpp
+++ linked list/intersectionSortedLL.cpp
@@ -9,3 +9,29 @@ Output: 2->4->6
 Explanation: For the given two linked list, 2, 4 and 6 are the elements in the intersection.
 
 */
+class Solution {
+  public:
+    Node* findIntersection(Node* head1, Node* head2) {
+        if(!head1 || !head2)
+            return  NULL;
+        Node* t1=head1;
+        Node* t2=head2;
+        Node* dummy=new Node(0);
+        Node* cur=dummy;
+        while(t1 && t2)
+        {
+            if(t1->data==t2->data)
+            {
+                cur->next=new Node(t1->data);
+                cur=cur->next;
+                t1=t1->next;
+                t2=t2->next;
+            }
+            else if(t1->data<t2->data)
+                t1=t1->next;
+            else
+                t2=t2->next;
+        }
+        return dummy->next;
+    }
+};
