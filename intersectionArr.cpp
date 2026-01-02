7f8d217ba5d1f2a7a3bf579a540fd2ee99a2b8e8
Sahana240107 <sahana240107@gmail.com>
2026-01-02 21:52:46 +0530

    intersection os array II question added

---
+++ intersectionArr.cpp
@@ -0,0 +1,10 @@
+/*Given two unsorted integer arrays a[] and b[] each consisting of distinct elements, the task is to return the count of elements in the intersection (or common elements) of the two arrays.
+
+Intersection of two arrays can be defined as the set containing distinct common elements between the two arrays. 
+
+Examples:
+
+Input: a[] = [89, 24, 75, 11, 23], b[] = [89, 2, 4]
+Output: 1
+Explanation: 89 is the only element in the intersection of two arrays.
+*/
