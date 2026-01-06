0c169cb9283019ac6e52745e321ccf98ac7db890
Sahana240107 <sahana240107@gmail.com>
2026-01-06 18:48:51 +0530

    first unique character in a string question solved

--- Strings/1stUniqueCharacter.cpp
+++ Strings/1stUniqueCharacter.cpp
@@ -9,3 +9,20 @@ Output: 0
 Explanation:
 
 The character 'l' at index 0 is the first character that does not occur at any other index.*/
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int frq[26]={0};
+        for(char c:s)
+        {
+            frq[c-'a']++;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s[i];
+            if(frq[c-'a']==1)
+                return i;
+        }
+        return -1;
+    }
+};
