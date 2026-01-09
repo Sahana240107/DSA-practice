07f788f3d9d08387c42c87004a051140fbcbbc10
Sahana240107 <sahana240107@gmail.com>
2026-01-09 23:21:08 +0530

    rotate list solved

--- linked list/rotateList.cpp
+++ linked list/rotateList.cpp
@@ -4,3 +4,30 @@ Example 1:
 Input: head = [1,2,3,4,5], k = 2
 Output: [4,5,1,2,3]
 */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head||!head->next||k==0)
+            return head;
+        ListNode* last=head;
+        int len=1;
+        while(last->next)
+        {
+            last=last->next;
+            len++;
+        }
+        k=k%len;
+        if(k==0)
+            return head;
+        last->next=head;
+        ListNode* newTail=head;
+        int step=len-k;
+        for(int i=1;i<step;i++)
+        {
+            newTail=newTail->next;
+        }
+        ListNode* newHead=newTail->next;
+        newTail->next=NULL;
+        return newHead;
+    }
+};
