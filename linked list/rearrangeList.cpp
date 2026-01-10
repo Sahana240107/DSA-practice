6ea0149432abb420df221990d2a73b7b7349bc9b
Sahana240107 <sahana240107@gmail.com>
2026-01-10 20:51:41 +0530

    rearrange the even nodes solved

--- linked list/rearrangeList.cpp
+++ linked list/rearrangeList.cpp
@@ -13,3 +13,36 @@ Output: 10->9->3->9->4->5->1->4
 Explanation: Alternative nodes in the given linked list are 4,1,5,4.
  Reversing the alternative nodes from the given list, and then appending 
  them to the end of the list results in a list 10->9->3->9->4->5->1->4.*/
+ class Solution {
+  public:
+    void rearrange(Node* head) {
+        // add code here
+        if(!head||!head->next)
+            return;
+        Node* last=head;
+        while(last->next)
+            last=last->next;
+        int i=1;
+        Node* prev=NULL;
+        Node* temp=head;
+        while(temp!=last)
+        {
+            if(i%2==0)
+            {
+
+                Node* front=temp->next;
+                temp->next=last->next;
+                prev->next=front;
+                last->next=temp;
+                prev=temp;
+                temp=front;
+            }
+            else
+            {
+                prev=temp;
+                temp=temp->next;
+            }
+            i++;
+        }
+    }
+};
