a03e278fa35a9836215767f1fd1a10c98c449744
Sahana240107 <sahana240107@gmail.com>
2026-01-10 20:48:49 +0530

    rearrange the even nodes in linked list question added

---
+++ linked list/rearrangeList.cpp
@@ -0,0 +1,15 @@
+/*Given a linked list, you have to perform the following task:
+
+Extract the alternative nodes starting from the second node.
+Reverse the extracted list.
+Append the extracted list at the end of the original list.
+Note: Try to solve the problem without using any extra memory.
+
+Examples:
+
+Input: LinkedList: 10->4->9->1->3->5->9->4
+Output: 10->9->3->9->4->5->1->4
+
+Explanation: Alternative nodes in the given linked list are 4,1,5,4.
+ Reversing the alternative nodes from the given list, and then appending 
+ them to the end of the list results in a list 10->9->3->9->4->5->1->4.*/
