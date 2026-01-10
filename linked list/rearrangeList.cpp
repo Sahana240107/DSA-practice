5bb8acf81620b6d84eab424c7ac066e0226c9bd5
Sahana240107 <sahana240107@gmail.com>
2026-01-10 23:07:14 +0530

    odd even reorder solved

--- linked list/rearrangeList.cpp
+++ linked list/rearrangeList.cpp
@@ -148,3 +148,23 @@ Example 1:
 Input: head = [1,2,3,4,5]
 Output: [1,3,5,2,4]
 */
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head||!head->next)
+            return head;
+        ListNode* odd=head;
+        ListNode* even=head->next;
+        ListNode* evenHead=even;
+        while(even && even->next)
+        {
+            odd->next=even->next;
+            odd=odd->next;
+            even->next=odd->next;
+            even=even->next;
+        }
+        odd->next=evenHead;
+        return head;
+    }
+};
