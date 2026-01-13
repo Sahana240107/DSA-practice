368ec12e9c85c9905a9701d9327492b5be153ba2
Sahana240107 <sahana240107@gmail.com>
2026-01-13 22:07:03 +0530

    valid palindrome II solved

--- Strings/validPalindromeII.cpp
+++ Strings/validPalindromeII.cpp
@@ -15,3 +15,29 @@ Example 3:
 
 Input: s = "abc"
 Output: false*/
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return isPalindrome(s, left + 1, right) ||
+                       isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+private:
+    bool isPalindrome(const string& s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
