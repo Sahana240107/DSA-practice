89b11061893e9e38899238f60b1bc62eacf75ba0
Sahana240107 <sahana240107@gmail.com>
2026-01-13 23:35:20 +0530

    power of 4 solved

--- bit/power4.cpp
+++ bit/power4.cpp
@@ -1,1 +1,9 @@
 /*Check whether the given number is power of 4*/
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0)
+            return false;
+        return (n&(n-1))==0 && (n&0x55555555);
+    }
+};
