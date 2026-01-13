c480e27c42dd7aa8a6a79f72f9fb48f08cd85bb7
Sahana240107 <sahana240107@gmail.com>
2026-01-13 23:27:52 +0530

    power of 3 solved

--- bit/power3.cpp
+++ bit/power3.cpp
@@ -1,1 +1,10 @@
 /*Given a non-negative integer n. You have to check if it is a power of 3 or not.*/
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0)
+            return false;
+        int maxPow=pow(3,19);
+        return maxPow%n==0;
+    }
+};
