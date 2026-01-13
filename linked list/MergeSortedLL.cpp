af6bb43de3a24b7a53995a5dd44df362c6aadef9
Sahana240107 <sahana240107@gmail.com>
2026-01-13 20:51:28 +0530

    merge sorted LL solved

--- linked list/MergeSortedLL.cpp
+++ linked list/MergeSortedLL.cpp
@@ -8,3 +8,26 @@ Example 1:
 
 Input: list1 = [1,2,4], list2 = [1,3,4]
 Output: [1,1,2,3,4,4]*/
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *dummy=new ListNode(0);
+        ListNode * temp=dummy;
+        while(list1!=NULL && list2!=NULL)
+        {
+            if(list1->val<list2->val)
+            {
+                temp->next=list1;
+                list1=list1->next;
+            }
+            else
+            {
+                temp->next=list2;
+                list2=list2->next;
+            }
+            temp=temp->next;
+        }
+        temp->next=(list1!=NULL)?list1:list2;
+        return dummy->next;
+    }
+};
