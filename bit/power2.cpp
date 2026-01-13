0f84146d6b28e4349b591d835f931200d3842402
Sahana240107 <sahana240107@gmail.com>
2026-01-13 23:16:54 +0530

    power of 2 question added

---
+++ bit/power2.cpp
@@ -0,0 +1,11 @@
+/*Given a non-negative integer n. You have to check if it is a power of 2 or not. 
+
+Examples
+
+Input: n = 8
+Output: true
+Explanation: 8 is equal to 2 raised to 3 (23 = 8).
+Input: n = 98
+Output: false
+Explanation: 98 cannot be obtained by any power of 2.
+*/
