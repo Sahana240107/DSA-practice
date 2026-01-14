5788991aa4fa4d649be977d67152239902e4b5ac
Sahana240107 <sahana240107@gmail.com>
2026-01-14 15:58:39 +0530

    perfect number question added

---
+++ math/perfectNum.cpp
@@ -0,0 +1,7 @@
+/*Given a number n, check if the number is perfect or not. A number is said to be perfect if sum of all its factors excluding the number itself is equal to the number.
+
+Examples:
+
+Input: n = 6
+Output: true 
+Explanation: Factors of 6 are 1, 2, 3 and 6. Excluding 6 their sum is 6 which is equal to n itself. So, it's a Perfect Number.*/
