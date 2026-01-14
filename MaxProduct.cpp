8d06f274fa50aeddb6c9712d44db5b3a8e6f5699
Sahana240107 <sahana240107@gmail.com>
2026-01-14 12:45:42 +0530

    max product solved

--- MaxProduct.cpp
+++ MaxProduct.cpp
@@ -5,3 +5,22 @@ Example:
 Input: arr[] = [1, 4, 3, 6, 7, 0] 
 Output: 42
 Explanation: 6 and 7 have the maximum product.*/
+
+class Solution {
+  public:
+
+    int maxProduct(vector<int>& arr) {
+        int maxi=INT_MIN,second=INT_MIN;
+        for(int x:arr)
+        {
+            if(x>maxi)
+            {
+                second=maxi;
+                maxi=x;
+            }
+            else if(x>second)
+                second=x;
+        }
+        return maxi*second;
+    }
+};
