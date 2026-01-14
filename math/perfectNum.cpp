9fd49335e01a6888166d55648ffb7ab512e05efb
Sahana240107 <sahana240107@gmail.com>
2026-01-14 15:59:43 +0530

    perfect number solved

--- math/perfectNum.cpp
+++ math/perfectNum.cpp
@@ -5,3 +5,20 @@ Examples:
 Input: n = 6
 Output: true 
 Explanation: Factors of 6 are 1, 2, 3 and 6. Excluding 6 their sum is 6 which is equal to n itself. So, it's a Perfect Number.*/
+class Solution {
+  public:
+    bool isPerfect(int n) {
+        // code here
+        int sum=1;
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                sum+=i;
+                if(i!=n/i)
+                    sum+=n/i;
+            }
+        }
+        return sum==n;
+    }
+};
