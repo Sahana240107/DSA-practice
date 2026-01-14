a23664d7f311ced08853d5f0e0d771338167ca57
Sahana240107 <sahana240107@gmail.com>
2026-01-14 16:56:16 +0530

    third largest solved

--- thirdLargest.cpp
+++ thirdLargest.cpp
@@ -18,3 +18,34 @@ Explanation:
 The first distinct maximum is 2.
 The second distinct maximum is 1.
 The third distinct maximum does not exist, so the maximum (2) is returned instead.*/
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+       if(nums.size()==0)
+            return -1;
+        long long first=LLONG_MIN,second=LLONG_MIN,third=LLONG_MIN;
+        int count=0;
+        for(int x:nums)
+        {
+            if(x>first)
+            {
+                third=second;
+                second=first;
+                first=x;
+                count++;
+            }
+            else if(x<first && x>second)
+            {
+                third=second;
+                second=x;
+                count++;
+            }
+            else if(x<second && x>third)
+            {
+                third=x;
+                count++;
+            }
+        } 
+        return count<3?first:(int)third;
+    }
+};
