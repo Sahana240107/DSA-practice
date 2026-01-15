a3e820b8a4df1daaf4bf6641c3a88c03656aaf6d
Sahana240107 <sahana240107@gmail.com>
2026-01-15 20:07:02 +0530

    gcd question added

---
+++ math/gcd.cpp
@@ -0,0 +1,9 @@
+/*Given two positive integers a and b, find GCD of a and b.
+
+Note: Don't use the inbuilt gcd function
+
+Examples:
+
+Input: a = 20, b = 28
+Output: 4
+Explanation: GCD of 20 and 28 is 4*/
