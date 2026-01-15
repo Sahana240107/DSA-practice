d3a40a2102e0bb4625861cf65faf35ae8ab50959
Sahana240107 <sahana240107@gmail.com>
2026-01-15 20:07:51 +0530

    brute force solution added

--- math/gcd.cpp
+++ math/gcd.cpp
@@ -7,3 +7,13 @@ Examples:
 Input: a = 20, b = 28
 Output: 4
 Explanation: GCD of 20 and 28 is 4*/
+
+//Brute force
+int gcd(int a, int b) {
+    int ans = 1;
+    for (int i = 1; i <= min(a, b); i++) {
+        if (a % i == 0 && b % i == 0)
+            ans = i;
+    }
+    return ans;
+}
