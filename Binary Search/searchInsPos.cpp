5ca8a8034e526600fc4ecec749d9bf44d9e08bde
Sahana240107 <sahana240107@gmail.com>
2026-01-16 22:53:28 +0530

    search insert position solved

--- Binary Search/searchInsPos.cpp
+++ Binary Search/searchInsPos.cpp
@@ -13,3 +13,24 @@ Example 2:
 Input: nums = [1,3,5,6], target = 2
 Output: 1
 */
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left=0,right=nums.size()-1,ind=0;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<target)
+            {
+                ind=mid+1;
+                left=mid+1;
+            }
+            else
+                right=mid-1;
+        }
+        return ind;
+    }
+};
