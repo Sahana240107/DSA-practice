ee56fb52167388fc10e2e7aae41b5f3895092fb9
Sahana240107 <sahana240107@gmail.com>
2026-01-17 22:48:06 +0530

    intersection of unsorted array solved

--- intersectUnsorted.cpp
+++ intersectUnsorted.cpp
@@ -1,1 +1,14 @@
 /*Intersection of unsorted arrays*/
+vector<int> intersection(vector<int>& a, vector<int>& b) {
+    unordered_set<int> st(a.begin(), a.end());
+    vector<int> ans;
+
+    for (int x : b) {
+        if (st.find(x) != st.end()) {
+            ans.push_back(x);
+            st.erase(x); // avoid duplicates
+        }
+    }
+
+    return ans;
+}
