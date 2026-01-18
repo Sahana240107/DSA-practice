6068a0f0415118b6f994c7db5f1858be9443c66c
Sahana240107 <sahana240107@gmail.com>
2026-01-18 19:07:12 +0530

    maximum consecutive 1s in binary representation question added

---
+++ bit/BinaryConsec1.cpp
@@ -0,0 +1,8 @@
+/*Given a number n. Find the length of the longest consecutive 1s in its binary representation.
+
+Examples:
+
+Input: n = 14
+Output: 3
+Explanation: Binary representation of 14 is 1110, in which 111 is the longest consecutive set bits of length is 3.
+*/
