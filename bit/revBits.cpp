bca10d0de14612ec6e8bf5fae1d23e6e975a46f6
Sahana240107 <sahana240107@gmail.com>
2026-01-18 19:53:23 +0530

    only significant bits reverse solved

--- bit/revBits.cpp
+++ bit/revBits.cpp
@@ -40,3 +40,19 @@ Explanation:
 After reversing the bits we get:
 (1101)2 = (13)10.
 */
+
+// User function Template for C++
+
+class Solution {
+  public:
+    unsigned int reverseBits(unsigned int n) {
+        // code here
+        unsigned int ans=0;
+        while(n>0)
+        {
+            ans=(ans<<1)|(n&1);
+            n>>=1;
+        }
+        return ans;
+    }
+};
