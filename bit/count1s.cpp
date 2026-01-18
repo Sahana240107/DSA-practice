603d40bcc9e596239cbb94fab2a2ab337bb5c5db
Sahana240107 <sahana240107@gmail.com>
2026-01-18 19:21:46 +0530

    count of 1s in binary

--- bit/count1s.cpp
+++ bit/count1s.cpp
@@ -5,3 +5,17 @@ Examples:
 Input: n = 6
 Output: 2
 Explanation: Binary representation is '110', so the count of the set bit is 2.*/
+class Solution {
+  public:
+    int setBits(int n) {
+        // Write Your Code here
+        int count=0;
+        while(n)
+        {
+            if(n&1)
+                count++;
+            n>>=1;
+        }
+        return count;
+    }
+};
