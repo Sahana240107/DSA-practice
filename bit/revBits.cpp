4c132a9bff2403ecb567f1a81a062a5a597399fc
Sahana240107 <sahana240107@gmail.com>
2026-01-18 19:46:45 +0530

    reverse bits solved

--- bit/revBits.cpp
+++ bit/revBits.cpp
@@ -14,3 +14,15 @@ Integer	Binary
 43261596	00000010100101000001111010011100
 964176192	00111001011110000010100101000000
 */
+class Solution {
+public:
+    int reverseBits(int n) {
+      int ans=0;
+      for(int i=0;i<32;i++)
+      {
+        ans=(ans<<1)|(n&1);
+        n>>=1;
+      }
+      return ans;
+    }
+};
