d7c53ed061baf23b7c904f62baf9c327bbe9682f
Sahana240107 <sahana240107@gmail.com>
2026-01-20 23:21:30 +0530

    length of longest subarray question added

---
+++ linked list/longestSubarr.cpp
@@ -0,0 +1,12 @@
+/*Given an array arr[], return the length of the longest subarray of non-negative integers.
+Note: Subarray here means a continuous part of the array.
+
+Examples:
+
+Input: arr[] = [2, 3, 4, -1, -2, 1, 5, 6, 3]
+Output: 4
+Explanation: The subarray [ 1, 5, 6, 3] has longest length 4 and contains no negative integers
+Input: arr[] = [1, 0, 0, 1, -1, -1, 0, 0, 1, 0]
+Output: 4
+Explanation: Subarrays [1, 0, 0, 1] and [0, 0, 1, 0] have equal lengths but sum of first one is greater so that will be the output.
+*/
