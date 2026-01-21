d746b9efdc05cd122952c3081712fbf32ef13aa2
Sahana240107 <sahana240107@gmail.com>
2026-01-21 21:43:35 +0530

    longest subarray with sum divisible by K question solved

--- longSubarrSumDivByK.cpp
+++ longSubarrSumDivByK.cpp
@@ -7,3 +7,24 @@ Input: arr[] = [2, 7, 6, 1, 4, 5], k = 3
 Output: 4
 Explanation: The subarray [7, 6, 1, 4] has sum = 18, which is divisible by 3.
 */
+// User function template for C++
+class Solution {
+  public:
+    int longestSubarrayDivK(vector<int>& arr, int k) {
+        // Complete the function
+        unordered_map<int,int> mp;
+        int rem=0,maxlen=0;
+        for(int i=0;i<arr.size();i++)
+        {
+               rem+=arr[i];
+               rem=((rem%k)+k)%k;
+               if(rem==0)
+                    maxlen=i+1;
+                if(mp.find(rem)!=mp.end())
+                    maxlen=max(maxlen,i-mp[rem]);
+                if(mp.find(rem)==mp.end())
+                    mp[rem]=i;
+        }
+        return maxlen;
+    }
+};
