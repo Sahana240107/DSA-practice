531dcf31c7146f1624806c06597c44f2d775f3f9
Sahana240107 <sahana240107@gmail.com>
2026-01-21 21:42:46 +0530

    longest subarray with sum divisible by K question added

---
+++ longSubarrSumDivByK.cpp
@@ -0,0 +1,9 @@
+/*Given an array arr[] and a positive integer k, find the length of the longest subarray with the sum of the elements divisible by k.
+Note: If there is no subarray with sum divisible by k, then return 0.
+
+Examples :
+
+Input: arr[] = [2, 7, 6, 1, 4, 5], k = 3
+Output: 4
+Explanation: The subarray [7, 6, 1, 4] has sum = 18, which is divisible by 3.
+*/
