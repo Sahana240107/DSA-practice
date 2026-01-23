2765d59725fd9612204b34ba6a5ba5a64d0adaaa
Sahana240107 <sahana240107@gmail.com>
2026-01-23 23:08:22 +0530

    maxProductSubarr question solved

--- maxProductSubarr.cpp
+++ maxProductSubarr.cpp
@@ -8,3 +8,20 @@ Input: arr[] = [-2, 6, -3, -10, 0, 2]
 Output: 180
 Explanation: The subarray with maximum product is [6, -3, -10] with product = 6 * (-3) * (-10) = 180.
 */
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int pre=1,suf=1,maxi=INT_MIN,n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(pre==0)
+                pre=1;
+            if(suf==0)
+                suf=1;
+            pre=pre*nums[i];
+            suf=suf*nums[n-i-1];
+            maxi=max(maxi,max(pre,suf));
+        }
+        return maxi;
+    }
+};
