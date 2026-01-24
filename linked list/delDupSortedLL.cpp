8a25c0052b6d36ad6c892e8c4b3c8cf5a62f4dd6
Sahana240107 <sahana240107@gmail.com>
2026-01-24 23:13:45 +0530

    delete all occurrences of duplicates in sorted linked list

--- linked list/delDupSortedLL.cpp
+++ linked list/delDupSortedLL.cpp
@@ -8,3 +8,30 @@ Explanation:
 
 The duplicate numbers are 28, 49 and 53 which are removed from the list.
 */
+
+class Solution {
+  public:
+    Node* removeAllDuplicates(struct Node* head) {
+        // code here
+        Node* dummy=new Node(0);
+        dummy->next=head;
+        Node* prev=dummy;
+        Node* cur=head;
+        while(cur)
+        {
+            if(cur->next && cur->data==cur->next->data)
+            {
+                int dup=cur->data;
+                while(cur && cur->data==dup)
+                    cur=cur->next;
+                prev->next=cur;
+            }
+            else
+            {
+                prev=cur;
+                cur=cur->next;
+            }
+        }
+        return dummy->next;
+    }
+};
