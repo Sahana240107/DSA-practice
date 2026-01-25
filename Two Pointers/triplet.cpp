f18ee0d9821659ad7b4b40cad360f15d282f227e
Sahana240107 <sahana240107@gmail.com>
2026-01-25 16:20:48 +0530

    triplet question solved

--- Two Pointers/triplet.cpp
+++ Two Pointers/triplet.cpp
@@ -8,3 +8,29 @@ Explanation: The pair (1, 2) sums to 3.
 Input: arr[] = [3, 4, 5]
 Output: false
 Explanation: No triplets satisfy the condition.*/
+
+class Solution {
+  public:
+    bool findTriplet(vector<int>& arr) {
+        // Your code
+        if(arr.size()<3)
+            return false;
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        for(int k=n-1;k>=2;k--)
+        {
+            int key=arr[k],i=0,j=k-1;
+            while(i<j)
+            {
+                long long sum=(long long)arr[i]+arr[j];
+                if(sum==key)
+                    return true;
+                else if(sum<key)
+                    i++;
+                else
+                    j--;
+            }
+        }
+        return false;
+    }
+};
