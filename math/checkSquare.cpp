7154372625ea402cbd84f6761406c51502e75bce
Sahana240107 <sahana240107@gmail.com>
2026-01-25 21:59:28 +0530

    check square question added

---
+++ math/checkSquare.cpp
@@ -0,0 +1,19 @@
+/*Given coordinates of four points in a plane. Find if the four points form a square or not.
+
+Example 1:
+
+Input:
+points=(0,0),(0,1),(1,0),(1,1)
+Output:
+1
+Explanation:
+These points form a square.
+Example 2:
+
+Input:
+points=(0,0),(1,1),(1,0),(0,2)
+Output:
+0
+Explanation:
+These four points do not form a square.
+*/
