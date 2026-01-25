fd695ac155d09f9e26fe81e5065903a580cc3bea
Sahana240107 <sahana240107@gmail.com>
2026-01-25 16:19:44 +0530

    triplet numbers question added

---
+++ Two Pointers/triplet.cpp
@@ -0,0 +1,10 @@
+/*Given an array arr of integers. First sort the array then find whether three numbers are such that the sum of two elements equals the third element.
+
+Example:
+
+Input: arr[] = [1, 2, 3, 4, 5]
+Output: true
+Explanation: The pair (1, 2) sums to 3.
+Input: arr[] = [3, 4, 5]
+Output: false
+Explanation: No triplets satisfy the condition.*/
