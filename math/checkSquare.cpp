eb8202b6e8924d3083dd32395de539b3667f64de
Sahana240107 <sahana240107@gmail.com>
2026-01-25 21:59:57 +0530

    check square question solved

--- math/checkSquare.cpp
+++ math/checkSquare.cpp
@@ -17,3 +17,26 @@ Output:
 Explanation:
 These four points do not form a square.
 */
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int fourPointSquare(vector<vector<int>> points) {
+        // code here
+        vector<int> dist;
+        for(int i=0;i<4;i++)
+        {
+            for(int j=i+1;j<4;j++)
+            {
+                int dx=points[i][0]-points[j][0];
+                int dy=points[i][1]-points[j][1];
+                dist.push_back(dx*dx+dy*dy);
+            }
+        }
+        sort(dist.begin(),dist.end());
+        if(dist[0]==dist[1]&&dist[1]==dist[2]&&dist[2]==dist[3]&&dist[4]==dist[5]&&dist[4]==2*dist[0]&&dist[0]!=0)
+            return 1;
+        return 0;
+    }
+};
