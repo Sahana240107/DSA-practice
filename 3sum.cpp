a7865ea87ed6cd3549d21faf441279a7412a03ee
Sahana240107 <sahana240107@gmail.com>
2026-01-25 17:24:48 +0530

    3 sum solved

--- 3sum.cpp
+++ 3sum.cpp
@@ -14,3 +14,42 @@ nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
 nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
 The distinct triplets are [-1,0,1] and [-1,-1,2].
 Notice that the order of the output and the order of the triplets does not matter.*/
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-2;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+            int j=i+1,k=n-1;
+            while(j<k)
+            {
+                long long sum=nums[i]+nums[j]+nums[k];
+                if(sum==0)
+                {
+
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])
+                        j++;
+                    while(j<k && nums[k]==nums[k+1])
+                        k--;
+                }
+                else if(sum<0)
+                {
+                    j++;
+                }
+                else
+                {
+                    k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
