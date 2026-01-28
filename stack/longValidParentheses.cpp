76ec3f719c2fb06e2684ff85208010621fe01db1
Sahana240107 <sahana240107@gmail.com>
2026-01-28 23:32:50 +0530

    longest valid parentheses solved using stack

--- stack/longValidParentheses.cpp
+++ stack/longValidParentheses.cpp
@@ -50,3 +50,29 @@ public:
         return maxi;
     }
 };
+
+//Using stack
+
+class Solution {
+  public:
+    int maxLength(string& s) {
+        // code here
+        stack<int> st;
+        st.push(-1);
+        int maxi=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(')
+                st.push(i);
+            else
+            {
+                st.pop();
+                if(st.empty())
+                    st.push(i);
+                else
+                    maxi=max(maxi,i-st.top());
+            }
+        }
+        return maxi;
+    }
+};
