4b5ab21cb660a85d0f7afb577dcc8f029ba92412
Sahana240107 <sahana240107@gmail.com>
2026-01-31 23:14:04 +0530

    minimum k to make array non positive question added

---
+++ Binary Search/minKtoMakeArrayNeg.cpp
@@ -0,0 +1,15 @@
+/* Minimum k to Make Array Non-Positive
+
+You are given a positive integer array nums.
+
+For a positive integer k, define nonPositive(nums, k) as the minimum number of operations needed to make every element of nums non-positive. In one operation, you can choose an index i and reduce nums[i] by k.
+
+Return the minimum value of k such that nonPositive(nums, k) ≤ k².
+
+Example 1
+Input: nums = [3, 7, 5]
+Output: 3
+
+Example 2
+Input: nums = [1]
+Output: 1*/
