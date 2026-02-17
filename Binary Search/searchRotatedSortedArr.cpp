fe44eabe810f424798d8b8ba3d28f2d71e6f9615
Sahana240107 <sahana240107@gmail.com>
2026-02-17 18:40:23 +0530

    search in rotated sorted array question solved

--- Binary Search/searchRotatedSortedArr.cpp
+++ Binary Search/searchRotatedSortedArr.cpp
@@ -17,3 +17,30 @@ Example 2:
 Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
 */
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left=0,right=nums.size()-1;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target)
+                return mid;
+            if(nums[left]<=nums[mid])
+            {
+                if(nums[left]<=target && nums[mid]>target)
+                    right=mid-1;
+                else 
+                    left=mid+1;
+            }
+            else
+            {
+                if(nums[mid]<target && nums[right]>=target)
+                    left=mid+1;
+                else
+                    right=mid-1;
+            }
+        }
+        return -1;
+    }
+};
