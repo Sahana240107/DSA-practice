8472c542b28655b32d2df7aa0666f5862de7ff19
Sahana240107 <sahana240107@gmail.com>
2026-02-17 20:01:13 +0530

    find minimum in rotated sorted array solved

--- Binary Search/minRotatedSortedArr.cpp
+++ Binary Search/minRotatedSortedArr.cpp
@@ -26,3 +26,18 @@ Input: nums = [11,13,15,17]
 Output: 11
 Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
  */
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left=0,right=nums.size()-1;
+        while(left<right)
+        {
+            int mid=left+(right-left)/2;
+            if(nums[mid]>nums[right])
+                left=mid+1;
+            else
+                right=mid;
+        }
+        return nums[left];
+    }
+};
