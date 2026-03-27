fc230c86505a9d8f875979f4ec53c089aa8bf865
Sahana M <sahana240107@gmail.com>
2026-03-27 19:46:15 +0530

    minimum cost climbing stairs solved

--- dp/mimCostStairs.cpp
+++ dp/mimCostStairs.cpp
@@ -4,8 +4,6 @@ You can either start from the step with index 0, or the step with index 1.
 
 Return the minimum cost to reach the top of the floor.
 
- 
-
 Example 1:
 
 Input: cost = [10,15,20]
@@ -26,6 +24,64 @@ Explanation: You will start at index 0.
 - Pay 1 and climb one step to reach the top.
 The total cost is 6.
 */
+
+
+//Recursion
+int minCost(int i,vector<int>& cost)
+{
+    if(i<=0)
+        return 0;
+    int oneStep=minCost(i-1,cost)+cost[i-1];
+    int twoStep=minCost(i-2,cost)+cost[i-2];
+    return min(oneStep,twoStep);
+}
+
+
+//Memoization
+class Solution {
+public:
+    int minCost(int i, vector<int>& cost, vector<int>& dp)
+    {
+        if(i<=0)
+            return 0;
+
+        if(dp[i] != -1)
+            return dp[i];
+
+        int oneStep = minCost(i-1, cost, dp) + cost[i-1];
+        int twoStep = minCost(i-2, cost, dp) + cost[i-2];
+
+        return dp[i] = min(oneStep, twoStep);
+    }
+
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n+1, -1);
+        return minCost(n, cost, dp);
+    }
+};
+
+
+//Tabulation
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        vector<int> dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=0;
+        for(int i=2;i<=n;i++)
+        {
+            int oneStep=dp[i-1]+cost[i-1];
+            int twoStep=dp[i-2]+cost[i-2];
+            dp[i]=min(oneStep,twoStep);
+        }
+        return dp[n];
+    }
+};
+
+
+//Optimal
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
