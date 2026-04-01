44f03fa87bd3a41ff93b1df8f4abd2fe317e8021
Sahana M <sahana240107@gmail.com>
2026-04-01 18:59:56 +0530

    nQueens solved

--- backtracking/nQueens.cpp
+++ backtracking/nQueens.cpp
@@ -13,3 +13,45 @@ Example 2:
 
 Input: n = 1
 Output: [["Q"]]*/
+class Solution {
+public:
+    vector<vector<string>> result;
+    bool isSafe(int r,int c,vector<int>& queens)
+    {
+        for(int preRow=0;preRow<r;preRow++)
+        {
+            int preCol=queens[preRow];
+            if(c==preCol)
+                return false;
+            if(abs(preRow-r)==abs(preCol-c))
+                return false;
+        }
+        return true;
+    }
+    void solve(int r,vector<int>& queens,int n)
+    {
+        if(r==n)
+        {
+            vector<string> board(n,string(n,'.'));
+            for(int i=0;i<n;i++)
+            {
+                board[i][queens[i]]='Q';
+            }
+            result.push_back(board);
+            return;
+        }
+        for(int c=0;c<n;c++)
+        {
+            if(isSafe(r,c,queens))
+            {
+                queens[r]=c;
+                solve(r+1,queens,n);
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<int> queens(n,-1);
+        solve(0,queens,n);
+        return result;
+    }
+};
