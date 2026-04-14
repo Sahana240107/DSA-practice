3e65efba222a717cd394597ceb70d59411ba1735
Sahana M <sahana240107@gmail.com>
2026-04-14 21:46:55 +0530

    sort the half sorted array question added

---
+++ sort/sortHalfSorted.cpp
@@ -0,0 +1,12 @@
+/*Given an integer array of which both the first halve and second halve are sorted. The task is to merge these two sorted halves of the array into a single sorted array.
+Note: The two halves can be of arbitrary sizes (i.e. if first halve of size k then the second halve is of size N-k where 0<=k<=N).
+
+Example 1:
+
+Input:
+N = 6
+arr[] = {2 3 8 -1 7 10}
+Output: -1 2 3 7 8 10 
+Explanation: {2 3 8} and {-1 7 10} are sorted 
+in the original array. The overall sorted 
+version is {-1 2 3 7 8 10}*/
