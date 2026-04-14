345b50900a2194e7b779354b1b67d5de4fd72773
Sahana M <sahana240107@gmail.com>
2026-04-14 22:16:23 +0530

    extract integer question solved

--- Strings/extractInteger.cpp
+++ Strings/extractInteger.cpp
@@ -15,3 +15,21 @@ s = "geeksforgeeks"
 Output: No Integers
 Explanation: 
 No integers present in the string.*/
+vector<string> extractIntegerWords(string s) {
+    // code here
+    vector<string> ans;
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]>='0' && s[i]<='9')
+        {
+            string num=string(1,s[i]);
+            while(s[i+1]>='0' && s[i+1]<='9')
+            {
+                num+=s[i+1];
+                i++;
+            }
+            ans.push_back(num);
+        }
+    }
+    return ans;
+}
