4a8cd04ec2ccc9af2914a6c22b450b4dc90c94ed
Sahana M <sahana240107@gmail.com>
2026-04-14 22:15:51 +0530

    extract integers question added

---
+++ Strings/extractInteger.cpp
@@ -0,0 +1,17 @@
+/*Given a string s, extract all the integers from s.
+
+Example 1:
+
+Input:
+s = "1: Prakhar Agrawal, 2: Manish Kumar Rai, 
+     3: Rishabh Gupta56"
+Output: 1 2 3 56
+Explanation: 
+1, 2, 3, 56 are the integers present in s.
+Example 2:
+
+Input:
+s = "geeksforgeeks"
+Output: No Integers
+Explanation: 
+No integers present in the string.*/
