96637bc6745f0ff96f5af44c8eb610964add710b
Sahana M <sahana240107@gmail.com>
2026-04-14 23:15:40 +0530

    count pairs in sorted array whose sum is less than target solved

--- Two Pointers/countPairSortedArrSumLessThanTarget.cpp
+++ Two Pointers/countPairSortedArrSumLessThanTarget.cpp
@@ -9,3 +9,25 @@ Input: arr[] = [1, 2, 3, 4, 5, 6, 7, 8], target = 7
 Output: 6
 Explanation: The pairs are (1, 2), (1, 3), (1, 4), (1, 5), (2, 3) and (2, 4)
 */
+
+// User function Template for C++
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+        // Your code here
+        int i=0,j=arr.size()-1;
+        int count=0;
+        while(i<j)
+        {
+            int sum=arr[i]+arr[j];
+            if(sum<target)
+            {
+                count+=(j-i);
+                i++;
+            }
+            else
+                j--;
+        }
+        return count;
+    }
+};
