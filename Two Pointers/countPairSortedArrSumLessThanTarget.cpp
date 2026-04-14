90039f06103a848218fc6ad36eb7b039c8cd2abf
Sahana M <sahana240107@gmail.com>
2026-04-14 23:14:37 +0530

    count pairs in sorted array whose sum is less than target question added

---
+++ Two Pointers/countPairSortedArrSumLessThanTarget.cpp
@@ -0,0 +1,11 @@
+/*Given an array arr[] and an integer target. You have to find the number of pairs in the array whose sum is strictly less than the target.
+
+Examples:
+
+Input: arr[] = [2, 3, 5, 7], target = 8
+Output: 2
+Explanation: The pairs are (2, 3) and (2, 5).
+Input: arr[] = [1, 2, 3, 4, 5, 6, 7, 8], target = 7
+Output: 6
+Explanation: The pairs are (1, 2), (1, 3), (1, 4), (1, 5), (2, 3) and (2, 4)
+*/
