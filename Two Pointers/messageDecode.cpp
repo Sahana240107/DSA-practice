694e4ca065f92d0744b58070da9c029069f83560
Sahana M <sahana240107@gmail.com>
2026-04-14 22:00:31 +0530

    message decoding question added

---
+++ Two Pointers/messageDecode.cpp
@@ -0,0 +1,20 @@
+/*Given a string S, check whether it can be converted into the string "hello" by deleting some characters from it.
+Note : S can have both uppercase and lowercase letters.
+
+Example 1:
+
+Input:
+S = "bbbbbxxhhelllllooudd"
+Output: 1
+Explanation: The string hello is marked
+in bold: bbbbbxxhhelllllooudd
+
+â€‹Example 2:
+
+Input: 
+S = "hlelo"
+Output: 0
+Explanation: It's impossible to convert
+the given string into "hello".
+
+*/
