bfecd4ff05e619f3b3e11a24b35301b2387b5e59
Sahana M <sahana240107@gmail.com>
2026-04-15 11:43:12 +0530

    closest two sum question solved

--- Two Pointers/closestTwoSum.cpp
+++ Two Pointers/closestTwoSum.cpp
@@ -10,3 +10,40 @@ Input: arr[] = [5, 2, 7, 1, 4], target = 10
 Output: [2, 7]
 Explanation: As (4, 5), (2, 7) and (4, 7) both are closest to 10, but absolute difference of (4, 5) is 1, (2, 7) is 5 and (4, 7) is 3. Hence, [2, 7] has maximum absolute difference and closest to target. 
 */
+
+class Solution {
+public:
+    vector<int> sumClosest(vector<int>& arr, int target) {
+        if(arr.size() < 2)
+            return {};
+
+        sort(arr.begin(), arr.end());
+
+        int i = 0, j = arr.size() - 1;
+
+        int closeDif = INT_MAX;
+        vector<int> close;
+
+        while(i < j) {
+            int sum = arr[i] + arr[j];
+            int diff = abs(target - sum);
+
+            if(diff < closeDif) {
+                closeDif = diff;
+                close = {arr[i], arr[j]};
+            }
+            else if(diff == closeDif) {
+                // max absolute difference
+                if((arr[j] - arr[i]) > (close[1] - close[0])) {
+                    close = {arr[i], arr[j]};
+                }
+            }
+            if(sum < target)
+                i++;
+            else
+                j--;
+        }
+
+        return close;
+    }
+};
