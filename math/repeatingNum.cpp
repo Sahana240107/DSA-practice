2e482f3331c7ddc73db339a4e7f57ee297f285fa
Sahana M <sahana240107@gmail.com>
2026-04-15 18:20:34 +0530

    find repeating number question added

---
+++ math/repeatingNum.cpp
@@ -0,0 +1,13 @@
+/*Given an array arr[] of size n, filled with numbers from 1 to n-1 in random order. The array has only one repetitive element. Your task is to find the repetitive element.
+
+Note: It is guaranteed that there is a repeating element present in the array.
+
+Examples:
+
+Input: arr[] = [1, 3, 2, 3, 4]
+Output: 3 
+Explanation: The number 3 is the only repeating element.
+Input: arr[] = [1, 5, 1, 2, 3, 4]
+Output: 1  
+Explanation: The number 1 is the only repeating element.
+*/
