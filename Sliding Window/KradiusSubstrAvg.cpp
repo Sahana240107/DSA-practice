facbefcbd4d3c688b1c7513d3f296c3b2d315284
Sahana M <sahana240107@gmail.com>
2026-05-22 21:31:36 +0530

    k radius substring average solved

--- Sliding Window/KradiusSubstrAvg.cpp
+++ Sliding Window/KradiusSubstrAvg.cpp
@@ -34,3 +34,28 @@ Input: nums = [8], k = 100000
 Output: [-1]
 Explanation: 
 - avg[0] is -1 because there are less than k elements before and after index 0.*/
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& nums, int k) {
+        long long sum=0;
+        int size=2*k+1;
+        int n=nums.size();
+        if(size>n)
+        {
+            vector<int> res(n,-1);
+            return res;
+        }
+        vector<int> ans(n,-1);
+        for(int i=0;i<size;i++)
+        {
+            sum +=nums[i];
+        }
+        ans[k]=sum/size;
+        for(int i=size;i<n;i++)
+        {
+            sum+=nums[i]-nums[i-size];
+            ans[i-k]=sum/size;
+        }
+        return ans;
+    }
+};
