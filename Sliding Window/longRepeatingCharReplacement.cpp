949bc7939076463a5d448dc10faf3d7a27ecdc01
Sahana M <sahana240107@gmail.com>
2026-05-26 21:32:35 +0530

    longest repeating character replacement solved

--- Sliding Window/longRepeatingCharReplacement.cpp
+++ Sliding Window/longRepeatingCharReplacement.cpp
@@ -17,3 +17,22 @@ Output: 4
 Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
 The substring "BBBB" has the longest repeating letters, which is 4.
 There may exists other ways to achieve this answer too.*/
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int start=0,len=0,freq=0;
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.size();i++)
+        {
+            mp[s[i]]++;
+            freq=max(freq,mp[s[i]]);
+            while((i-start+1)-freq>k)
+            {
+                mp[s[start]]--;
+                start++;
+            }
+            len=max(len,i-start+1);
+        }
+        return len;
+    }
+};
