ee0effb20e0db8f0fc93addbfaa3acc3933b1292
Sahana M <sahana240107@gmail.com>
2026-05-30 22:51:20 +0530

    minimum length of string after deleting similar ends solved using two pointer

--- Two Pointers/minLenAfterDeletingSimilarEnds.cpp
+++ Two Pointers/minLenAfterDeletingSimilarEnds.cpp
@@ -55,3 +55,21 @@ public:
         return right-left+1;
     }
 };
+
+//Perfect Two Pointer Approach
+class Solution {
+public:
+    int minimumLength(string s) {
+        int n=s.size();
+        int left=0,right=n-1;
+        while(left<right && s[left]==s[right])
+        {
+            char c=s[left];
+            while(left<=right && s[left]==c)
+                left++;
+            while(left<=right && s[right]==c)
+                right--;
+        }
+        return right-left+1;
+    }
+};
