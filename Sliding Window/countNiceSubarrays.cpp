a985ea161c6ad71a1457488aeb3eda4d511b611c
Sahana M <sahana240107@gmail.com>
2026-06-01 22:32:18 +0530

    count nice subarrays solved

--- Sliding Window/countNiceSubarrays.cpp
+++ Sliding Window/countNiceSubarrays.cpp
@@ -20,3 +20,28 @@ Example 3:
 Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
 Output: 16
  */
+ class Solution {
+public:
+    int atmostKodd(vector<int>& nums,int k)
+    {
+        if(k<0)
+            return 0;
+        int left=0,count=0,odds=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]%2!=0)
+                odds++;
+            while(odds>k)
+            {
+                if(nums[left]%2!=0)
+                    odds--;
+                left++;
+            }
+            count+=(i-left+1);
+        }
+        return count;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return atmostKodd(nums,k)-atmostKodd(nums,k-1);
+    }
+};
