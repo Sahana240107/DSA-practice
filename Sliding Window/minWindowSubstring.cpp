96ac6c445023f9c86b5707d44fb135d7386dab85
Sahana M <sahana240107@gmail.com>
2026-06-02 22:04:23 +0530

    minimum window substring solved

--- Sliding Window/minWindowSubstring.cpp
+++ Sliding Window/minWindowSubstring.cpp
@@ -22,3 +22,42 @@ Input: s = "a", t = "aa"
 Output: ""
 Explanation: Both 'a's from t must be included in the window.
 Since the largest window of s only has one 'a', return empty string.*/
+class Solution {
+public:
+    string minWindow(string s, string t) {
+       
+        int n=s.size(),m=t.size();
+        if(m>n)
+            return "";
+        unordered_map<char,int> mp;
+        for(char c:t)
+            mp[c]++;
+        int left=0,min_len=INT_MAX,start=0;
+        for(int i=0;i<n;i++)
+        {
+            if(mp.find(s[i])!=mp.end())
+            {
+                mp[s[i]]--;
+                if(mp[s[i]]>=0)
+                    m--;
+            }
+            while(m==0)
+            {
+                int len=i-left+1;
+                if(len<min_len)
+                {
+                    min_len=len;
+                    start=left;
+                }
+                if(mp.find(s[left])!=mp.end())
+                {
+                    mp[s[left]]++;
+                    if(mp[s[left]]>0)
+                        m++;
+                }
+                left++;
+            }
+        }
+        return min_len==INT_MAX?"":s.substr(start,min_len);
+    }
+};
