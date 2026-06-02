82a7075d5636161bb13d32d0d067dd281b856081
Sahana M <sahana240107@gmail.com>
2026-06-02 22:28:34 +0530

    minimum distinct window solved

--- Sliding Window/minDistinctWindow.cpp
+++ Sliding Window/minDistinctWindow.cpp
@@ -13,3 +13,44 @@ Explanation: Sub-String "ab" has the smallest length that contains all the chara
 Input: str = "geeksforgeeks"
 Output: 7
 Explanation: There are multiple substring with smallest length that contains all characters of str, "eksforg" and "ksforge".*/
+
+class Solution {
+  public:
+    int findSubString(string& str) {
+        // code here
+        unordered_map<char,int> mp;
+        for(char c:str)
+        {
+            mp[c]=1;
+        }
+        int n=mp.size();
+        int left=0,start=0,min_len=INT_MAX;
+        for(int i=0;i<str.size();i++)
+        {
+            if(mp.find(str[i])!=mp.end())
+            {
+                mp[str[i]]--;
+                if(mp[str[i]]>=0)
+                    n--;
+            }
+            while(n==0)
+            {
+                int len=i-left+1;
+                if(min_len>len)
+                {
+                    min_len=len;
+                    start=left;
+                }
+                if(mp.find(str[left])!=mp.end())
+                {
+                    mp[str[left]]++;
+                    if(mp[str[left]]>0)
+                        n++;
+            
+                }
+                left++;
+            }
+        }
+        return min_len==INT_MAX?0:min_len;
+    }
+};
