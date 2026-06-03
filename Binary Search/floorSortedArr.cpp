13e0823c12ce27d3c7e573e5769e21cb57bae4a2
Sahana M <sahana240107@gmail.com>
2026-06-03 21:04:15 +0530

    floor sorted array solved

--- Binary Search/floorSortedArr.cpp
+++ Binary Search/floorSortedArr.cpp
@@ -20,3 +20,22 @@ Constraints:
 1 ≤ arr[i] ≤ 106
 0 ≤ x ≤ arr[n-1]
 */
+class Solution {
+  public:
+    int findFloor(vector<int>& arr, int x) {
+        // code here
+        int low=0,high=arr.size()-1,ans=-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(arr[mid]<=x)
+            {
+                ans=mid;
+                low=mid+1;
+            }
+            else
+                high=mid-1;
+        }
+        return ans;
+    }
+};
