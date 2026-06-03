5861455358fa9484aab26f192cd0f71e671af010
Sahana M <sahana240107@gmail.com>
2026-06-03 11:31:02 +0530

    substring with k distinct chars solved

--- Sliding Window/substrWithKDistinctChars.cpp
+++ Sliding Window/substrWithKDistinctChars.cpp
@@ -13,3 +13,34 @@ Explanation: Possible substrings are ["ab", "ba", "aba"]
 Input: s = "aa", k = 1
 Output: 3
 Explanation: Possible substrings are ["a", "a", "aa"]*/
+class Solution {
+  public:
+    int atmostKDistinct(string &s,int k)
+    {
+        if(k<=0)
+            return 0;
+        vector<int> mp(26,0);
+        int left=0,distinct=0;
+        long long count=0;
+        for(int i=0;i<s.size();i++)
+        {
+            mp[s[i]-'a']++;
+            if(mp[s[i]-'a']==1)
+                distinct++;
+            while(distinct>k)
+            {
+                mp[s[left]-'a']--;
+                if(mp[s[left]-'a']==0)
+                    distinct--;
+                left++;
+            }
+            int len=i-left+1;
+            count+=len;
+        }
+        return count;
+    }
+    int countSubstr(string& s, int k) {
+        // code here
+        return atmostKDistinct(s,k)-atmostKDistinct(s,k-1);
+    }
+};
