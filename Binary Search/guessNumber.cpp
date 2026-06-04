8cd08215bb9ea895bd816079ddf804b314cf30cd
Sahana M <sahana240107@gmail.com>
2026-06-04 11:47:51 +0530

    guess number solved

--- Binary Search/guessNumber.cpp
+++ Binary Search/guessNumber.cpp
@@ -29,3 +29,29 @@ Example 3:
 
 Input: n = 2, pick = 1
 Output: 1*/
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low=1,high=n,ans=-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(guess(mid)==0)
+                return mid;
+            else if(guess(mid)==1)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return ans;
+    }
+};
