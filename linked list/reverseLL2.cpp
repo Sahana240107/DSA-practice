8b05435b185c926ffac496fa5c235c5b15eba942
Sahana M <sahana240107@gmail.com>
2026-06-04 22:02:22 +0530

    reverse linked list 2 solved

--- linked list/reverseLL2.cpp
+++ linked list/reverseLL2.cpp
@@ -9,3 +9,44 @@ Example 2:
 
 Input: head = [5], left = 1, right = 1
 Output: [5]*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(!head)
+            return NULL;
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        ListNode* before=dummy;
+        ListNode* curr=head;
+        int pos=1;
+        while(curr && pos<left)
+        {
+            pos++;
+            before=curr;
+            curr=curr->next;
+        }
+        ListNode* temp=curr;
+        ListNode* prev=NULL;
+        while(temp!=NULL && pos<=right)
+        {
+            pos++;
+            ListNode* front=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;
+        }
+        before->next=prev;
+        curr->next=temp;
+        return dummy->next;
+    }
+};
