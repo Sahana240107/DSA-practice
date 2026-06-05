34db9bf16c6544321941de264fbf5cca7fb795ee
Sahana M <sahana240107@gmail.com>
2026-06-05 21:42:04 +0530

    min days to make bouquet solved

--- Binary Search/minDaysToMakeBouquets.cpp
+++ Binary Search/minDaysToMakeBouquets.cpp
@@ -32,3 +32,39 @@ After day 7: [x, x, x, x, _, x, x]
 We can make one bouquet of the first three flowers that bloomed. We cannot make another bouquet from the last three flowers that bloomed because they are not adjacent.
 After day 12: [x, x, x, x, x, x, x]
 It is obvious that we can make two bouquets in different ways.*/
+class Solution {
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int maxi=INT_MIN;
+        for(int i=0;i<bloomDay.size();i++)
+        {
+            maxi=max(maxi,bloomDay[i]);
+        }
+        int low=1,high=maxi,ans=-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            int count=0,con=0;
+            for(int i=0;i<bloomDay.size();i++)
+            {
+                if(bloomDay[i]<=mid)
+                    con++;
+                else
+                    con=0;
+                if(con==k)
+                {
+                    count++;
+                    con=0;
+                }
+            }
+            if(count>=m)
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+                low=mid+1;
+        }
+        return ans;
+    }
+};
