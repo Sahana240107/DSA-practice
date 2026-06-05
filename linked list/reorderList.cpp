6ca7be7aa51f10e099e4c4aebaf668923d891034
Sahana M <sahana240107@gmail.com>
2026-06-05 20:35:50 +0530

    reorder list solved

--- linked list/reorderList.cpp
+++ linked list/reorderList.cpp
@@ -13,6 +13,38 @@ Input: head = [1,2,3,4]
 Output: [1,4,2,3]
 Example 2:
 
-
 Input: head = [1,2,3,4,5]
 Output: [1,5,2,4,3]*/
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast && fast->next)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* temp=slow->next;
+        slow->next=NULL;
+        ListNode* prev=NULL;
+        while(temp)
+        {
+            ListNode* front=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;
+        }
+        ListNode* first=head;
+        ListNode* second=prev;
+        while(second)
+        {
+            ListNode* t1=first->next;
+            ListNode* t2=second->next;
+            first->next=second;
+            second->next=t1;
+            first=t1;
+            second=t2;
+        }
+    }
+};
