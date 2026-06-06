b1cd2099bf459f7165e70af84af99e5c5f53f196
Sahana M <sahana240107@gmail.com>
2026-06-06 11:59:01 +0530

    peakElement question solved

--- Binary Search/peakElement.cpp
+++ Binary Search/peakElement.cpp
@@ -24,3 +24,18 @@ Example 2:
 Input: nums = [1,2,1,3,5,6,4]
 Output: 5
 Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.*/
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low=0,high=nums.size()-1;
+        while(low<high)
+        {
+            int mid=low+(high-low)/2;
+            if(nums[mid]>nums[mid+1])
+                high=mid;
+            else
+                low=mid+1;
+        }
+        return low;
+    }
+};
