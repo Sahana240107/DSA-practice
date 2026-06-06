f7e384f113c7eee5e1b6a714fbf700ac26a5a8a1
Sahana M <sahana240107@gmail.com>
2026-06-06 23:27:44 +0530

    single element in sorted array solved

--- Binary Search/singleElementSortedArray.cpp
+++ Binary Search/singleElementSortedArray.cpp
@@ -9,3 +9,27 @@ Explanation: Only the number 4 appears once in the array.
 Input: arr[] = {1,1,3,5,5}
 Output : 3
 Explanation: Only the number 3 appears once in the array.`***/
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        if(nums.size()==1)
+            return nums[0];
+        int n=nums.size();
+        if(nums[0]!=nums[1])
+            return nums[0];
+        if(nums[n-1]!=nums[n-2])
+            return nums[n-1];
+        int low=1,high=nums.size()-2,ans=-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
+                return nums[mid];
+            else if(mid%2==1 && nums[mid]!=nums[mid-1] || mid%2==0 && nums[mid]!=nums[mid+1])
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        return ans;
+    }
+};
