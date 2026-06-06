16414ef496975c99bf50099bc78486222e3fe9e6
Sahana M <sahana240107@gmail.com>
2026-06-06 23:19:25 +0530

    single element in sorted array added

---
+++ Binary Search/singleElementSortedArray.cpp
@@ -0,0 +1,11 @@
+/*# **Search Single Element in a sorted array**
+
+**Problem Statement: Given an array of N integers. Every number in the array except one appears twice. Find the single number in the array.Examples
+
+`Input : arr[] = {1,1,2,2,3,3,4,5,5,6,6}
+Output: 4
+Explanation: Only the number 4 appears once in the array.
+
+Input: arr[] = {1,1,3,5,5}
+Output : 3
+Explanation: Only the number 3 appears once in the array.`***/
