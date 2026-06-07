85865df74388e060931149fe29eb70bf84686538
Sahana M <sahana240107@gmail.com>
2026-06-07 17:47:03 +0530

    length of loop added

---
+++ linked list/lengthLoop.cpp
@@ -0,0 +1,3 @@
+/*# **Length of Loop in Linked List**
+
+**Problem Statement: Given the head of a linked list, determine the length of a loop present in the linked list. If there's no loop present, return 0.***/
