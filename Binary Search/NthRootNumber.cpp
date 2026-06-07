2c163207301d0bc764dcaf98d6dda1a8d117b611
Sahana M <sahana240107@gmail.com>
2026-06-07 21:37:11 +0530

    Nth root of a number solved

--- Binary Search/NthRootNumber.cpp
+++ Binary Search/NthRootNumber.cpp
@@ -4,3 +4,29 @@
 
 `Input: N = 3, M = 27Output: 3Explanation: The cube root of 27 is equal to 3.
 Input : N = 4, M = 69Output: -1Explanation : The 4th root of 69 does not exist. So, the answer is -1.`***/
+
+class Solution {
+public:
+    // Function to find N-th root of M using binary search
+    int nthRoot(int n, int m) {
+       int low=1,high=m;
+       while(low<=high)
+       {
+        int mid=low+(high-low)/2;
+        long long ans=1;
+        for(int i=0;i<n;i++)
+        {
+            ans*=mid;
+            if(ans>m)
+                break;
+        }
+        if(ans==m)
+            return mid;
+        else if(ans<m)
+            low=mid+1;
+        else   
+            high=mid-1;
+       }
+       return -1;
+    }
+};
