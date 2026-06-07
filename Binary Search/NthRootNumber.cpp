e1f0cc401583f3134be52c4afc7bd89097796a53
Sahana M <sahana240107@gmail.com>
2026-06-07 21:33:14 +0530

    Nth root of a number added

---
+++ Binary Search/NthRootNumber.cpp
@@ -0,0 +1,6 @@
+/*# **Nth Root of a Number using Binary Search**
+
+**Problem Statement: Given two numbers N and M, find the Nth root of M. The nth root of a number M is defined as a number X when raised to the power N equals M. If the 'nth root is not an integer, return -1.Examples
+
+`Input: N = 3, M = 27Output: 3Explanation: The cube root of 27 is equal to 3.
+Input : N = 4, M = 69Output: -1Explanation : The 4th root of 69 does not exist. So, the answer is -1.`***/
