18f4a7a9acbb6398e8dd015142cad1db24295bc8
Sahana M <sahana240107@gmail.com>
2026-06-08 13:18:47 +0530

    allDivisors added

---
+++ math/allDivisors.cpp
@@ -0,0 +1,3 @@
+/*Print all Divisors of a given Number
+Problem Statement: Given an integer N, return all divisors of N.
+A divisor of an integer N is a positive integer that divides N without leaving a remainder. In other words, if N is divisible by another integer without any remainder, then that integer is considered a divisor of N*/
