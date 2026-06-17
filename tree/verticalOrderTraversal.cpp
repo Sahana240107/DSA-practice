ebf352e70cef09a5b4a0474cac42be9f54726024
Sahana M <sahana240107@gmail.com>
2026-06-17 22:49:45 +0530

    vertical order traversal gfg version solved

--- tree/verticalOrderTraversal.cpp
+++ tree/verticalOrderTraversal.cpp
@@ -7,3 +7,33 @@ Examples:
 Input: root = [1, 2, 3, 4, 5, 6, 7, N, N, N, 8, N, 9, N, 10, 11, N]
                     
 Output: [[4], [2], [1, 5, 6, 11], [3, 8, 9], [7], [10]]*/
+class Solution {
+  public:
+    vector<vector<int>> verticalOrder(Node *root) {
+        // code here
+        unordered_map<int,vector<int>> mp;
+        queue<pair<Node*,int>> q;
+        q.push({root,0});
+        int mini=INT_MAX;
+        int maxi=INT_MIN;
+        while(!q.empty())
+        {
+            Node* node=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            maxi=max(maxi,col);
+            mini=min(mini,col);
+            mp[col].push_back(node->data);
+            if(node->left)
+                q.push({node->left,col-1});
+            if(node->right)
+                q.push({node->right,col+1});
+        }
+        vector<vector<int>> ans;
+        for(int i=mini;i<=maxi;i++)
+        {
+            ans.push_back(mp[i]);
+        }
+        return ans;
+    }
+};
