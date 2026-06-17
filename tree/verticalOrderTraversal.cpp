b04b2ac1897a8379314c8925589b3aacc13a4511
Sahana M <sahana240107@gmail.com>
2026-06-17 22:49:14 +0530

    vertical order traversal gfg version added

---
+++ tree/verticalOrderTraversal.cpp
@@ -0,0 +1,9 @@
+/*Given the root of a Binary Tree, find the vertical traversal of the tree starting from the leftmost level to the rightmost level.
+
+Note: If there are multiple nodes passing through a vertical line, then they should be printed as they appear in level order traversal of the tree.
+
+Examples:
+
+Input: root = [1, 2, 3, 4, 5, 6, 7, N, N, N, 8, N, 9, N, 10, 11, N]
+                    
+Output: [[4], [2], [1, 5, 6, 11], [3, 8, 9], [7], [10]]*/
