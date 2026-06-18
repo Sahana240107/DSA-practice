4dc6a572ed2dcaba7a4f7d6e810b755b73f94faf
Sahana M <sahana240107@gmail.com>
2026-06-18 12:24:01 +0530

    swap nodes in pairs recursive solution

--- linked list/swapNodesPairs.cpp
+++ linked list/swapNodesPairs.cpp
@@ -27,3 +27,18 @@ public:
         return dummy.next;
     }
 };
+
+
+//RECURSIVE
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head||!head->next)
+            return head;
+        ListNode* first=head;
+        ListNode* second=head->next;
+        first->next=swapPairs(second->next);
+        second->next=first;
+        return second;
+    }
+};
