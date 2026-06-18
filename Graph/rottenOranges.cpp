9a4090921db4734d8e3d570e6c683d948d0aa61f
Sahana M <sahana240107@gmail.com>
2026-06-18 21:39:10 +0530

    rotten oranges solved

--- Graph/rottenOranges.cpp
+++ Graph/rottenOranges.cpp
@@ -20,3 +20,52 @@ Example 3:
 Input: grid = [[0,2]]
 Output: 0
 Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.*/
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int maxi=0;
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        int orange=0;
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    visited[i][j]=2;
+                    q.push({{i,j},0});
+                }
+                else if(grid[i][j]==1)
+                    orange++;
+                else
+                    visited[i][j]=0;
+            }
+        }
+        while(!q.empty())
+        {
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int t=q.front().second;
+            q.pop();
+            maxi=max(maxi,t);
+            int dr[]={0,0,-1,1};
+            int dc[]={-1,1,0,0};
+            for(int k=0;k<4;k++)
+            {
+                int nr=r+dr[k];
+                int nc=c+dc[k];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1 && !visited[nr][nc])
+                {
+                    q.push({{nr,nc},t+1});
+                    orange--;
+                    visited[nr][nc]=2;
+                }
+            }
+        }
+        if(orange!=0)
+            return -1;
+        return maxi;
+    }
+};
