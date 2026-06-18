96211671b40a4878237cdab017f708f0d37418c5
Sahana M <sahana240107@gmail.com>
2026-06-18 13:12:55 +0530

    remove outermost parentheses optimized

--- Strings/removeOutermostParentheses.cpp
+++ Strings/removeOutermostParentheses.cpp
@@ -79,3 +79,22 @@ public:
         return ans;
     }
 };
+
+//WITHOUT SUBSTR OPTIMIZED
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans="";
+        int count=0;
+        for(char c:s)
+        {
+            if(c==')')
+                count--;
+            if(count!=0)
+                ans+=c;
+            if(c=='(')
+                count++;
+        }
+        return ans;
+    }
+};
