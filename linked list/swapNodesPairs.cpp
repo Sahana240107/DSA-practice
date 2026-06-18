15126e5e8aa22b51232f71dde3c10a6998194c93
Sahana M <sahana240107@gmail.com>
2026-06-18 12:20:21 +0530

    swap nodes in pairs iterative solution

--- linked list/swapNodesPairs.cpp
+++ linked list/swapNodesPairs.cpp
@@ -9,3 +9,21 @@ Output: [2,1,4,3]
 Explanation:
 
 */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode dummy(0);
+        dummy.next=head;
+        ListNode* prev=&dummy;
+        while(prev->next && prev->next->next)
+        {
+            ListNode* first=prev->next;
+            ListNode* second=first->next;
+            first->next=second->next;
+            second->next=first;
+            prev->next=second;
+            prev=first;
+        }
+        return dummy.next;
+    }
+};
