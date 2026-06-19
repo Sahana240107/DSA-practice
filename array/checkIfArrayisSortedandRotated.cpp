d93c6f89baeac46266bbcde99e1567d09b1c0359
Sahana M <sahana240107@gmail.com>
2026-06-19 09:48:40 +0530

    check if array is sorted and rotated solved

--- array/checkIfArrayisSortedandRotated.cpp
+++ array/checkIfArrayisSortedandRotated.cpp
@@ -31,3 +31,16 @@ Output: true
 Explanation: [1,2,3] is the original sorted array.
 You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
 */
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int drop=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>nums[(i+1)%n])
+                drop++;
+        }
+        return (drop>1)?false:true;
+    }
+};
