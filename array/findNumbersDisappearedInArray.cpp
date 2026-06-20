8e70e658cb66cd03f7fb111bee9ec64bc7060400
Sahana M <sahana240107@gmail.com>
2026-06-20 21:26:35 +0530

    find numbers disappeared in array solved

--- array/findNumbersDisappearedInArray.cpp
+++ array/findNumbersDisappearedInArray.cpp
@@ -9,3 +9,21 @@ Example 2:
 
 Input: nums = [1,1]
 Output: [2]*/
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++)
+        {
+            int ind=abs(nums[i])-1;
+            if(nums[ind]>0)
+                nums[ind]=-nums[ind];
+        }
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>0)
+                ans.push_back(i+1);
+        }
+        return ans;
+    }
+};
