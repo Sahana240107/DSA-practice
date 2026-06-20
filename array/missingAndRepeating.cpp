479af1a64c0ac5a603fb3d694141bbf24bce5233
Sahana M <sahana240107@gmail.com>
2026-06-20 21:58:02 +0530

    missing and repeating solved

--- array/missingAndRepeating.cpp
+++ array/missingAndRepeating.cpp
@@ -13,3 +13,25 @@ Explanation: Repeating number is 3 and the missing number is 2.
 Input: arr[] = [4, 3, 6, 2, 1, 1]
 Output: [1, 5]
 Explanation: Repeating number is 1 and the missing number is 5.*/
+
+class Solution {
+  public:
+    vector<int> findTwoElement(vector<int>& arr) {
+        // code here
+        int mis=-1,dup=-1;
+        for(int i=0;i<arr.size();i++)
+        {
+            int ind=abs(arr[i])-1;
+            if(arr[ind]>0)  
+                arr[ind]=-arr[ind];
+            else
+                dup=ind+1;
+        }
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]>0)
+                mis=i+1;
+        }
+        return {dup,mis};
+    }
+};
