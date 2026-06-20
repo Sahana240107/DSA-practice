e2249e449032a37fdffc9ec2f11abd724123ebc5
Sahana M <sahana240107@gmail.com>
2026-06-20 23:07:59 +0530

    partition list solved

--- linked list/partitionList.cpp
+++ linked list/partitionList.cpp
@@ -12,3 +12,40 @@ Example 2:
 
 Input: head = [2,1], x = 2
 Output: [1,2]*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode less(0);
+        ListNode greater(0);
+        ListNode* t1=&less;
+        ListNode* t2=&greater;
+        ListNode* cur=head;
+        while(cur)
+        {
+            if(cur->val<x)
+            {
+                t1->next=cur;
+                t1=t1->next;
+            }
+            else
+            {
+                t2->next=cur;
+                t2=t2->next;
+            }
+            cur=cur->next;
+        }
+        t2->next=NULL;
+        t1->next=greater.next;
+        return less.next;
+    }
+};
