a3b90ad088956c028b95c2a3056ddbbd4c4946c4
Sahana M <sahana240107@gmail.com>
2026-06-20 21:57:30 +0530

    missing and repeating added

---
+++ array/missingAndRepeating.cpp
@@ -0,0 +1,15 @@
+/*Missing And Repeating
+Difficulty: EasyAccuracy: 24.83%Submissions: 710K+Points: 2Average Time: 30m
+Given an unsorted array arr[] of size n, containing elements from the range 1 to n, it is known that one number in this range is missing, and another number occurs twice in the array, find both the duplicate number and the missing number.
+
+Examples:
+
+Input: arr[] = [2, 2]
+Output: [2, 1]
+Explanation: Repeating number is 2 and the missing number is 1.
+Input: arr[] = [1, 3, 3] 
+Output: [3, 2]
+Explanation: Repeating number is 3 and the missing number is 2.
+Input: arr[] = [4, 3, 6, 2, 1, 1]
+Output: [1, 5]
+Explanation: Repeating number is 1 and the missing number is 5.*/
