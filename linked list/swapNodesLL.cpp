2011bace6e1692c4876389759c90c9fdf0d9d0f5
Sahana M <sahana240107@gmail.com>
2026-06-21 17:35:12 +0530

    swap nodes solved

--- linked list/swapNodesLL.cpp
+++ linked list/swapNodesLL.cpp
@@ -12,3 +12,37 @@ Example 2:
 
 Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
 Output: [7,9,6,6,8,7,3,0,9,5]*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode* t1=head;
+        int i=1;
+        while(t1 && i<k)
+        {
+            t1=t1->next;
+            i++;
+        }
+        ListNode* x=t1;
+        ListNode* t2=head;
+        while(t1->next)
+        {
+            t2=t2->next;
+            t1=t1->next;
+        }
+        ListNode* y=t2;
+        int temp=x->val;
+        x->val=y->val;
+        y->val=temp;
+        return head;
+    }
+};
