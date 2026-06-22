f1cfbabb2544172651ae2461727e4f2da05942a2
Sahana M <sahana240107@gmail.com>
2026-06-22 23:32:19 +0530

    remove adjacent duplicates optimized

--- stack/removeAdjacentDuplicates.cpp
+++ stack/removeAdjacentDuplicates.cpp
@@ -40,3 +40,22 @@ public:
         return ans;
     }
 };
+
+//Optimized
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans;
+        int i=0,n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            if(!ans.empty()&& ans.back()==s[i])
+            {
+                ans.pop_back();
+            }
+            else
+                ans.push_back(s[i]);
+        }
+        return ans;
+    }
+};
