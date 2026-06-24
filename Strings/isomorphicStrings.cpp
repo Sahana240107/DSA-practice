fd5d7824b1ef56d754425398745acc270c8e9c09
Sahana M <sahana240107@gmail.com>
2026-06-24 23:28:20 +0530

    isomorphic strings solved

--- Strings/isomorphicStrings.cpp
+++ Strings/isomorphicStrings.cpp
@@ -32,3 +32,19 @@ Example 3:
 Input: s = "paper", t = "title"
 
 Output: true*/
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<int> m1(256,-1);
+        vector<int> m2(256,-1);
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            if(m1[s[i]]!=m2[t[i]])
+                return false;
+            m1[s[i]]=i;
+            m2[t[i]]=i;
+        }
+        return true;
+    }
+};
