48c7369bb2fa8bfd3e7717d7624525e7372dce6c
Sahana M <sahana240107@gmail.com>
2026-06-24 23:01:54 +0530

    sum of subarray ranges solved

--- stack/sumOfSubarrayRanges.cpp
+++ stack/sumOfSubarrayRanges.cpp
@@ -5,8 +5,6 @@ Return the sum of all subarray ranges of nums.
 
 A subarray is a contiguous non-empty sequence of elements within an array.
 
- 
-
 Example 1:
 
 Input: nums = [1,2,3]
@@ -36,3 +34,97 @@ Example 3:
 Input: nums = [4,-2,-3,4,1]
 Output: 59
 Explanation: The sum of all subarray ranges of nums is 59.*/
+class Solution {
+    vector<int> findnse(vector<int>& a)
+    {
+        int n=a.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.empty() && a[st.top()]>=a[i])
+                st.pop();
+            ans[i]=st.empty()?n:st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int> findpse(vector<int>& a)
+    {
+        int n=a.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && a[st.top()]>a[i])
+                st.pop();
+            ans[i]=st.empty()?-1:st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int> findnge(vector<int>& a)
+    {    
+        int n=a.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.empty() && a[st.top()]<=a[i])
+                st.pop();
+            ans[i]=st.empty()?n:st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int> findpge(vector<int>& a)
+    {
+        int n=a.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && a[st.top()]<a[i])
+                st.pop();
+            ans[i]=st.empty()?-1:st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+    long long subarrayMax(vector<int>& a)
+    {
+        int n=a.size();
+        vector<int> nge=findnge(a);
+        vector<int> pge=findpge(a);
+        long long sum=0;
+        for(int i=0;i<n;i++)
+        {
+            int left=i-pge[i];
+            int right=nge[i]-i;
+            long long freq=left*right*1LL;
+            long long val=freq*a[i]*1LL;
+            sum+=val;
+        }
+        return sum;
+    }
+    long long subarrayMin(vector<int>& a)
+    {
+        int n=a.size();
+        vector<int> nse=findnse(a);
+        vector<int> pse=findpse(a);
+        long long sum=0;
+        for(int i=0;i<n;i++)
+        {
+            int left=i-pse[i];
+            int right=nse[i]-i;
+            long long freq=left*right*1LL;
+            long long val=freq*a[i]*1LL;
+            sum+=val;
+        }
+        return sum;
+    }
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        return subarrayMax(nums)-subarrayMin(nums);
+    }
+};
