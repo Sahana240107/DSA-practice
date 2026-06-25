de646d23fa8abbc0555fe9c9c4823cfdc09ebc19
Sahana M <sahana240107@gmail.com>
2026-06-25 23:19:11 +0530

    search in sorted matrix solved

--- Binary Search/searchInSortedMatrix.cpp
+++ Binary Search/searchInSortedMatrix.cpp
@@ -10,6 +10,25 @@ You must write a solution in O(log(m * n)) time complexity.
 
 Example 1:
 
-
 Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
 Output: true*/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size(),m=matrix[0].size();
+        int low=0,high=n*m-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            int row=mid/m;
+            int col=mid%m;
+            if(matrix[row][col]==target)
+                return true;
+            else if(matrix[row][col]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return false;
+    }
+};
