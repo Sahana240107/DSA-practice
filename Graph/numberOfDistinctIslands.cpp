231f3683c8089077e9213cf25c08fe02ba054b75
Sahana M <sahana240107@gmail.com>
2026-06-26 17:48:24 +0530

    number of distinct islands solved

--- Graph/numberOfDistinctIslands.cpp
+++ Graph/numberOfDistinctIslands.cpp
@@ -19,3 +19,41 @@ Output: 3
 Explanation: There are four islands in the grid. Two islands have the same shape, while the other two have different shapes. Therefore, the number of distinct island shapes is 3.
 
 */
+class Solution {
+    void dfs(int r,int c,vector<vector<char>>& grid,vector<vector<bool>>& visited,
+    vector<pair<int,int>>& vec,int sr,int sc)
+    {
+        visited[r][c]=true;
+        vec.push_back({r-sr,c-sc});
+        int n=grid.size(),m=grid[0].size();
+        int dr[]={0,0,-1,1};
+        int dc[]={-1,1,0,0};
+        for(int k=0;k<4;k++)
+        {
+            int nr=r+dr[k];
+            int nc=c+dc[k];
+            if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]=='L' && !visited[nr][nc])
+                dfs(nr,nc,grid,visited,vec,sr,sc);
+        }
+    }
+  public:
+    int countDistinctIslands(vector<vector<char>>& grid) {
+        // code here
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<bool>> visited(n,vector<bool> (m,false));
+        set<vector<pair<int,int>>> st;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]=='L' && !visited[i][j])
+                {
+                    vector<pair<int,int>> vec;
+                    dfs(i,j,grid,visited,vec,i,j);
+                    st.insert(vec);
+                }
+            }
+        }
+        return st.size();
+    }
+};
