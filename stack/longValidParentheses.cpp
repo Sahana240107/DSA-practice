0796818770907d845ca313b916cf56dfc80a4220
Sahana M <sahana240107@gmail.com>
2026-06-27 22:59:40 +0530

    long valid parentheses solved using stack

--- stack/longValidParentheses.cpp
+++ stack/longValidParentheses.cpp
@@ -14,6 +14,31 @@ Output: 4
 Explanation: The longest valid parentheses substring is "()()".
 */
 
+//Stack approach
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n=s.size();
+        stack<int> st;
+        st.push(-1);
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(')
+                st.push(i);
+            else
+            {
+                st.pop();
+                if(st.empty())
+                    st.push(i);
+                else
+                    ans=max(ans,i-st.top());
+            }
+        }
+        return ans;
+    }
+};
+
 class Solution {
 public:
     int longestValidParentheses(string s) {
