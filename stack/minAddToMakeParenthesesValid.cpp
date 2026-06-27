0b59586ad8ceadd4ffb50ec54374270bb47bcf52
Sahana M <sahana240107@gmail.com>
2026-06-27 23:31:05 +0530

    minimum add to make parentheses valid solved using stack

--- stack/minAddToMakeParenthesesValid.cpp
+++ stack/minAddToMakeParenthesesValid.cpp
@@ -16,6 +16,29 @@ Example 2:
 
 Input: s = "((("
 Output: 3*/
+
+//USING STACK
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<int> st;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+                st.push(i);
+            else
+            {
+                if(!st.empty() && s[st.top()]=='(')
+                    st.pop();
+                else
+                    st.push(i);
+            }
+        }
+        return st.size();
+    }
+};
+
+//SPACE OPTIMIZED
 class Solution {
 public:
     int minAddToMakeValid(string s) {
