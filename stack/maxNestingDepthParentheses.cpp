f5c98edf884584160bd69ed31b9318b0f176a456
Sahana M <sahana240107@gmail.com>
2026-06-27 23:41:30 +0530

    maximum nesting depth of parentheses optimized

--- stack/maxNestingDepthParentheses.cpp
+++ stack/maxNestingDepthParentheses.cpp
@@ -52,3 +52,20 @@ public:
         return maxi;
     }
 };
+
+//Space optimized
+class Solution {
+public:
+    int maxDepth(string s) {
+        int open=0,ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+                open++;
+            else if(s[i]==')')
+                open--;
+            ans=max(ans,open);
+        }
+        return ans;
+    }
+};
