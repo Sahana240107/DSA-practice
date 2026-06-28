9b5655859a33fcb4f9d8887892a5f0e71e47ac45
Sahana M <sahana240107@gmail.com>
2026-06-28 13:19:36 +0530

    longest harmonious sequence solved using sorting

--- array/longestHarmoniousSequence.cpp
+++ array/longestHarmoniousSequence.cpp
@@ -51,3 +51,18 @@ public:
 };
 
 //Sorting
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int left=0,ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            while(nums[i]-nums[left]>1)
+                left++;
+            if(nums[i]-nums[left]==1)
+                ans=max(ans,i-left+1);
+        }
+        return ans;
+    }
+};
