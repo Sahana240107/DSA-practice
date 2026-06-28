9ccdf2db889fa060db5e067b9ccfcb6007a55d95
Sahana M <sahana240107@gmail.com>
2026-06-28 17:49:26 +0530

    top k frequent words optimized

--- heap/topKFrequentWords.cpp
+++ heap/topKFrequentWords.cpp
@@ -35,3 +35,36 @@ public:
         return ans;
     }
 };
+
+//optimal heap solution
+class Solution {
+    struct compare{
+        bool operator()(pair<string,int>& a,pair<string,int>& b)
+        {
+            if(a.second==b.second)   
+                return a.first<b.first;
+            return a.second>b.second;
+        }
+    };
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string,int> freq;
+        for(int i=0;i<words.size();i++)
+            freq[words[i]]++;
+        priority_queue<pair<string,int>,vector<pair<string,int>>,compare> pq;
+        for(auto it:freq)
+        {
+            pq.push({it.first,it.second});
+            if(pq.size()>k)
+                pq.pop();
+        }
+        vector<string> ans;
+        while(!pq.empty())
+        {
+            ans.push_back(pq.top().first);
+            pq.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
