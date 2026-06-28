434ab6264a83867cc5514b806dfb5256a49a21ac
Sahana M <sahana240107@gmail.com>
2026-06-28 18:59:37 +0530

    top k frequent elements solved

--- heap/topKFrequentElements.cpp
+++ heap/topKFrequentElements.cpp
@@ -17,3 +17,36 @@ Example 3:
 Input: nums = [1,2,1,2,1,2,3,1,3,2], k = 2
 
 Output: [1,2]*/
+
+class Solution {
+    struct compare
+    {
+        bool operator()(pair<int,int>& a,pair<int,int>& b)
+        {
+            return a.second>b.second;
+        }
+    };
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,compare> pq;
+        for(auto it:mp)
+        {
+            pq.push(it);
+            if(pq.size()>k)
+                pq.pop();
+        }
+        vector<int> ans;
+        while(!pq.empty())
+        {
+            ans.push_back(pq.top().first);
+            pq.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
