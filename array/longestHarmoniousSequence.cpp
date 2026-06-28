090ec2784ec93c015f6c91498461eac2e559e333
Sahana M <sahana240107@gmail.com>
2026-06-28 13:09:27 +0530

    longest harmonious sequence solved

--- array/longestHarmoniousSequence.cpp
+++ array/longestHarmoniousSequence.cpp
@@ -39,13 +39,15 @@ public:
         unordered_map<int,int> mp;
         for(int i=0;i<nums.size();i++)
             mp[nums[i]]++;
-        int ans=INT_MIN;
+        int ans=0;
         for(auto it:mp)
         {
             int num=it.first;
             if(mp.find(num+1)!=mp.end())
                 ans=max(ans,mp[num+1]+mp[num]);
         }
-        return ans==INT_MIN?0:ans;
+        return ans;
     }
 };
+
+//Sorting
