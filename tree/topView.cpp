c952f5a0f0d73699c0d81d2b7aab253c792082c5
Sahana M <sahana240107@gmail.com>
2026-06-28 23:13:02 +0530

    top view solved

--- tree/topView.cpp
+++ tree/topView.cpp
@@ -17,3 +17,51 @@ Output: [40, 20, 10, 30, 100]
 Explanation: The Green colored nodes represents the top view in the below Binary tree.
 
 */
+
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> topView(Node *root) {
+        // code here
+        if(!root)
+            return {};
+        int mini=INT_MAX,maxi=INT_MIN;
+        unordered_map<int,int> mp;
+        queue<pair<Node*,int>> q;
+        q.push({root,0});
+        while(!q.empty())
+        {
+            Node* node=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            maxi=max(maxi,col);
+            mini=min(mini,col);
+            if(mp.find(col)==mp.end())
+                mp[col]=node->data;
+            if(node->left)
+                q.push({node->left,col-1});
+            if(node->right)
+                q.push({node->right,col+1});
+        }
+        vector<int> ans;
+        for(int i=mini;i<=maxi;i++)
+        {
+            ans.push_back(mp[i]);
+        }
+        return ans;
+    }
+};
