74e8d832885f44364a59abb9483ee00e0f0d988c
Sahana M <sahana240107@gmail.com>
2026-07-12 19:36:57 +0530

    lca question solved

--- tree/lca.cpp
+++ tree/lca.cpp
@@ -14,3 +14,18 @@ Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
 Output: 2
 Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
 */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root || root==p || root==q)
+            return root;
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        if(left==NULL)
+            return right;
+        else if(right==NULL)
+            return left;
+        else
+            return root;
+    }
+};
