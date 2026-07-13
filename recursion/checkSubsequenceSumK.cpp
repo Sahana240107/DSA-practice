6fdf3a3c5f8b425eae60f2ca35342cdf0cd0c9f0
Sahana M <sahana240107@gmail.com>
2026-07-13 17:56:05 +0530

    check subsequence sum k added

---
+++ recursion/checkSubsequenceSumK.cpp
@@ -0,0 +1,13 @@
+/*Subsequence with Sum K
+Difficulty: MediumAccuracy: 40.26%Submissions: 85K+Points: 4Average Time: 30m
+Given an array arr and target sum k, check whether there exists a subsequence such that the sum of all elements in the subsequence equals to k.
+
+Examples:
+
+Input: arr = [10, 1, 2, 7, 6, 1, 5], k = 8.
+Output: true
+Explanation: Subsequences like [2, 6], [1, 7] sum upto 8
+Input: arr = [2, 3, 5, 7, 9], k = 100. 
+Output: false
+Explanation: No subsequence can sum upto 100
+*/
