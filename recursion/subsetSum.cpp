a787f8c29e38c1f7a4a4bffd947de267cb6612d7
Sahana M <sahana240107@gmail.com>
2026-07-15 21:15:41 +0530

    subset sum solved

--- recursion/subsetSum.cpp
+++ recursion/subsetSum.cpp
@@ -23,3 +23,25 @@ public:
         return sums;
     }
 };
+
+/*78. Subsets
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+*/
