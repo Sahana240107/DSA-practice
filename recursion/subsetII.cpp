b223bc61cd62987d08a1c98ba50e88eec350c65e
Sahana M <sahana240107@gmail.com>
2026-07-15 21:33:17 +0530

    subset II solved

--- recursion/subsetII.cpp
+++ recursion/subsetII.cpp
@@ -10,3 +10,26 @@ Example 2:
 
 Input: nums = [0]
 Output: [[],[0]]*/
+
+class Solution {
+    void helper(int ind,vector<int>& ds,vector<int>& a,vector<vector<int>>& ans)
+    {
+        ans.push_back(ds);
+        for(int i=ind;i<a.size();i++)
+        {
+            if(i>ind && a[i]==a[i-1])
+                continue;
+            ds.push_back(a[i]);
+            helper(i+1,ds,a,ans);
+            ds.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(nums.begin(),nums.end());
+        helper(0,ds,nums,ans);
+        return ans;
+    }
+};
