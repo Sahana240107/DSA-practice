b51c6029e9a1b566be6a0407645ba9c63e6d9918
Sahana M <sahana240107@gmail.com>
2026-07-15 22:33:26 +0530

    permutation solved

--- recursion/permutation.cpp
+++ recursion/permutation.cpp
@@ -48,3 +48,28 @@ public:
         return ans;
     }
 };
+
+
+//optimal
+class Solution {
+    void helper(int ind,vector<int>& a,vector<vector<int>>& ans)
+    {
+        if(ind==a.size())
+        {
+            ans.push_back(a);
+            return;
+        }
+        for(int i=ind;i<a.size();i++)
+        {
+            swap(a[ind],a[i]);
+            helper(ind+1,a,ans);
+            swap(a[ind],a[i]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        helper(0,nums,ans);
+        return ans;
+    }
+};
