1c56e63fb05668f8ab2f621e2654809cfde20fcc
Sahana M <sahana240107@gmail.com>
2026-07-16 21:28:03 +0530

    kth element of 2 sorted arrays solved

--- Binary Search/kthElementOf2SortedArrays.cpp
+++ Binary Search/kthElementOf2SortedArrays.cpp
@@ -9,3 +9,35 @@ Explanation: The final combined sorted array would be [1, 2, 3, 4, 6, 7, 8, 9, 1
 Input: a[] = [1, 4, 8, 10, 12], b[] = [5, 7, 11, 15, 17], k = 6
 Output: 10
 Explanation: Combined sorted array is [1, 4, 5, 7, 8, 10, 11, 12, 15, 17]. The 6th element of this array is 10*/
+class Solution {
+  public:
+    int kthElement(vector<int> &a, vector<int> &b, int k) {
+        // code here
+        int n1=a.size(),n2=b.size();
+        if(n1>n2)
+            return kthElement(b,a,k);
+        int low=max(0,k-n2),high=min(k,n1);
+        int left=k;
+        while(low<=high)
+        {
+            int mid1=low+(high-low)/2;
+            int mid2=left-mid1;
+            int l1=INT_MIN,l2=INT_MIN,r1=INT_MAX,r2=INT_MAX;
+            if(mid1<n1)
+                r1=a[mid1];
+            if(mid2<n2)
+                r2=b[mid2];
+            if(mid1-1>=0)
+                l1=a[mid1-1];
+            if(mid2-1>=0)
+                l2=b[mid2-1];
+            if(l1<=r2 && l2<=r1)
+                return max(l1,l2);
+            else if(l1>r2)
+                high=mid1-1;
+            else
+                low=mid1+1;
+        }
+        return -1;
+    }
+};
