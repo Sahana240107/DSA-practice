4d17de09c0cdb86f37a8ec1dc89505b030c0c792
Sahana M <sahana240107@gmail.com>
2026-07-16 17:29:47 +0530

    tree paths question solved

--- tree/treePaths.cpp
+++ tree/treePaths.cpp
@@ -7,3 +7,28 @@ Example 1:
 Input: root = [1,2,3,null,5]
 Output: ["1->2->5","1->3"]
 */
+class Solution {
+    void helper(TreeNode* root,string s,vector<string>& ans)
+    {
+        if(!root)
+        {
+            return;
+        }
+        if(s!="")
+            s+="->";
+        s+=to_string(root->val);
+        if(!root->left && !root->right)
+        {
+            ans.push_back(s);
+            return;
+        }
+        helper(root->left,s,ans);
+        helper(root->right,s,ans);
+    }
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> ans;
+        helper(root,"",ans);
+        return ans;
+    }
+};
