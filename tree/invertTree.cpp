a8cddbd6d86be18f4085bb0edb12bf6a6447c95d
Sahana M <sahana240107@gmail.com>
2026-07-16 13:15:29 +0530

    invert tree question solved

--- tree/invertTree.cpp
+++ tree/invertTree.cpp
@@ -3,3 +3,17 @@ Given the root of a binary tree, invert the tree, and return its root.
 Example 1:
 Input: root = [4,2,7,1,3,6,9]
 Output: [4,7,2,9,6,3,1]*/
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root)
+            return root;
+        TreeNode* left=root->left;
+        root->left=root->right;
+        root->right=left;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
