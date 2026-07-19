20282d302b4461c419fa2c448a12e8047344bff7
Sahana M <sahana240107@gmail.com>
2026-07-19 22:55:03 +0530

    children sum property check solved

--- tree/childrenSumCheck.cpp
+++ tree/childrenSumCheck.cpp
@@ -14,3 +14,20 @@ Input: root = [35, 20, 15, 15, 5, 10, 5]
 Output: True
 Explanation: Here, every node is sum of its left and right child.
 */
+class Solution {
+  public:
+    bool isSumProperty(Node *root) {
+        // code here
+        if(!root || !root->left && !root->right)
+            return true;
+        int child=0;
+        if(root->left)
+            child+=root->left->data;
+        if(root->right)
+            child+=root->right->data;
+        if(child!=root->data)
+            return false;
+        return isSumProperty(root->left)&&isSumProperty(root->right);
+        
+    }
+};
