4559ca7349056600a080856c1ce096d6220402db
Sahana M <sahana240107@gmail.com>
2026-07-19 22:54:43 +0530

    children sum property check question added

---
+++ tree/childrenSumCheck.cpp
@@ -0,0 +1,16 @@
+/*Children Sum in a Binary Tree
+Solved
+Difficulty: MediumAccuracy: 51.58%Submissions: 254K+Points: 4Average Time: 20m
+Given a binary tree, find if it satisfies the Children Sum Property which has the following rules
+
+Each non-leaf node must have a value equal to the sum of its left and right children's values.
+A NULL child is considered to have a value of 0, and all leaf nodes are considered valid by default.
+ 
+
+Examples:
+
+Input: root = [35, 20, 15, 15, 5, 10, 5]
+
+Output: True
+Explanation: Here, every node is sum of its left and right child.
+*/
