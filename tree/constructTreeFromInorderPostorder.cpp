8be8c0e5532b3b371923c9c45c376a7813045706
Sahana M <sahana240107@gmail.com>
2026-07-22 23:13:28 +0530

    construct tree from inorder and postorder solved

--- tree/constructTreeFromInorderPostorder.cpp
+++ tree/constructTreeFromInorderPostorder.cpp
@@ -4,3 +4,23 @@ Example 1:
 Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
 Output: [3,9,20,null,null,15,7]
 */
+class Solution {
+    TreeNode* helper(vector<int>& postorder,int ps,int pe,vector<int>& inorder,int is,int ie,unordered_map<int,int>& mp)
+    {
+        if(ps>pe||is>ie)
+            return NULL;
+        TreeNode* root=new TreeNode(postorder[pe]);
+        int inRoot=mp[postorder[pe]];
+        int numsleft=inRoot-is;
+        root->left=helper(postorder,ps,ps+numsleft-1,inorder,is,inRoot-1,mp);
+        root->right=helper(postorder,ps+numsleft,pe-1,inorder,inRoot+1,ie,mp);
+        return root;
+    }
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<inorder.size();i++)
+            mp[inorder[i]]=i;
+        return helper(postorder,0,postorder.size()-1,inorder,0,inorder.size()-1,mp);
+    }
+};
