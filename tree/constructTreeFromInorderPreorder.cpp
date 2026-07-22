557a4aa3a14bc68f977792461dbe500674e804b1
Sahana M <sahana240107@gmail.com>
2026-07-22 23:02:50 +0530

    construct tree from inorder and preorder solved

--- tree/constructTreeFromInorderPreorder.cpp
+++ tree/constructTreeFromInorderPreorder.cpp
@@ -4,3 +4,23 @@ Example 1:
 Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
 Output: [3,9,20,null,null,15,7]
 */
+class Solution {
+    TreeNode* helper(vector<int>& preorder,int ps,int pe,vector<int>& inorder,int is,int ie,unordered_map<int,int>& mp)
+    {
+        if(ps>pe||is>ie)
+            return NULL;
+        TreeNode* root=new TreeNode(preorder[ps]);
+        int inRoot=mp[preorder[ps]];
+        int numsLeft=inRoot-is;
+        root->left=helper(preorder,ps+1,ps+numsLeft,inorder,is,inRoot-1,mp);
+        root->right=helper(preorder,ps+numsLeft+1,pe,inorder,inRoot+1,ie,mp);
+        return root;
+    }
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int,int> inMap;
+        for(int i=0;i<inorder.size();i++)
+            inMap[inorder[i]]=i;
+        return helper(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,inMap);
+    }
+};
