60dc8cdb54ad35535dcc77e112d3c05deeac68b9
Sahana M <sahana240107@gmail.com>
2026-07-23 23:10:55 +0530

    group anagrams solved

--- Strings/groupAnagrams.cpp
+++ Strings/groupAnagrams.cpp
@@ -12,3 +12,23 @@ Explanation:
 There is no string in strs that can be rearranged to form "bat".
 The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
 The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.*/
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mp;
+        for(string s:strs)
+        {
+            char freq[26]={0};
+            for(char ch:s)
+                freq[ch-'a']++;
+            string key="";
+            for(auto it:freq)
+                key+=to_string(it);
+            mp[key].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for(auto it:mp)
+            ans.push_back(it.second);
+        return ans;
+    }
+};
