bf2a7f3265b351d9b3e82438bb732a05af169481
Sahana M <sahana240107@gmail.com>
2026-07-25 21:47:13 +0530

    merge intervals solved

--- interval/mergeInterval.cpp
+++ interval/mergeInterval.cpp
@@ -17,3 +17,23 @@ Input: intervals = [[4,7],[1,4]]
 Output: [[1,7]]
 Explanation: Intervals [1,4] and [4,7] are considered overlapping.
 */
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<intervals.size();i++)
+        {
+            if(ans.empty()||ans.back()[1]<intervals[i][0])
+            {
+                ans.push_back(intervals[i]);
+            }
+            else
+            {
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+            }
+        }
+        return ans;
+    }
+};
