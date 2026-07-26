c4060572844bb4d051e5aad1a5f6f0cf437f692e
Sahana M <sahana240107@gmail.com>
2026-07-26 23:07:02 +0530

    subtree sum tree added

---
+++ tree/subtreeSumTree.cpp
@@ -0,0 +1,18 @@
+/*Sum Tree
+Solved
+Difficulty: MediumAccuracy: 37.17%Submissions: 290K+Points: 4
+Given the root of a Binary Tree with n nodes, check whether it is a Sum Tree and return true if it is, otherwise return false.
+
+A Sum Tree is a Binary Tree in which the value of every non-leaf node is equal to the sum of all nodes present in its left and right subtrees. An empty tree and a leaf node are also considered Sum Trees.
+
+Examples:
+
+Input: root[] = [3, 1, 2]
+ 
+Output: true
+Explanation: The sum of left subtree and right subtree is 1 + 2 = 3, which is the value of the root node. Therefore, the given binary tree is a sum tree.
+Input: root[] = [10, 20, 30, 10, 10]
+ 
+Output: false
+Explanation: The given tree is not a Sum Tree. For the root node, the sum of nodes in the left and right subtrees is 40 + 30 = 70, which is not equal to the root value 10.
+*/
