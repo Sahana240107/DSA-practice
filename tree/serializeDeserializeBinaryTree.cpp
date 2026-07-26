4e7638d5e2f52e55e3449b7ecc0b9d98ccda4a65
Sahana M <sahana240107@gmail.com>
2026-07-26 21:55:53 +0530

    serialize and deserialize binary tree solved

--- tree/serializeDeserializeBinaryTree.cpp
+++ tree/serializeDeserializeBinaryTree.cpp
@@ -8,3 +8,64 @@ Example 1:
 Input: root = [1,2,3,null,null,4,5]
 Output: [1,2,3,null,null,4,5]
 */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if(!root)
+            return "";
+        queue<TreeNode*> q;
+        q.push(root);
+        string s="";
+        while(!q.empty())
+        {
+            TreeNode* node=q.front();
+            q.pop();
+            if(!node)
+                s.append("#,");
+            else
+                s.append(to_string(node->val)+",");
+            if(node)
+            {
+                q.push(node->left);
+                q.push(node->right);
+            }
+        }
+        return s;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if(data.size()==0)
+            return NULL;
+        stringstream s(data);
+        string str;
+        getline(s,str,',');
+        TreeNode* root=new TreeNode(stoi(str));
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            TreeNode* node=q.front();
+            q.pop();
+            getline(s,str,',');
+            if(str=="#")
+                node->left=NULL;
+            else
+            {
+                node->left=new TreeNode(stoi(str));
+                q.push(node->left);
+            }
+            getline(s,str,',');
+            if(str=="#")
+                node->right=NULL;
+            else
+            {
+                node->right=new TreeNode(stoi(str));
+                q.push(node->right);
+            }
+        }
+        return root;
+    }
+};
