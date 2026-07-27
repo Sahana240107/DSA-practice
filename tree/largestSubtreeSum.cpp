da65f463931b797c69e30ae68b0cf2f34e86a7e5
Sahana M <sahana240107@gmail.com>
2026-07-27 14:06:52 +0530

    largest subtree sum added

---
+++ tree/largestSubtreeSum.cpp
@@ -0,0 +1,20 @@
+/*Largest Subtree Sum
+Solved
+Difficulty: MediumAccuracy: 54.83%Submissions: 30K+Points: 4
+Given a binary tree, find subtree with maximum sum in the tree and return its sum.
+
+Examples:
+
+Input: root[] = [10, 8, 2, 3, 5, N, N]    
+        
+Output: 28
+Explanation: 
+As all the tree elements are positive, the largest subtree sum is equal to sum of all tree elements.
+Input: root[] = [1, -2, 3, 4, 5, -6, 2]
+        
+Output: 7
+Explanation: 
+Subtree with largest sum is : 
+         
+The whole tree sum is also 7. 
+*/
