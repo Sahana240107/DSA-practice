77e731d29e458d83c86310892e96f8359e2ee202
Sahana M <sahana240107@gmail.com>
2026-07-28 17:41:29 +0530

    range sum bst solved

--- tree/rangeSumBST.cpp
+++ tree/rangeSumBST.cpp
@@ -11,3 +11,15 @@ Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
 Output: 23
 Explanation: Nodes 6, 7, and 10 are in the range [6, 10]. 6 + 7 + 10 = 23.
 */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(!root)
+            return 0;
+        if(root->val<low)
+            return rangeSumBST(root->right,low,high);
+        if(root->val>high)
+            return rangeSumBST(root->left,low,high);
+        return root->val+rangeSumBST(root->left,low,high)+rangeSumBST(root->right,low,high);
+    }
+};
