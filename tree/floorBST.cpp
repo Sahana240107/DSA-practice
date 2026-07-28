9ed92033375c9e7d977854e44519d57b757d9cd6
Sahana M <sahana240107@gmail.com>
2026-07-28 22:56:15 +0530

    floor in BST added

---
+++ tree/floorBST.cpp
@@ -0,0 +1,14 @@
+/*Floor in BST
+Solved
+Difficulty: EasyAccuracy: 36.98%Submissions: 243K+Points: 2
+Given the root of a binary search tree and a number k, find the greatest number in the binary search tree that is less than or equal to k.
+
+Note: If no such node value exists that is smaller than k, then return -1.
+
+Examples:
+
+Input: root = [10, 7, 15, 2, 8, 11, 16],  k  =  14
+
+Output: 11
+Explanation: The greatest element in the tree which is less than or equal to 14, is 11.
+*/
