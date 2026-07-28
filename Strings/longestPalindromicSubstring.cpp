cb538bc07c8df7d4844ebfe069d56d5782448216
Sahana M <sahana240107@gmail.com>
2026-07-28 19:28:46 +0530

    longest palindromic substring solved

--- Strings/longestPalindromicSubstring.cpp
+++ Strings/longestPalindromicSubstring.cpp
@@ -9,3 +9,30 @@ Example 2:
 
 Input: s = "cbbd"
 Output: "bb"*/
+class Solution {
+    int start=0,maxlen=0;
+    void expand(string& s,int l,int r)
+    {
+        while(l>=0 && r<s.size() && s[l]==s[r])
+        {
+            l--;
+            r++;
+        }
+        int len=r-l-1;
+        if(len>maxlen)
+        {
+            maxlen=len;
+            start=l+1;
+        }
+    }
+public:
+    string longestPalindrome(string s) {
+        int n=s.size();
+        for(int i=0;i<s.size();i++)
+        {
+            expand(s,i,i);
+            expand(s,i,i+1);
+        }
+        return s.substr(start,maxlen);
+    }
+};
