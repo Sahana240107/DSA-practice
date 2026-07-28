51048a21d27fe37c9dfa6102089093d5c1c0f7fb
Sahana M <sahana240107@gmail.com>
2026-07-28 22:52:07 +0530

    ceil in BST added

---
+++ tree/ceilBST.cpp
@@ -0,0 +1,12 @@
+/*Ceil in BST
+
+Given a root binary search tree and an integer x , find the Ceil of x in the tree.
+
+Ceil(x) is a number that is either equal to x or is immediately greater than x. If Ceil could not be found, return -1.
+
+Examples:
+
+Input: root = [5, 1, 7, N, 2, N, N, N, 3], x = 3
+
+Output: 3
+Explanation: We find 3 in BST, so ceil of 3 is 3.*/
