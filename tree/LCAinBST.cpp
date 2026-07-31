4963193621560f15fdb1324dd8260ddab6f22e0d
Sahana M <sahana240107@gmail.com>
2026-07-31 22:50:55 +0530

    lca in bst solved

--- tree/LCAinBST.cpp
+++ tree/LCAinBST.cpp
@@ -11,3 +11,15 @@ Example 2:
 Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
 Output: 2
 Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.*/
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)
+            return NULL;
+        if(root->val>p->val && root->val>q->val)
+            return lowestCommonAncestor(root->left,p,q);
+        else if(root->val<p->val && root->val<q->val)
+            return lowestCommonAncestor(root->right,p,q);
+        return root;
+    }
+};
