085a5a4da275a975654d7b5582e1ba1b47fdd2ae
Sahana M <sahana240107@gmail.com>
2026-08-09 18:57:01 +0530

    reverse dll added

---
+++ linked list/reverseDLL.cpp
@@ -0,0 +1,17 @@
+/*Reverse a Doubly Linked List
+Solved
+Difficulty: EasyAccuracy: 70.38%Submissions: 225K+Points: 2Average Time: 15m
+Given the head of a doubly linked list, reverse the list and return the head of the reversed doubly linked list.
+
+Note: Driver code will print the returned list in both forward and backward directions.
+
+Examples:
+
+Input:
+
+Output: 
+5 <-> 4 <-> 3
+3 <-> 4 <-> 5
+Explanation: After reversing the given doubly linked list the new list will be 5 <-> 4 <-> 3.
+
+*/
