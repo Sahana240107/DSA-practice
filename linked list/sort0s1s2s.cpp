fc3e1472b169b3a9507110809e42daaaa29484ad
Sahana M <sahana240107@gmail.com>
2026-08-09 19:54:29 +0530

    sort 0s 1s 2s solved

--- linked list/sort0s1s2s.cpp
+++ linked list/sort0s1s2s.cpp
@@ -16,3 +16,39 @@ Output: 0 → 1 → 2 → 2
 Explanation: After arranging all the 0s, 1s and 2s in the given format, the output will be:
    
 */
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        // code here
+        Node* zeroHead=new Node(-1);
+        Node* oneHead=new Node(-1);
+        Node* twoHead=new Node(-1);
+        Node* zero=zeroHead;
+        Node* one=oneHead;
+        ode* two=twoHead;
+        Node* temp=head;
+        while(temp)
+        {
+            if(temp->data==0)
+            {
+                zero->next=temp;
+                zero=zero->next;
+            }
+            else if(temp->data==1)
+            {
+                one->next=temp;
+                one=one->next;
+            }
+            else
+            {
+                two->next=temp;
+                two=two->next;
+            }
+            temp=temp->next;
+        }
+        zero->next=oneHead->next?oneHead->next:twoHead->next;
+        one->next=twoHead->next;
+        two->next=NULL;
+        return zeroHead->next;
+    }
+};
