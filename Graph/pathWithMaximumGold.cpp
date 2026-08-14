29bc3acc4741eac1969d719aadddfd5e5fd7eba9
Sahana M <sahana240107@gmail.com>
2026-08-14 21:12:02 +0530

    path with maximum gold solved

--- Graph/pathWithMaximumGold.cpp
+++ Graph/pathWithMaximumGold.cpp
@@ -31,4 +31,41 @@ Explanation:
  [0,3,0],
  [9,0,20]]
 Path to get the maximum gold, 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7.*/
-
+class Solution {
+    void dfs(int sr,int sc,vector<vector<int>>& grid,vector<vector<bool>>& visited,int sum,int& maxi)
+    {
+        int n=grid.size(),m=grid[0].size();
+        visited[sr][sc]=true;
+        sum+=grid[sr][sc];
+        maxi=max(maxi,sum);
+        int dr[]={-1,1,0,0};
+        int dc[]={0,0,-1,1};
+        for(int i=0;i<4;i++)
+        {
+            int r=sr+dr[i];
+            int c=sc+dc[i];
+            if(r<n && r>=0 && c<m && c>=0 && !visited[r][c] && grid[r][c]!=0)
+            {
+                dfs(r,c,grid,visited,sum,maxi);
+            }
+        }
+        visited[sr][sc]=false;
+    }
+public:
+    int getMaximumGold(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<bool>> visited(n,vector<bool> (m,false));
+        int maxi=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]!=0)
+                {
+                    dfs(i,j,grid,visited,0,maxi);
+                }
+            }
+        }
+        return maxi;
+    }
+};
