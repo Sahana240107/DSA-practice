13c913ae51937b791bf7f71b277ff6b7307f6078
Sahana M <sahana240107@gmail.com>
2026-08-19 23:16:36 +0530

    coin change solved

--- dp/coinChange.cpp
+++ dp/coinChange.cpp
@@ -25,3 +25,21 @@ Example 3:
 
 Input: coins = [1], amount = 0
 Output: 0*/
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        if(amount==0)
+            return 0;
+        vector dp(amount+1,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++)
+        {
+            for(int coin:coins)
+            {
+                if(coin<=i)
+                    dp[i]=min(dp[i],dp[i-coin]+1);
+            }
+        }
+        return dp[amount]>amount?-1:dp[amount];
+    }
+};
