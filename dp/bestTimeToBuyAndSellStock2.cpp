ef91270a9a60d44db3d9384554a7c745a2b435a2
Sahana M <sahana240107@gmail.com>
2026-09-06 23:19:40 +0530

    best time to buy and sell stock solved using dp

--- dp/bestTimeToBuyAndSellStock2.cpp
+++ dp/bestTimeToBuyAndSellStock2.cpp
@@ -43,3 +43,21 @@ public:
         return profit;
     }
 };
+
+//dp 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<int> buy(n);
+        vector<int> sell(n);
+        buy[0]=-prices[0];
+        sell[0]=0;
+        for(int i=1;i<n;i++)
+        {
+            buy[i]=max(buy[i-1],sell[i-1]-prices[i]);
+            sell[i]=max(sell[i-1],buy[i-1]+prices[i]);
+        }
+        return sell[n-1];
+    }
+};
