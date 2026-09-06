3b0b73c7c13d152735219160061113ac8e2878fe
Sahana M <sahana240107@gmail.com>
2026-09-06 12:40:44 +0530

    find the difference solved

--- hash/findTheDifference.cpp
+++ hash/findTheDifference.cpp
@@ -15,3 +15,26 @@ Example 2:
 
 Input: s = "", t = "y"
 Output: "y"*/
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int> mp;
+        for(char c:s)
+        {
+            mp[c]++;
+        }
+        for(char c:t)
+        {
+            if(mp.find(c)!=mp.end())
+            {
+                mp[c]--;
+                if(mp[c]==0)
+                    mp.erase(c);
+            }
+            else
+                return c;
+        }
+        return ' ';
+    }
+};
